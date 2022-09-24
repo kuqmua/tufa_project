7d0dc914831514fb53eadb5ae5166cc9d6e2ac07
kuqmua <serezhashmakov@outlook.com>
2022-09-24 21:27:52 +0300

    impl_get_source_for_enum_with_method proc macro concept(not actually a submodule yet)

---
+++ proc_macros/impl_get_source_for_enum_with_method/src/lib.rs
@@ -0,0 +1,134 @@
+#[proc_macro_derive(ImplGetSourceForEnumWithMethod)]
+pub fn derive_impl_get_source_for_enum_with_method(
+    input: proc_macro::TokenStream,
+) -> proc_macro::TokenStream {
+    let ast: syn::DeriveInput =
+        syn::parse(input).expect("ImplGetSourceForEnumWithMethod syn::parse(input) failed");
+    let ident = &ast.ident;
+    match ast.data {
+        syn::Data::Union(_) => panic!("ImplGetSourceForEnumWithMethod only work on enums!"),
+        syn::Data::Struct(_) => panic!("ImplGetSourceForEnumWithMethod only work on enums!"),
+        syn::Data::Enum(data_enum) => {
+            let variants = data_enum.variants.into_iter().map(|v| {
+                let variant_ident = v.ident;
+                match v.fields {
+                    syn::Fields::Unit => panic!(
+                        "ImplGetSourceForEnumWithMethod still not work with syn::Fields::Unit"
+                    ),
+                    syn::Fields::Named(fields_named) => {
+                        
+                        // let one = fields_named.clone();
+                        // let two = fields_named.clone();
+                        // let mut scopes = one.named.iter().map(|_| String::from("{}\n")).fold(
+                        //     String::from(""),
+                        //     |mut acc, elem| {
+                        //         acc.push_str(&elem);
+                        //         acc
+                        //     },
+                        // );
+                        // if !scopes.is_empty() {
+                        //     scopes.pop();
+                        // }
+                        // let fields_idents = two.named.iter().map(|field| {
+                        //     let field_ident = field
+                        //         .ident
+                        //         .clone()
+                        //         .expect("some of named fields doesnt have ident");
+                        //     quote::quote! { #field_ident }
+                        // });
+                        // let fields_idents_map = fields_named.named.iter().map(|field| {
+                        //     let field_ident = field
+                        //         .ident
+                        //         .clone()
+                        //         .expect("some of named fields doesnt have ident");
+                        //     quote::quote! { #field_ident.get_source() }
+                        // });
+                        quote::quote! {
+                            // #ident::#variant_ident{
+                            //     #(#fields_idents,)*
+                            // } => {
+                            //     format!(
+                            //         #scopes,
+                            //         #(#fields_idents_map,)*
+                            //     )
+                            // }
+                        }
+                    }
+                    syn::Fields::Unnamed(unnamed) => {
+                        match unnamed.unnamed.len() {
+                            1 => {
+                                match &unnamed.unnamed[0].ty {
+                                    // Array(TypeArray),
+                                    // BareFn(TypeBareFn),
+                                    // Group(TypeGroup),
+                                    // ImplTrait(TypeImplTrait),
+                                    // Infer(TypeInfer),
+                                    // Macro(TypeMacro),
+                                    // Never(TypeNever),
+                                    // Paren(TypeParen),
+                                    // Ptr(TypePtr),
+                                    // Reference(TypeReference),
+                                    // Slice(TypeSlice),
+                                    // TraitObject(TypeTraitObject),
+                                    // Tuple(TypeTuple),
+                                    // Verbatim(TokenStream),
+                                    syn::Type::Path(type_path) => {
+                                        match type_path.path.segments.len() {
+                                            1 => {
+                                                let segment_ident = format!("{}", type_path.path.segments[0].ident);
+                                                if segment_ident == *"Vec" {
+                                                    quote::quote! {
+                                                        #ident::#variant_ident(error_vec) => {
+                                                            let mut formatted = error_vec
+                                                                .iter()
+                                                                .map(|error| format!("{} ,", error.get_source()))
+                                                                .fold(String::from(""), |mut acc, elem| {
+                                                                        acc.push_str(&elem);
+                                                                        acc
+                                                                }
+                                                            );
+                                                            if !formatted.is_empty() {
+                                                                formatted.pop();
+                                                                formatted.pop();
+                                                            }
+                                                            format!("[{}]", formatted)
+                                                            // String::from("kke")
+
+                                                        }
+                                                    }
+                                                }
+                                                // else if segment_ident == *"HashMap" {
+                                                //     quote::quote! {
+                                                        
+                                                //     }
+                                                // }
+                                                else {
+                                                    quote::quote! {
+                                                        //
+                                                    }
+                                                }
+                                            },
+                                            _ => panic!("ImplGetSourceForEnumWithMethod only work on enums with type_path.segments.len() == 1!")
+                                        }
+                                    },
+                                    _ => panic!("ImplGetSourceForEnumWithMethod only work on enums with Path(type_path)!")
+                                }
+                            }
+                            _ => panic!("ImplGetSourceForEnumWithMethod only work on enums with unnamed.len() == 1!")
+                        }
+                    }
+                }
+            });
+            let gen = quote::quote! {
+                impl tufa_common::traits::get_source::GetSource for #ident {
+                    fn get_source(&self) -> String {
+                        match self {
+                            #(#variants,)*
+                        }
+                    }
+                }
+            };
+            gen.into()
+        }
+    }
+}
---
+++ proc_macros/impl_get_source_for_struct_with_method/src/lib.rs
@@ -0,0 +1,84 @@
+#[proc_macro_derive(ImplGetSourceForStructWithMethod)]
+pub fn derive_impl_get_source_for_struct_with_method(
+    input: proc_macro::TokenStream,
+) -> proc_macro::TokenStream {
+    let ast: syn::DeriveInput = syn::parse(input).expect(
+        "ImplGetSourceForStructWithMethod syn::parse(input) failed",
+    );
+    let ident = &ast.ident;
+    match ast.data {
+        syn::Data::Union(_) => {
+            panic!("ImplGetSourceForStructWithMethod only work on structs!")
+        }
+        syn::Data::Enum(_) => {
+            panic!("ImplGetSourceForStructWithMethod only work on structs!")
+        }
+        syn::Data::Struct(data_struct) => {
+            match data_struct.fields {
+                syn::Fields::Named(fields_named) => {
+                    match fields_named.named.len() {
+                        2 => {
+                            let source_field_ident = fields_named.named[0].ident.clone().expect("ImplGetSourceForStructWithMethod - there is no first field ident!");
+                            if format!("{}", source_field_ident) != *"source" {
+                                panic!("ImplGetSourceForStructWithMethod - no 'source'-named field found!");
+                            }
+                            match fields_named.named[0].ty.clone() {
+                                // syn::Type::Array(type_array) => {},
+                                // syn::Type::BareFn(TypeBareFn),
+                                // syn::Type::Group(TypeGroup),
+                                // syn::Type::ImplTrait(TypeImplTrait),
+                                // syn::Type::Infer(TypeInfer),
+                                // syn::Type::Macro(TypeMacro),
+                                // syn::Type::Never(TypeNever),
+                                // syn::Type::Paren(TypeParen),
+                                // syn::Type::Ptr(TypePtr),
+                                // syn::Type::Reference(TypeReference),
+                                // syn::Type::Slice(TypeSlice),
+                                // syn::Type::TraitObject(TypeTraitObject),
+                                // syn::Type::Tuple(TypeTuple),
+                                // syn::Type::Verbatim(TokenStream),
+                                syn::Type::Path(type_path) => {
+                                    match type_path.path.segments.len() {
+                                        1 => {
+                                            let possible_vec_or_hashmap_ident_as_string = format!("{}", type_path.path.segments[0].ident);
+                                            if possible_vec_or_hashmap_ident_as_string == *"Vec" {
+                                                let gen = quote::quote! {
+                                                    impl tufa_common::traits::get_source::GetSource for #ident {
+                                                    
+                                                    }
+                                                };
+                                                gen.into()
+                                            }
+                                            else if possible_vec_or_hashmap_ident_as_string == *"HashMap" {
+                                                let gen = quote::quote! {
+                                                    impl tufa_common::traits::get_source::GetSource for #ident {
+                                                    
+                                                    }
+                                                };
+                                                gen.into()
+                                            }
+                                            else {
+                                                let gen = quote::quote! {
+                                                    impl tufa_common::traits::get_source::GetSource for #ident {
+                                                    
+                                                    }
+                                                };
+                                                gen.into()
+                                            }
+                                        }
+                                        _ => panic!("ImplGetSourceForStructWithMethod only work with type_path.path.segments.len() == 1!"),
+                                    }
+                                },
+                                _ => panic!("ImplGetSourceForStructWithMethod only work on Type::Path!")
+                            }
+                        }
+                        _ => panic!("ImplGetSourceForStructWithMethod only work on structs with 2 named fields!")
+                    }
+                },
+                // syn::Fields::Unnamed(_) => {},
+                // syn::Fields::Unit(_) => {},
+                _ => panic!("ImplGetSourceForStructWithMethod only work with syn::Fields::Named!")
+            }
+        }
+    }
+}
