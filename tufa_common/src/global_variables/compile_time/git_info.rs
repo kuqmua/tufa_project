605132c424f2248cb6ceec95fd312b117f2d4827
kuqmua <serezhashmakov@outlook.com>
2024-01-27 17:18:18 +0300

    moch

--- tufa_common/src/global_variables/compile_time/git_info.rs
+++ tufa_common/src/global_variables/compile_time/git_info.rs
@@ -1,2 +1,8 @@
+// pub const GIT_INFO: error_occurence_lib::git_info::GitInfo =
+//     compile_time_git_info::compile_time_git_info_tufa_common!();
+
 pub const GIT_INFO: error_occurence_lib::git_info::GitInfo =
-    compile_time_git_info::compile_time_git_info_tufa_common!();
+    error_occurence_lib::git_info::GitInfo {
+        git_commit_id: "todo",
+        git_repo_link: "todo",
+    };
