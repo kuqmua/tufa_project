c79bb5140ba53d528d7ec395f0f70df285c3b25a
kuqmua <serezhashmakov@outlook.com>
2024-01-27 19:45:37 +0300

    next steps

--- proc_macro_helpers/src/error_occurence/panic_if_not_string.rs
+++ proc_macro_helpers/src/error_occurence/panic_if_not_string.rs
@@ -6,7 +6,7 @@ pub fn panic_if_not_string(
     as_std_collections_hashmap_key_type_stringified: &str,
     attribute: &crate::error_occurence::named_attribute::NamedAttribute,
 ) {
-    if let false = segments_stringified == std_string_string_stringified {
+    if segments_stringified != std_string_string_stringified {
         panic!("{proc_macro_name_ident_stringified} {} {supports_only_stringified} {std_string_string_stringified} {as_std_collections_hashmap_key_type_stringified} (hashmap key must be string for deserialization)", attribute.attribute_view_stringified());
     }
 }
--- proc_macro_helpers/src/error_occurence/possible_lifetime_addition.rs
+++ proc_macro_helpers/src/error_occurence/possible_lifetime_addition.rs
@@ -2,7 +2,7 @@ pub fn possible_lifetime_addition(
     lifetime: std::string::String,
     lifetimes_for_serialize_deserialize: &mut Vec<String>,
 ) {
-    if let false = lifetimes_for_serialize_deserialize.contains(&lifetime) {
+    if !lifetimes_for_serialize_deserialize.contains(&lifetime) {
         lifetimes_for_serialize_deserialize.push(lifetime);
     };
 }
