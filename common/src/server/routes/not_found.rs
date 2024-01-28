069de4f73a0940a8030a7264d7ced5a4434426db
kuqmua <serezhashmakov@outlook.com>
2024-01-28 20:28:58 +0300

    next steps

--- common/src/server/routes/not_found.rs
+++ common/src/server/routes/not_found.rs
@@ -6,32 +6,29 @@ pub trait NotFoundRouteParameters:
     + crate::common::git::get_git_commit_link::GetGitCommitLink
 {
 }
+
+#[derive(Debug, serde::Serialize)]
+struct NotFoundHandle {
+    message: std::string::String,
+    project_commit: std::string::String,
+    open_api_specification: std::string::String,
+}
+
 //todo maybe use swagger instead
 async fn not_found(
-    uri: http::Uri,
+    uri: axum::http::Uri,
     axum::extract::State(app_info): axum::extract::State<DynArcNotFoundRouteParametersSendSync>,
 ) -> impl axum::response::IntoResponse {
-    #[derive(Debug, serde::Serialize)]
-    struct NotFoundHandle {
-        message: std::string::String,
-        project_commit: std::string::String,
-        commit: std::string::String,
-    }
     (
         axum::http::StatusCode::NOT_FOUND,
         axum::Json(NotFoundHandle {
             message: format!("No route for {uri}"),
             project_commit: app_info.get_project_git_commit_link(),
-            commit: app_info.get_git_commit_link(),
+            open_api_specification: std::string::String::from("/swagger-ui/"),
         }),
     )
 }
 
-async fn handler() {}
-
 pub fn not_found_route(app_info: DynArcNotFoundRouteParametersSendSync) -> axum::Router {
-    axum::Router::new().fallback(
-        handler
-        // not_found
-    ).with_state(app_info)
+    axum::Router::new().fallback(not_found).with_state(app_info)
 }
