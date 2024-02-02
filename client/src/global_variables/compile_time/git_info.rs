074b9a45cc0e8a6a56670bf3b1652798f1530985
kuqmua <serezhashmakov@outlook.com>
2024-02-02 12:49:18 +0300

    remove git_infos

--- client/src/global_variables/compile_time/git_info.rs
+++
@@ -1,8 +0,0 @@
-// pub const GIT_INFO: common::common::git::git_info::GitInfo =
-//     compile_time_git_info::compile_time_git_info!(client);
-
-pub const GIT_INFO: error_occurence_lib::git_info::GitInfo =
-    error_occurence_lib::git_info::GitInfo {
-        git_commit_id: "todo",
-        git_repo_link: "todo",
-    };
--- server/src/global_variables/compile_time/git_info.rs
+++
@@ -1,8 +0,0 @@
-// pub const GIT_INFO: error_occurence_lib::git_info::GitInfo =
-//     compile_time_git_info::compile_time_git_info!(server);
-
-pub const GIT_INFO: error_occurence_lib::git_info::GitInfo =
-    error_occurence_lib::git_info::GitInfo {
-        git_commit_id: "todo",
-        git_repo_link: "todo",
-    };
