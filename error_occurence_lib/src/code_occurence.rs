5907961111d396a5e49df6b4681ccb1f623bfa2a
kuqmua <serezhashmakov@outlook.com>
2024-02-04 16:01:46 +0300

    next steps

--- error_occurence_lib/src/code_occurence.rs
+++ error_occurence_lib/src/code_occurence.rs
@@ -34,8 +34,8 @@ impl CodeOccurence {
 impl crate::form_error_path::FormErrorPathDirectory for CodeOccurence {
     fn form_error_path_directory(&self) -> std::string::String {
         format!(
-            "src/{}:{}:{}", //todo "src" - hardcode, for some reason vscode stops following just {}:{}:{} path(without prefix "src")
-            self.file, self.line, self.column
+            "{}/src/{}:{}:{}", //todo "src" - hardcode, for some reason vscode stops following just {}:{}:{} path(without prefix "src")
+            self.project_part, self.file, self.line, self.column
         )
     }
 }
@@ -43,8 +43,8 @@ impl crate::form_error_path::FormErrorPathDirectory for CodeOccurence {
 impl crate::form_error_path::FormErrorPathGithub for CodeOccurence {
     fn form_error_path_github(&self) -> std::string::String {
         format!(
-            "https://github.com/kuqmua/tufa_project/blob/{}/{}#L{}",
-            self.commit, self.file, self.line
+            "https://github.com/kuqmua/tufa_project/blob/{}/{}/{}#L{}",
+            self.commit, self.project_part, self.file, self.line
         )
     }
 }
