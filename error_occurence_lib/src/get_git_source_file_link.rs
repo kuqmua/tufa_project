6f92020f7da302dbbcf1766a392435d324e9dbcb
kuqmua <serezhashmakov@outlook.com>
2024-02-04 16:20:42 +0300

    next steps

--- error_occurence_lib/src/get_git_source_file_link.rs
+++
@@ -1,3 +0,0 @@
-pub trait GetGitSourceFileLink {
-    fn get_git_source_file_link(&self, file: &str, line: u32) -> std::string::String;
-}
--- error_occurence_lib/src/lib.rs
+++ error_occurence_lib/src/lib.rs
@@ -1,26 +1,19 @@
 pub use error_occurence::ErrorOccurence;
+pub mod code_occurence;
+pub mod code_occurence_prepare_for_log;
+pub mod display_foreign_type;
 pub mod error_log;
 pub mod error_occurence_named;
 pub mod error_occurence_unnamed;
-pub mod lines_space_backslash;
-pub mod to_string_with_config;
-pub mod to_string_without_config;
-pub mod helpers;
-pub mod source_to_string_with_config;
-pub mod source_to_string_without_config;
+pub mod form_error_path;
 pub mod get_code_occurence;
-pub mod code_occurence_prepare_for_log;
+pub mod get_code_path;
 pub mod get_code_path_without_config;
 pub mod get_column;
 pub mod get_duration;
 pub mod get_file;
 pub mod get_line;
-pub mod form_error_path;
-pub mod get_git_source_file_link;
 pub mod git_fields;
-pub mod display_foreign_type;
-pub mod get_code_path;
-pub mod code_occurence;
 pub mod hashmap_display_display_foreign_type_into_hashmap_display_string;
 pub mod hashmap_display_display_foreign_type_into_hashmap_string_string;
 pub mod hashmap_display_display_foreign_type_to_string;
@@ -38,8 +31,14 @@ pub mod hashmap_display_foreign_type_to_string_with_config_to_string;
 pub mod hashmap_display_foreign_type_to_string_without_config_to_string;
 pub mod hashmap_display_to_string_with_config_to_string;
 pub mod hashmap_display_to_string_without_config_to_string;
+pub mod helpers;
+pub mod lines_space_backslash;
+pub mod source_to_string_with_config;
+pub mod source_to_string_without_config;
 #[cfg(test)]
 pub mod test;
+pub mod to_string_with_config;
+pub mod to_string_without_config;
 pub mod vec_display_foreign_type_into_vec_string;
 pub mod vec_display_foreign_type_to_string;
 pub mod vec_display_into_vec_string;
@@ -47,7 +46,8 @@ pub mod vec_display_to_string;
 pub mod vec_to_string_with_config_to_string;
 pub mod vec_to_string_without_config_to_string;
 
-
 pub trait IntoSerdeSerializeSerdeDeserialize {
-    fn into_serde_serialize_serde_deserialize(self) -> impl serde::Serialize + serde::Deserialize<'static>;
+    fn into_serde_serialize_serde_deserialize(
+        self,
+    ) -> impl serde::Serialize + serde::Deserialize<'static>;
 }
