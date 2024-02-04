37f72ea01443a263d27247982b269b52e0801277
kuqmua <serezhashmakov@outlook.com>
2024-02-04 23:07:53 +0300

    next steps

--- postgresql_crud/generate_postgresql_crud/src/check_for_none.rs
+++ postgresql_crud/generate_postgresql_crud/src/check_for_none.rs
@@ -36,10 +36,18 @@ pub fn check_for_none(
     .unwrap_or_else(|_| panic!("{proc_macro_name_upper_camel_case_ident_stringified} {none_elements} {}", proc_macro_common::global_variables::hardcode::PARSE_PROC_MACRO2_TOKEN_STREAM_FAILED_MESSAGE));
     let match_elements_token_stream = match_elements.parse::<proc_macro2::TokenStream>()
     .unwrap_or_else(|_| panic!("{proc_macro_name_upper_camel_case_ident_stringified} {match_elements} {}", proc_macro_common::global_variables::hardcode::PARSE_PROC_MACRO2_TOKEN_STREAM_FAILED_MESSAGE));
-    let response_variant_token_stream = quote::quote!{
-        NoPayloadFields {
-            no_payload_fields: std::string::String::from("no payload fields"), 
-            code_occurence: crate::code_occurence!()
+    let response_variant_token_stream = {
+        let field_code_occurence_new_23fdf468_0468_4c5c_8670_08f6f747e417_token_stream = crate::generate_field_code_occurence_new_token_stream(
+            file!(),
+            line!(),
+            column!(),
+            &proc_macro_name_upper_camel_case_ident_stringified,
+        );
+        quote::quote!{
+            NoPayloadFields {
+                no_payload_fields: std::string::String::from("no payload fields"), 
+                #field_code_occurence_new_23fdf468_0468_4c5c_8670_08f6f747e417_token_stream
+            }
         }
     };
     quote::quote!{
