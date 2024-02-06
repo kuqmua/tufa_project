a2c40ecf022272eac141abd1698e6da451e5c72b
kuqmua <serezhashmakov@outlook.com>
2024-02-06 22:31:38 +0300

    next steps

--- proc_macro_helpers/src/generate_simple_syn_punctuated_punctuated.rs
+++ proc_macro_helpers/src/generate_simple_syn_punctuated_punctuated.rs
@@ -5,23 +5,25 @@ pub fn generate_simple_syn_punctuated_punctuated(
     let parts_vec_len = parts_vec.len();
     match parts_vec_len >= 1 {
         true => {
-            let parts_vec_len_minus_one = parts_vec_len - 1;
             let mut handle = syn::punctuated::Punctuated::<syn::PathSegment, syn::token::Colon2>::new();
-            for (index, element) in parts_vec.iter().enumerate() {
+            for element in parts_vec.iter().rev().skip(1).rev() {
                 handle.push_value(
                     syn::PathSegment {
                         ident: proc_macro2::Ident::new(element, proc_macro2::Span::call_site()),
                         arguments: syn::PathArguments::None,
                     }
                 );
-                match index < parts_vec_len_minus_one {
-                    true => {
-                        handle.push_punct(syn::token::Colon2{
-                            spans: [proc_macro2::Span::call_site(),proc_macro2::Span::call_site()],
-                        });
+                handle.push_punct(syn::token::Colon2{
+                    spans: [proc_macro2::Span::call_site(),proc_macro2::Span::call_site()],
+                });
+            }
+            if let Some(value) = parts_vec.last() {
+                handle.push_value(
+                    syn::PathSegment {
+                        ident: proc_macro2::Ident::new(value, proc_macro2::Span::call_site()),
+                        arguments: syn::PathArguments::None,
                     }
-                    false => ()
-                }
+                );
             }
             handle
         },
