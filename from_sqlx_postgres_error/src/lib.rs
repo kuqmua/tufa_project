0458817dce15a274de4f07d80f7d9b1a3810228b
kuqmua <serezhashmakov@outlook.com>
2024-02-06 19:14:28 +0300

    next steps

--- from_sqlx_postgres_error/src/lib.rs
+++ from_sqlx_postgres_error/src/lib.rs
@@ -16,6 +16,96 @@ pub fn from_sqlx_postgres_error(input: proc_macro::TokenStream) -> proc_macro::T
         column!(),
         &proc_macro_name_upper_camel_case_ident_stringified,
     );
+    let field_code_occurence_new_d5e60d03_63d8_44be_be72_86f900b51836_token_stream = proc_macro_helpers::generate_field_code_occurence_new_token_stream::generate_field_code_occurence_new_token_stream(
+        file!(),
+        line!(),
+        column!(),
+        &proc_macro_name_upper_camel_case_ident_stringified,
+    );
+    let field_code_occurence_new_7e40e82a_3043_41f3_974c_973a53d221de_token_stream = proc_macro_helpers::generate_field_code_occurence_new_token_stream::generate_field_code_occurence_new_token_stream(
+        file!(),
+        line!(),
+        column!(),
+        &proc_macro_name_upper_camel_case_ident_stringified,
+    );
+    let field_code_occurence_new_279a8e42_c552_42db_b137_c8acb9973f92_token_stream = proc_macro_helpers::generate_field_code_occurence_new_token_stream::generate_field_code_occurence_new_token_stream(
+        file!(),
+        line!(),
+        column!(),
+        &proc_macro_name_upper_camel_case_ident_stringified,
+    );
+    let field_code_occurence_new_e5c5b92a_40b4_429b_a29f_2e077ec23c04_token_stream = proc_macro_helpers::generate_field_code_occurence_new_token_stream::generate_field_code_occurence_new_token_stream(
+        file!(),
+        line!(),
+        column!(),
+        &proc_macro_name_upper_camel_case_ident_stringified,
+    );
+    let field_code_occurence_new_bd227744_1167_4a95_8a83_27ea50261d2d_token_stream = proc_macro_helpers::generate_field_code_occurence_new_token_stream::generate_field_code_occurence_new_token_stream(
+        file!(),
+        line!(),
+        column!(),
+        &proc_macro_name_upper_camel_case_ident_stringified,
+    );
+    let field_code_occurence_new_353cb4ca_0abb_4ba9_a78f_c89ea961a260_token_stream = proc_macro_helpers::generate_field_code_occurence_new_token_stream::generate_field_code_occurence_new_token_stream(
+        file!(),
+        line!(),
+        column!(),
+        &proc_macro_name_upper_camel_case_ident_stringified,
+    );
+    let field_code_occurence_new_d032743c_7ee4_4a40_b46c_6cc5d13dc8cb_token_stream = proc_macro_helpers::generate_field_code_occurence_new_token_stream::generate_field_code_occurence_new_token_stream(
+        file!(),
+        line!(),
+        column!(),
+        &proc_macro_name_upper_camel_case_ident_stringified,
+    );
+    let field_code_occurence_new_d3992828_c626_477d_b14a_9146037ab416_token_stream = proc_macro_helpers::generate_field_code_occurence_new_token_stream::generate_field_code_occurence_new_token_stream(
+        file!(),
+        line!(),
+        column!(),
+        &proc_macro_name_upper_camel_case_ident_stringified,
+    );
+    let field_code_occurence_new_81a0d2c3_5246_477c_8c67_10a5940c95c1_token_stream = proc_macro_helpers::generate_field_code_occurence_new_token_stream::generate_field_code_occurence_new_token_stream(
+        file!(),
+        line!(),
+        column!(),
+        &proc_macro_name_upper_camel_case_ident_stringified,
+    );
+    let field_code_occurence_new_89ccb64c_050b_45bb_ae7b_1ade4f90ada5_token_stream = proc_macro_helpers::generate_field_code_occurence_new_token_stream::generate_field_code_occurence_new_token_stream(
+        file!(),
+        line!(),
+        column!(),
+        &proc_macro_name_upper_camel_case_ident_stringified,
+    );
+    let field_code_occurence_new_e0ca61d3_f936_4414_a82c_8a87c60f02c5_token_stream = proc_macro_helpers::generate_field_code_occurence_new_token_stream::generate_field_code_occurence_new_token_stream(
+        file!(),
+        line!(),
+        column!(),
+        &proc_macro_name_upper_camel_case_ident_stringified,
+    );
+    let field_code_occurence_new_f235ca0a_319b_411b_b6b4_a022261725ae_token_stream = proc_macro_helpers::generate_field_code_occurence_new_token_stream::generate_field_code_occurence_new_token_stream(
+        file!(),
+        line!(),
+        column!(),
+        &proc_macro_name_upper_camel_case_ident_stringified,
+    );
+    let field_code_occurence_new_75e32177_f3b7_46f3_81f3_f6f56e8f2308_token_stream = proc_macro_helpers::generate_field_code_occurence_new_token_stream::generate_field_code_occurence_new_token_stream(
+        file!(),
+        line!(),
+        column!(),
+        &proc_macro_name_upper_camel_case_ident_stringified,
+    );
+    let field_code_occurence_new_b281dabf_8137_4f23_ad97_daa8b2347670_token_stream = proc_macro_helpers::generate_field_code_occurence_new_token_stream::generate_field_code_occurence_new_token_stream(
+        file!(),
+        line!(),
+        column!(),
+        &proc_macro_name_upper_camel_case_ident_stringified,
+    );
+    let field_code_occurence_new_24cd6c76_3a1f_42bb_9769_fb8d1b3e93f9_token_stream = proc_macro_helpers::generate_field_code_occurence_new_token_stream::generate_field_code_occurence_new_token_stream(
+        file!(),
+        line!(),
+        column!(),
+        &proc_macro_name_upper_camel_case_ident_stringified,
+    );
     let gen = quote::quote! {
         impl From<sqlx::Error> for #ident {
             fn from(val: sqlx::Error) -> Self {
@@ -29,72 +119,72 @@ pub fn from_sqlx_postgres_error(input: proc_macro::TokenStream) -> proc_macro::T
                     sqlx::Error::Database(database) => {
                         Self::Database {
                             database: database.message().to_string(),
-                            code_occurence: crate::code_occurence!(),
+                            #field_code_occurence_new_d5e60d03_63d8_44be_be72_86f900b51836_token_stream,
                         }
                     }
                     sqlx::Error::Io(io) => Self::Io {
                         io,
-                        code_occurence: crate::code_occurence!(),
+                        #field_code_occurence_new_7e40e82a_3043_41f3_974c_973a53d221de_token_stream,
                     },
                     sqlx::Error::Tls(value) => Self::Tls {
                         tls: value.to_string(),
-                        code_occurence: crate::code_occurence!(),
+                        #field_code_occurence_new_279a8e42_c552_42db_b137_c8acb9973f92_token_stream,
                     },
                     sqlx::Error::Protocol(string) => Self::Protocol {
                         protocol: string,
-                        code_occurence: crate::code_occurence!(),
+                        #field_code_occurence_new_e5c5b92a_40b4_429b_a29f_2e077ec23c04_token_stream,
                     },
                     sqlx::Error::RowNotFound => Self::RowNotFound {
-                        row_not_found: std::string::String::from("row_not_foundcrrrr"),
-                        code_occurence: crate::code_occurence!(),
+                        row_not_found: std::string::String::from("row_not_found"),
+                        #field_code_occurence_new_bd227744_1167_4a95_8a83_27ea50261d2d_token_stream,
                     },
                     sqlx::Error::TypeNotFound { type_name } => Self::TypeNotFound {
                         type_not_found: type_name,
-                        code_occurence: crate::code_occurence!(),
+                        #field_code_occurence_new_353cb4ca_0abb_4ba9_a78f_c89ea961a260_token_stream,
                     },
                     sqlx::Error::ColumnIndexOutOfBounds { index, len } => {
                         Self::ColumnIndexOutOfBounds {
                             column_index_out_of_bounds: index,
                             len,
-                            code_occurence: crate::code_occurence!(),
+                            #field_code_occurence_new_d032743c_7ee4_4a40_b46c_6cc5d13dc8cb_token_stream,
                         }
                     }
                     sqlx::Error::ColumnNotFound(column_not_found) => {
                         Self::ColumnNotFound {
                             column_not_found,
-                            code_occurence: crate::code_occurence!(),
+                            #field_code_occurence_new_d3992828_c626_477d_b14a_9146037ab416_token_stream,
                         }
                     }
                     sqlx::Error::ColumnDecode { index, source } => {
                         Self::ColumnDecode {
                             column_decode_index: index,
                             source_handle: source.to_string(),
-                            code_occurence: crate::code_occurence!(),
+                            #field_code_occurence_new_81a0d2c3_5246_477c_8c67_10a5940c95c1_token_stream,
                         }
                     }
                     sqlx::Error::Decode(value) => Self::Decode {
                         decode: value.to_string(),
-                        code_occurence: crate::code_occurence!(),
+                        #field_code_occurence_new_89ccb64c_050b_45bb_ae7b_1ade4f90ada5_token_stream,
                     },
                     sqlx::Error::PoolTimedOut => Self::PoolTimedOut {
                         pool_timed_out: std::string::String::from("pool timed out"),
-                        code_occurence: crate::code_occurence!(),
+                        #field_code_occurence_new_e0ca61d3_f936_4414_a82c_8a87c60f02c5_token_stream,
                     },
                     sqlx::Error::PoolClosed => Self::PoolClosed {
                         pool_closed: std::string::String::from("pool closed"),
-                        code_occurence: crate::code_occurence!(),
+                        #field_code_occurence_new_f235ca0a_319b_411b_b6b4_a022261725ae_token_stream,
                     },
                     sqlx::Error::WorkerCrashed => Self::WorkerCrashed {
                         worker_crashed: std::string::String::from("worker crashed"),
-                        code_occurence: crate::code_occurence!(),
+                        #field_code_occurence_new_75e32177_f3b7_46f3_81f3_f6f56e8f2308_token_stream,
                     },
                     sqlx::Error::Migrate(migrate_error) => Self::Migrate {
                         migrate: *migrate_error,
-                        code_occurence: crate::code_occurence!(),
+                        #field_code_occurence_new_b281dabf_8137_4f23_ad97_daa8b2347670_token_stream,
                     },
                     _ => Self::UnexpectedCase {
                         unexpected_case: std::string::String::from("unexpected_case"),
-                        code_occurence: crate::code_occurence!(),
+                        #field_code_occurence_new_24cd6c76_3a1f_42bb_9769_fb8d1b3e93f9_token_stream,
                     },
                 }
             }
