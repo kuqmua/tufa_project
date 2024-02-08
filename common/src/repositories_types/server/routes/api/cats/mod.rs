962dd2e2e5e915e17a3d949452e776668e63ff21
kuqmua <serezhashmakov@outlook.com>
2024-02-08 17:23:03 +0300

    next stes

--- common/src/repositories_types/server/routes/api/cats/mod.rs
+++ common/src/repositories_types/server/routes/api/cats/mod.rs
@@ -985,128 +985,95 @@ async fn tvfrr_extraction_logic_try_delete_one<'a>(
     match future.await
     {
         Ok(response) => {
-            //
-            let match_value = {
             // try_from_response_try_delete_one(response).await
-            //
-            let status_code = response.status();
-            let headers = response.headers().clone();
-            match response.text().await {
-                Ok(response_text) => {
-                    if status_code == http :: StatusCode :: OK {
-                        match serde_json :: from_str :: <
-                        TryDeleteOneResponseVariantsTvfrr200Ok > (& response_text)
-                        {
-                            Ok(value) => {
-                                Ok(TryDeleteOneResponseVariants :: from(value))
+            let match_value = {
+                let status_code = response.status();
+                let headers = response.headers().clone();
+                match response.text().await {
+                    Ok(response_text) => {
+                        if status_code == http::StatusCode::OK {
+                            match serde_json::from_str::<TryDeleteOneResponseVariantsTvfrr200Ok>(&response_text) {
+                                Ok(value) => {
+                                    Ok(TryDeleteOneResponseVariants::from(value))
+                                    as 
+                                    Result<
+                                        TryDeleteOneResponseVariants,
+                                        crate::common::api_request_unexpected_error::ApiRequestUnexpectedError,
+                                    >
+                                    //
+                                    // match crate::server::postgres::uuid_wrapper::PossibleUuidWrapper::try_from(variants)
+                                    // {
+                                    //     Ok(value) => Ok(value), 
+                                    //     Err(e) => Err(TryDeleteOneRequestError :: ExpectedType
+                                    //     {
+                                    //         expected_type : e, code_occurence : error_occurence_lib ::
+                                    //         code_occurence :: CodeOccurence ::
+                                    //         new(crate :: global_variables :: compile_time ::
+                                    //         project_git_info :: PROJECT_GIT_INFO.commit.to_string(),
+                                    //         file! ().to_string(), line! (), column! (),
+                                    //         Some(error_occurence_lib :: code_occurence :: MacroOccurence
+                                    //         {
+                                    //             file : std :: string :: String ::
+                                    //             from("postgresql_crud/generate_postgresql_crud/src/type_variants_from_request_response_generator.rs"),
+                                    //             line : 882, column : 17,
+                                    //         })),
+                                    //     }),
+                                    // }
+                                    //
+                                }, 
+                                Err(e) => Err(
+                                    crate :: common :: api_request_unexpected_error ::ApiRequestUnexpectedError :: DeserializeBody{ serde : e, status_code, headers, response_text }
+                                )
                                 as 
                                 Result<
                                     TryDeleteOneResponseVariants,
                                     crate::common::api_request_unexpected_error::ApiRequestUnexpectedError,
-                                >
-                                //
-                                // match crate::server::postgres::uuid_wrapper::PossibleUuidWrapper::try_from(variants)
-                                // {
-                                //     Ok(value) => Ok(value), 
-                                //     Err(e) => Err(TryDeleteOneRequestError :: ExpectedType
-                                //     {
-                                //         expected_type : e, code_occurence : error_occurence_lib ::
-                                //         code_occurence :: CodeOccurence ::
-                                //         new(crate :: global_variables :: compile_time ::
-                                //         project_git_info :: PROJECT_GIT_INFO.commit.to_string(),
-                                //         file! ().to_string(), line! (), column! (),
-                                //         Some(error_occurence_lib :: code_occurence :: MacroOccurence
-                                //         {
-                                //             file : std :: string :: String ::
-                                //             from("postgresql_crud/generate_postgresql_crud/src/type_variants_from_request_response_generator.rs"),
-                                //             line : 882, column : 17,
-                                //         })),
-                                //     }),
-                                // }
-                                //
-                            }, 
-                            Err(e) => Err(
-                                crate :: common :: api_request_unexpected_error ::ApiRequestUnexpectedError :: DeserializeBody{ serde : e, status_code, headers, response_text }
-                            )
-                            
-                                                            as 
-                                Result<
-                                    TryDeleteOneResponseVariants,
-                                    crate::common::api_request_unexpected_error::ApiRequestUnexpectedError,
-                                >
-                            ,
-                        }
-                    } else if status_code == http :: StatusCode ::INTERNAL_SERVER_ERROR
-                    {
-                        match serde_json :: from_str :: <
-                        TryDeleteOneResponseVariantsTvfrr500InternalServerError >
-                        (& response_text)
-                        {
-                            Ok(value) => 
-                                Ok(TryDeleteOneResponseVariants :: from(value))
-                                                            as 
-                                Result<
-                                    TryDeleteOneResponseVariants,
-                                    crate::common::api_request_unexpected_error::ApiRequestUnexpectedError,
-                                >    
-                            , 
-                            Err(e) => 
-                            
-                                Err(crate :: common :: api_request_unexpected_error ::ApiRequestUnexpectedError :: DeserializeBody{ serde : e, status_code, headers, response_text })
-                                
-                                                            as 
-                                Result<
-                                    TryDeleteOneResponseVariants,
-                                    crate::common::api_request_unexpected_error::ApiRequestUnexpectedError,
-                                >    
-                            ,
-                        }
-                    } else if status_code == http :: StatusCode :: BAD_REQUEST
-                    {
-                        match serde_json :: from_str :: <
-                        TryDeleteOneResponseVariantsTvfrr400BadRequest >
-                        (& response_text)
-                        {
-                            Ok(value) => 
-                                Ok(TryDeleteOneResponseVariants :: from(value))
-                                                                as 
-                                Result<
-                                    TryDeleteOneResponseVariants,
-                                    crate::common::api_request_unexpected_error::ApiRequestUnexpectedError,
-                                >
-                                , 
-                            Err(e) => 
-                                Err(crate :: common :: api_request_unexpected_error ::ApiRequestUnexpectedError :: DeserializeBody{ serde : e, status_code, headers, response_text })
-                                                                as 
-                                Result<
-                                    TryDeleteOneResponseVariants,
-                                    crate::common::api_request_unexpected_error::ApiRequestUnexpectedError,
-                                >
-                                ,
-                        }
-                    } else if status_code == http :: StatusCode :: NOT_FOUND
-                    {
-                        match serde_json :: from_str :: <
-                        TryDeleteOneResponseVariantsTvfrr404NotFound >
-                        (& response_text)
-                        {
-                            Ok(value) =>
-                            Ok(TryDeleteOneResponseVariants :: from(value))
-                                                            as 
-                                Result<
-                                    TryDeleteOneResponseVariants,
-                                    crate::common::api_request_unexpected_error::ApiRequestUnexpectedError,
-                                >
-                            , Err(e) =>
-                            Err(crate :: common :: api_request_unexpected_error ::
-                            ApiRequestUnexpectedError :: DeserializeBody
-                            { serde : e, status_code, headers, response_text })
-                                                            as 
-                                Result<
-                                    TryDeleteOneResponseVariants,
-                                    crate::common::api_request_unexpected_error::ApiRequestUnexpectedError,
-                                >
-                            ,
+                                >,
+                            }
+                        } else if status_code == http::StatusCode::INTERNAL_SERVER_ERROR {
+                            match serde_json::from_str::<TryDeleteOneResponseVariantsTvfrr500InternalServerError>(&response_text) {
+                                Ok(value) => Ok(TryDeleteOneResponseVariants :: from(value))
+                                    as 
+                                    Result<
+                                        TryDeleteOneResponseVariants,
+                                        crate::common::api_request_unexpected_error::ApiRequestUnexpectedError,
+                                    >, 
+                                Err(e) => Err(crate::common::api_request_unexpected_error::ApiRequestUnexpectedError::DeserializeBody{ serde: e, status_code, headers, response_text })
+                                    as 
+                                    Result<
+                                        TryDeleteOneResponseVariants,
+                                        crate::common::api_request_unexpected_error::ApiRequestUnexpectedError,
+                                    >,
+                            }
+                        } else if status_code == http::StatusCode::BAD_REQUEST {
+                            match serde_json::from_str::<TryDeleteOneResponseVariantsTvfrr400BadRequest>(&response_text) {
+                                Ok(value) => Ok(TryDeleteOneResponseVariants :: from(value))
+                                    as 
+                                    Result<
+                                        TryDeleteOneResponseVariants,
+                                        crate::common::api_request_unexpected_error::ApiRequestUnexpectedError,
+                                    >, 
+                                Err(e) => Err(crate :: common :: api_request_unexpected_error ::ApiRequestUnexpectedError :: DeserializeBody{ serde : e, status_code, headers, response_text })
+                                    as 
+                                    Result<
+                                        TryDeleteOneResponseVariants,
+                                        crate::common::api_request_unexpected_error::ApiRequestUnexpectedError,
+                                    >,
+                            }
+                        } else if status_code == http::StatusCode::NOT_FOUND {
+                            match serde_json::from_str::<TryDeleteOneResponseVariantsTvfrr404NotFound>(&response_text) {
+                                Ok(value) => Ok(TryDeleteOneResponseVariants::from(value))
+                                    as 
+                                    Result<
+                                        TryDeleteOneResponseVariants,
+                                        crate::common::api_request_unexpected_error::ApiRequestUnexpectedError,
+                                    >, 
+                                Err(e) => Err(crate::common::api_request_unexpected_error::ApiRequestUnexpectedError::DeserializeBody{ serde: e, status_code, headers, response_text })
+                                    as 
+                                    Result<
+                                        TryDeleteOneResponseVariants,
+                                        crate::common::api_request_unexpected_error::ApiRequestUnexpectedError,
+                                    >,
                         }
                     } else
                     {
