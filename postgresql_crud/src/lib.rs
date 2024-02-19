cf4da7460907067907d0fe131e782bc31e01a57b
kuqmua <serezhashmakov@outlook.com>
2024-02-19 20:48:59 +0300

    next steps

--- postgresql_crud/src/lib.rs
+++ postgresql_crud/src/lib.rs
@@ -2854,8 +2854,23 @@ pub fn test_check_supported_postgresql_column_type() {
     let sqlx_types_chrono_naive_date = SqlxTypesChronoNaiveDate(
         sqlx::types::chrono::NaiveDate::from_ymd_opt(2016, 11, 3).unwrap(),
     );
-    // let sqlx_types_chrono_naive_time = SqlxTypesChronoNaiveTime(pub sqlx::types::chrono::NaiveTime);
-    // let sqlx_postgres_types_pg_time_tz = SqlxPostgresTypesPgTimeTz(pub sqlx::postgres::types::PgTimeTz);
+    let sqlx_types_chrono_naive_time =
+        SqlxTypesChronoNaiveTime(sqlx::types::chrono::NaiveTime::from_hms_opt(10, 10, 10).unwrap());
+    let sqlx_postgres_types_pg_time_tz =
+        SqlxPostgresTypesPgTimeTz(sqlx::postgres::types::PgTimeTz {
+            time: sqlx::types::time::Time::from_hms(
+                std::primitive::u8::default(),
+                std::primitive::u8::default(),
+                std::primitive::u8::default(),
+            )
+            .unwrap(),
+            offset: sqlx::types::time::UtcOffset::from_hms(
+                std::primitive::i8::default(),
+                std::primitive::i8::default(),
+                std::primitive::i8::default(),
+            )
+            .unwrap(),
+        });
     // let sqlx_types_time_primitive_date_time = SqlxTypesTimePrimitiveDateTime(pub sqlx::types::time::PrimitiveDateTime);
     // let sqlx_types_time_offset_date_time = SqlxTypesTimeOffsetDateTime(pub sqlx::types::time::OffsetDateTime);
     // let sqlx_types_time_date = SqlxTypesTimeDate(pub sqlx::types::time::Date);
