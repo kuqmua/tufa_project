85c7cc000b0a7469cf538173942fb1c75006c38d
kuqmua <serezhashmakov@outlook.com>
2024-03-06 20:30:26 +0300

    next steps

--- postgresql_crud/generate_postgresql_crud/src/lib.rs
+++ postgresql_crud/generate_postgresql_crud/src/lib.rs
@@ -407,15 +407,10 @@ pub fn generate_postgresql_crud(input: proc_macro::TokenStream) -> proc_macro::T
             let field_vis = &element.field.vis;
             let field_ident = &element.field.ident;
             let field_type_path = &element.field.ty;
-            //todo json generic
-            let inner_type_with_serialize_deserialize_token_stream = {
-                let inner_type_with_serialize_deserialize_stringified = &element.rust_sqlx_map_to_postgres_type_variant.get_inner_type_with_serialize_deserialize_stringified("");
-                inner_type_with_serialize_deserialize_stringified.parse::<proc_macro2::TokenStream>()
-                .unwrap_or_else(|_| panic!("{proc_macro_name_upper_camel_case_ident_stringified} {inner_type_with_serialize_deserialize_stringified} {}", proc_macro_common::global_variables::hardcode::PARSE_PROC_MACRO2_TOKEN_STREAM_FAILED_MESSAGE))
-            };
+            let inner_type_with_serialize_deserialize_token_stream = &element.inner_type_with_serialize_deserialize_token_stream;
             quote::quote!{
                 #serde_skip_serializing_if_value_attribute_token_stream
-                #field_vis #field_ident: std::option::Option<#inner_type_with_serialize_deserialize_token_stream>//#field_type_path
+                #field_vis #field_ident: std::option::Option<#inner_type_with_serialize_deserialize_token_stream>
             }
         });
         quote::quote! {
