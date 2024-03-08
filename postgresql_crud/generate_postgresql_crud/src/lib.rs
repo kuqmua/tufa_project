b78036cf657956293d7ff011b2db2ec9e6dcfa68
kuqmua <serezhashmakov@outlook.com>
2024-03-08 13:38:57 +0300

    next steps

--- postgresql_crud/generate_postgresql_crud/src/lib.rs
+++ postgresql_crud/generate_postgresql_crud/src/lib.rs
@@ -7342,9 +7342,9 @@ pub fn generate_postgresql_crud(input: proc_macro::TokenStream) -> proc_macro::T
     let gen = quote::quote! {
         //comment out coz its impossible to correctly generate tokens
         // pub mod #mod_name_snake_case_token_stream {/
-            #common_token_stream
+            // #common_token_stream
 
-            #create_many_token_stream
+            // #create_many_token_stream
             // #create_one_token_stream
             // #read_many_token_stream
             // #read_one_token_stream
