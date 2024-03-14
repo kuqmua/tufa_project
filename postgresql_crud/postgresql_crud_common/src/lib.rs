5d49d67c6e4a8933d012e1689f099d1f641ac999
kuqmua <serezhashmakov@outlook.com>
2024-03-14 22:37:12 +0300

    next steps

--- postgresql_crud/postgresql_crud_common/src/lib.rs
+++ postgresql_crud/postgresql_crud_common/src/lib.rs
@@ -10136,50 +10136,49 @@ pub trait BindQuery {
         query: sqlx::query::Query<sqlx::Postgres, sqlx::postgres::PgArguments>,
     ) -> sqlx::query::Query<sqlx::Postgres, sqlx::postgres::PgArguments>;
 }
-// impl crate::server::postgres::bind_query::BindQuery for std::string::String {
-//     fn try_increment(
-//         &self,
-//         increment: &mut u64,
-//     ) -> Result<(), TryGenerateBindIncrementsErrorNamed> {
-//         match increment.checked_add(1) {
-//             Some(incr) => {
-//                 *increment = incr;
-//                 Ok(())
-//             }
-//             None => Err(crate::server::postgres::bind_query::TryGenerateBindIncrementsErrorNamed::CheckedAdd {
-//                 checked_add: std::string::String::from("checked_add is None"),
-//                 code_occurence: error_occurence_lib::code_occurence!(),
-//             })
-//         }
-//     }
-//     fn try_generate_bind_increments(
-//         &self,
-//         increment: &mut u64,
-//     ) -> Result<
-//         std::string::String,
-//         crate::server::postgres::bind_query::TryGenerateBindIncrementsErrorNamed,
-//     > {
-//         let mut increments = std::string::String::default();
-//         match increment.checked_add(1) {
-//             Some(incr) => {
-//                 *increment = incr;
-//                 increments.push_str(&format!("${increment}"));
-//             }
-//             None => {
-//                 return Err(crate::server::postgres::bind_query::TryGenerateBindIncrementsErrorNamed::CheckedAdd {
-//                     checked_add: std::string::String::from("checked_add is None"),
-//                     code_occurence: error_occurence_lib::code_occurence!(),
-//                 });
-//             }
-//         }
-//         Ok(increments)
-//     }
-//     fn bind_value_to_query(
-//         self,
-//         mut query: sqlx::query::Query<sqlx::Postgres, sqlx::postgres::PgArguments>,
-//     ) -> sqlx::query::Query<sqlx::Postgres, sqlx::postgres::PgArguments> {
-//         query = query.bind(self);
-//         query
-//     }
-// }
-//
+impl BindQuery for StdStringString {
+    fn try_increment(
+        &self,
+        increment: &mut u64,
+    ) -> Result<(), TryGenerateBindIncrementsErrorNamed> {
+        match increment.checked_add(1) {
+            Some(incr) => {
+                *increment = incr;
+                Ok(())
+            }
+            None => Err(TryGenerateBindIncrementsErrorNamed::CheckedAdd {
+                checked_add: std::string::String::from("checked_add is None"),
+                code_occurence: error_occurence_lib::code_occurence!(),
+            })
+        }
+    }
+    fn try_generate_bind_increments(
+        &self,
+        increment: &mut u64,
+    ) -> Result<
+        std::string::String,
+        TryGenerateBindIncrementsErrorNamed,
+    > {
+        let mut increments = std::string::String::default();
+        match increment.checked_add(1) {
+            Some(incr) => {
+                *increment = incr;
+                increments.push_str(&format!("${increment}"));
+            }
+            None => {
+                return Err(TryGenerateBindIncrementsErrorNamed::CheckedAdd {
+                    checked_add: std::string::String::from("checked_add is None"),
+                    code_occurence: error_occurence_lib::code_occurence!(),
+                });
+            }
+        }
+        Ok(increments)
+    }
+    fn bind_value_to_query(
+        self,
+        mut query: sqlx::query::Query<sqlx::Postgres, sqlx::postgres::PgArguments>,
+    ) -> sqlx::query::Query<sqlx::Postgres, sqlx::postgres::PgArguments> {
+        query = query.bind(self.0);
+        query
+    }
+}
