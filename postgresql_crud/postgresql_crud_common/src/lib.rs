eb0b2c67c35e9dde953c246c7390c7f490e4e400
kuqmua <serezhashmakov@outlook.com>
2024-03-19 00:10:35 +0300

    next steps

--- postgresql_crud/postgresql_crud_common/src/lib.rs
+++ postgresql_crud/postgresql_crud_common/src/lib.rs
@@ -10110,107 +10110,10 @@ impl<T> SqlxTypesJson<T> {
 //     }
 // }
 
-#[derive(Debug, PartialEq, bind_query::BindQueryForRustSqlxPostgresqlWrapperType)]
+#[derive(Debug, PartialEq, bind_query::PostgresqlTypeLogicUsingFromSerializeDeserialize)]
 pub struct SerdeJsonValue(pub serde_json::Value);
-#[derive(Debug, PartialEq, serde::Serialize, serde::Deserialize, utoipa::ToSchema)]
-pub struct SerdeJsonValueWithSerializeDeserialize(serde_json::Value);
-impl std::convert::From<SerdeJsonValueWithSerializeDeserialize> for SerdeJsonValue {
-    fn from(value: SerdeJsonValueWithSerializeDeserialize) -> Self {
-        Self(value.0)
-    }
-}
-impl std::convert::From<SerdeJsonValue> for SerdeJsonValueWithSerializeDeserialize {
-    fn from(value: SerdeJsonValue) -> Self {
-        Self(value.0)
-    }
-}
-impl SerdeJsonValue {
-    pub fn into_inner(self) -> serde_json::Value {
-        self.0
-    }
-}
-impl std::convert::From<SerdeJsonValue> for serde_json::Value {
-    fn from(value: SerdeJsonValue) -> Self {
-        value.0
-    }
-}
-impl sqlx::Type<sqlx::Postgres> for SerdeJsonValue {
-    fn type_info() -> <sqlx::Postgres as sqlx::Database>::TypeInfo {
-        <serde_json::Value as sqlx::Type<sqlx::Postgres>>::type_info()
-    }
-    fn compatible(ty: &<sqlx::Postgres as sqlx::Database>::TypeInfo) -> std::primitive::bool {
-        <serde_json::Value as sqlx::Type<sqlx::Postgres>>::compatible(ty)
-    }
-}
-// impl sqlx::Encode<'_, sqlx::Postgres> for SerdeJsonValue {
-//     fn encode_by_ref(&self, buf: &mut sqlx::postgres::PgArgumentBuffer) -> sqlx::encode::IsNull {
-//         sqlx::Encode::<sqlx::Postgres>::encode_by_ref(&self.0, buf)
-//     }
-//     fn encode(
-//         self,
-//         buf: &mut <sqlx::Postgres as sqlx::database::HasArguments<'_>>::ArgumentBuffer,
-//     ) -> sqlx::encode::IsNull
-//     where
-//         Self: Sized,
-//     {
-//         sqlx::Encode::<sqlx::Postgres>::encode(self.0, buf)
-//     }
-//     fn produces(&self) -> Option<<sqlx::Postgres as sqlx::Database>::TypeInfo> {
-//         sqlx::Encode::<sqlx::Postgres>::produces(&self.0)
-//     }
-//     fn size_hint(&self) -> std::primitive::usize {
-//         sqlx::Encode::<sqlx::Postgres>::size_hint(&self.0)
-//     }
-// }
-// impl sqlx::Decode<'_, sqlx::Postgres> for SerdeJsonValue {
-//     fn decode(value: sqlx::postgres::PgValueRef<'_>) -> Result<Self, sqlx::error::BoxDynError> {
-//         match sqlx::Decode::<sqlx::Postgres>::decode(value) {
-//             Ok(value) => Ok(Self(value)),
-//             Err(e) => Err(e),
-//         }
-//     }
-// }
-impl CheckSupportedPostgresqlColumnType for SerdeJsonValue {
-    fn check_supported_postgresql_column_type() {}
-}
 impl AsPostgresqlJson for SerdeJsonValue {}
 impl AsPostgresqlJsonB for SerdeJsonValue {}
-impl std::convert::From<SerdeJsonValue> for SupportedSqlxPostgresType {
-    fn from(_value: SerdeJsonValue) -> Self {
-        SupportedSqlxPostgresType::SerdeJsonValue
-    }
-}
-impl SerdeJsonValue {
-    pub fn into_inner_type_vec(value: std::vec::Vec<Self>) -> std::vec::Vec<serde_json::Value> {
-        value
-            .into_iter()
-            .map(|element| element.into_inner())
-            .collect()
-    }
-}
-// impl std::convert::From<> for {
-//     fn from(value: ) -> Self {
-//         value.0
-//     }
-// }
-#[derive(Debug, PartialEq, bind_query::BindQueryForWhere)]
-pub struct WhereSerdeJsonValue {
-    pub value: SerdeJsonValue,
-    pub conjuctive_operator: ConjunctiveOperator,
-}
-#[derive(Debug, PartialEq, serde::Serialize, serde::Deserialize)]
-pub struct WhereSerdeJsonValueWithSerializeDeserialize {
-    pub value: SerdeJsonValueWithSerializeDeserialize,
-    pub conjuctive_operator: ConjunctiveOperator,
-}
-impl std::convert::From<WhereSerdeJsonValueWithSerializeDeserialize> for WhereSerdeJsonValue {
-    fn from(value: WhereSerdeJsonValueWithSerializeDeserialize) -> Self {
-        Self {
-            value: SerdeJsonValue::from(value.value),
-            conjuctive_operator: value.conjuctive_operator
-        }
-    }
-}
 
 pub async fn something() {
     // let mut query = sqlx::query::<sqlx::Postgres>("test");
