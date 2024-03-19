17a9780f03257df6beb594b13521a42cd648baf2
kuqmua <serezhashmakov@outlook.com>
2024-03-19 20:36:11 +0300

    next steps

--- postgresql_crud/postgresql_crud_common/bind_query/src/lib.rs
+++ postgresql_crud/postgresql_crud_common/bind_query/src/lib.rs
@@ -1,117 +1,117 @@
-#[proc_macro_derive(BindQueryForRustSqlxPostgresqlWrapperType)] //todo check on postgresql max length value of type
-pub fn bind_query_for_rust_sqlx_postgresql_wrapper_type(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
-    //todo in few cases rows affected is usefull. (update delete for example). if 0 afftected -maybe its error? or maybe use select then update\delete?(rewrite query)
-    proc_macro_common::panic_location::panic_location();
-    let proc_macro_name_upper_camel_case = "BindQuery";
-    let proc_macro_name_snake_case = proc_macro_common::naming_conventions::ToSnakeCaseStringified::to_snake_case_stringified(
-        &proc_macro_name_upper_camel_case,
-    );
-    let ast: syn::DeriveInput = syn::parse(input).unwrap_or_else(|e| {
-        panic!(
-            "{proc_macro_name_upper_camel_case} {}: {e}",
-            proc_macro_common::global_variables::hardcode::AST_PARSE_FAILED
-        )
-    });
-    // println!("{:#?}", ast.data);
-    let ident = &ast.ident;
-    let gen = quote::quote!{
-        impl BindQuery for #ident {
-            fn try_increment(&self, increment: &mut u64) -> Result<(), TryGenerateBindIncrementsErrorNamed> {
-                match increment.checked_add(1) {
-                    Some(incr) => {
-                        *increment = incr;
-                        Ok(())
-                    }
-                    None => Err(TryGenerateBindIncrementsErrorNamed::CheckedAdd {
-                        checked_add: std::string::String::from(CHECKED_ADD_IS_NONE),
-                        code_occurence: error_occurence_lib::code_occurence!(),
-                    })
-                }
-            }
-            fn try_generate_bind_increments(&self, increment: &mut u64) -> Result<std::string::String, TryGenerateBindIncrementsErrorNamed> {
-                let mut increments = std::string::String::default();
-                match increment.checked_add(1) {
-                    Some(incr) => {
-                        *increment = incr;
-                        increments.push_str(&format!("${increment}"));
-                    }
-                    None => {
-                        return Err(TryGenerateBindIncrementsErrorNamed::CheckedAdd {
-                            checked_add: std::string::String::from(CHECKED_ADD_IS_NONE),
-                            code_occurence: error_occurence_lib::code_occurence!(),
-                        });
-                    }
-                }
-                Ok(increments)
-            }
-            fn bind_value_to_query(self, mut query: sqlx::query::Query<sqlx::Postgres, sqlx::postgres::PgArguments>) -> sqlx::query::Query<sqlx::Postgres, sqlx::postgres::PgArguments> {
-                query = query.bind(self.0);
-                query
-            }
-        }
-    };
-    gen.into()
-}
+// #[proc_macro_derive(BindQueryForRustSqlxPostgresqlWrapperType)] //todo check on postgresql max length value of type
+// pub fn bind_query_for_rust_sqlx_postgresql_wrapper_type(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+//     //todo in few cases rows affected is usefull. (update delete for example). if 0 afftected -maybe its error? or maybe use select then update\delete?(rewrite query)
+//     proc_macro_common::panic_location::panic_location();
+//     let proc_macro_name_upper_camel_case = "BindQuery";
+//     let proc_macro_name_snake_case = proc_macro_common::naming_conventions::ToSnakeCaseStringified::to_snake_case_stringified(
+//         &proc_macro_name_upper_camel_case,
+//     );
+//     let ast: syn::DeriveInput = syn::parse(input).unwrap_or_else(|e| {
+//         panic!(
+//             "{proc_macro_name_upper_camel_case} {}: {e}",
+//             proc_macro_common::global_variables::hardcode::AST_PARSE_FAILED
+//         )
+//     });
+//     // println!("{:#?}", ast.data);
+//     let ident = &ast.ident;
+//     let gen = quote::quote!{
+//         impl BindQuery for #ident {
+//             fn try_increment(&self, increment: &mut u64) -> Result<(), TryGenerateBindIncrementsErrorNamed> {
+//                 match increment.checked_add(1) {
+//                     Some(incr) => {
+//                         *increment = incr;
+//                         Ok(())
+//                     }
+//                     None => Err(TryGenerateBindIncrementsErrorNamed::CheckedAdd {
+//                         checked_add: std::string::String::from(CHECKED_ADD_IS_NONE),
+//                         code_occurence: error_occurence_lib::code_occurence!(),
+//                     })
+//                 }
+//             }
+//             fn try_generate_bind_increments(&self, increment: &mut u64) -> Result<std::string::String, TryGenerateBindIncrementsErrorNamed> {
+//                 let mut increments = std::string::String::default();
+//                 match increment.checked_add(1) {
+//                     Some(incr) => {
+//                         *increment = incr;
+//                         increments.push_str(&format!("${increment}"));
+//                     }
+//                     None => {
+//                         return Err(TryGenerateBindIncrementsErrorNamed::CheckedAdd {
+//                             checked_add: std::string::String::from(CHECKED_ADD_IS_NONE),
+//                             code_occurence: error_occurence_lib::code_occurence!(),
+//                         });
+//                     }
+//                 }
+//                 Ok(increments)
+//             }
+//             fn bind_value_to_query(self, mut query: sqlx::query::Query<sqlx::Postgres, sqlx::postgres::PgArguments>) -> sqlx::query::Query<sqlx::Postgres, sqlx::postgres::PgArguments> {
+//                 query = query.bind(self.0);
+//                 query
+//             }
+//         }
+//     };
+//     gen.into()
+// }
 
-#[proc_macro_derive(BindQueryForWhere)]
-pub fn bind_query_for_where(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
-    //todo in few cases rows affected is usefull. (update delete for example). if 0 afftected -maybe its error? or maybe use select then update\delete?(rewrite query)
-    proc_macro_common::panic_location::panic_location();
-    let proc_macro_name_upper_camel_case = "BindQueryForWhere";
-    let proc_macro_name_snake_case = proc_macro_common::naming_conventions::ToSnakeCaseStringified::to_snake_case_stringified(
-        &proc_macro_name_upper_camel_case,
-    );
-    let ast: syn::DeriveInput = syn::parse(input).unwrap_or_else(|e| {
-        panic!(
-            "{proc_macro_name_upper_camel_case} {}: {e}",
-            proc_macro_common::global_variables::hardcode::AST_PARSE_FAILED
-        )
-    });
-    // println!("{:#?}", ast.data);
-    let ident = &ast.ident;
-    let gen = quote::quote!{
-        impl BindQuery for #ident {
-            fn try_increment(&self, increment: &mut std::primitive::u64) -> Result<(), TryGenerateBindIncrementsErrorNamed> {
-                match increment.checked_add(1) {
-                    Some(incr) => {
-                        *increment = incr;
-                        Ok(())
-                    },
-                    None => Err(TryGenerateBindIncrementsErrorNamed::CheckedAdd {
-                        checked_add: std::string::String::from("checked_add is None"),
-                        code_occurence: error_occurence_lib::code_occurence!(),
-                    }),
-                }
-            }
-            fn try_generate_bind_increments(&self, increment: &mut std::primitive::u64) -> Result<
-                std::string::String,
-                TryGenerateBindIncrementsErrorNamed,
-            > {
-                match increment.checked_add(1) {
-                    Some(incr) => {
-                        *increment = incr;
-                        Ok(format!("${increment}"))
-                    },
-                    None => Err(TryGenerateBindIncrementsErrorNamed::CheckedAdd {
-                        checked_add: std::string::String::from("checked_add is None"),
-                        code_occurence: error_occurence_lib::code_occurence!(),
-                    }),
-                }
-            }
-            fn bind_value_to_query(self, mut query: sqlx::query::Query<sqlx::Postgres, sqlx::postgres::PgArguments>) -> sqlx::query::Query<sqlx::Postgres, sqlx::postgres::PgArguments> {
-                query = query.bind(self.value.0);
-                query
-            }
-        }
-    };
-    gen.into()
-}
+// #[proc_macro_derive(BindQueryForWhere)]
+// pub fn bind_query_for_where(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+//     //todo in few cases rows affected is usefull. (update delete for example). if 0 afftected -maybe its error? or maybe use select then update\delete?(rewrite query)
+//     proc_macro_common::panic_location::panic_location();
+//     let proc_macro_name_upper_camel_case = "BindQueryForWhere";
+//     let proc_macro_name_snake_case = proc_macro_common::naming_conventions::ToSnakeCaseStringified::to_snake_case_stringified(
+//         &proc_macro_name_upper_camel_case,
+//     );
+//     let ast: syn::DeriveInput = syn::parse(input).unwrap_or_else(|e| {
+//         panic!(
+//             "{proc_macro_name_upper_camel_case} {}: {e}",
+//             proc_macro_common::global_variables::hardcode::AST_PARSE_FAILED
+//         )
+//     });
+//     // println!("{:#?}", ast.data);
+//     let ident = &ast.ident;
+//     let gen = quote::quote!{
+//         impl BindQuery for #ident {
+//             fn try_increment(&self, increment: &mut std::primitive::u64) -> Result<(), TryGenerateBindIncrementsErrorNamed> {
+//                 match increment.checked_add(1) {
+//                     Some(incr) => {
+//                         *increment = incr;
+//                         Ok(())
+//                     },
+//                     None => Err(TryGenerateBindIncrementsErrorNamed::CheckedAdd {
+//                         checked_add: std::string::String::from("checked_add is None"),
+//                         code_occurence: error_occurence_lib::code_occurence!(),
+//                     }),
+//                 }
+//             }
+//             fn try_generate_bind_increments(&self, increment: &mut std::primitive::u64) -> Result<
+//                 std::string::String,
+//                 TryGenerateBindIncrementsErrorNamed,
+//             > {
+//                 match increment.checked_add(1) {
+//                     Some(incr) => {
+//                         *increment = incr;
+//                         Ok(format!("${increment}"))
+//                     },
+//                     None => Err(TryGenerateBindIncrementsErrorNamed::CheckedAdd {
+//                         checked_add: std::string::String::from("checked_add is None"),
+//                         code_occurence: error_occurence_lib::code_occurence!(),
+//                     }),
+//                 }
+//             }
+//             fn bind_value_to_query(self, mut query: sqlx::query::Query<sqlx::Postgres, sqlx::postgres::PgArguments>) -> sqlx::query::Query<sqlx::Postgres, sqlx::postgres::PgArguments> {
+//                 query = query.bind(self.value.0);
+//                 query
+//             }
+//         }
+//     };
+//     gen.into()
+// }
 
-#[proc_macro_derive(PostgresqlTypeLogicUsingFromSerializeDeserialize)]
-pub fn std_convert_from_where_ident_serialize_deserialize_for_where_ident(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+#[proc_macro_derive(FieldTypeImplementsSerializeDeserialize)]
+pub fn field_type_implements_serialize_deserialize(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     //todo in few cases rows affected is usefull. (update delete for example). if 0 afftected -maybe its error? or maybe use select then update\delete?(rewrite query)
     proc_macro_common::panic_location::panic_location();
-    let proc_macro_name_upper_camel_case = "PostgresqlTypeLogicUsingFromSerializeDeserialize";
+    let proc_macro_name_upper_camel_case = "FieldTypeImplementsSerializeDeserialize";
     let proc_macro_name_snake_case = proc_macro_common::naming_conventions::ToSnakeCaseStringified::to_snake_case_stringified(
         &proc_macro_name_upper_camel_case,
     );
