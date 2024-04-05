bfefc06a142f876bb3a8a41061d68b51ca050a15
kuqmua <serezhashmakov@outlook.com>
2024-04-05 18:52:53 +0300

    nexts steps

--- common/src/repositories_types/server/routes/api/cats/mod.rs
+++ common/src/repositories_types/server/routes/api/cats/mod.rs
@@ -217,18 +217,18 @@ pub const TABLE_NAME: &str = "dogs";
 #[derive(Debug, serde :: Serialize, serde :: Deserialize)]
 pub struct DogOptions {
     //todo maybe wrap inner type into value coz double option problem
-    #[serde(skip_serializing_if = "Option::is_none")]
+    // #[serde(skip_serializing_if = "Option::is_none")]
     pub std_primitive_i64_as_postgresql_big_serial_not_null_primary_key:
         std::option::Option<postgresql_crud::Value<postgresql_crud::StdPrimitiveI64WithSerializeDeserialize>>,
-    #[serde(skip_serializing_if = "Option::is_none")]
+    // #[serde(skip_serializing_if = "Option::is_none")]
     pub std_primitive_bool_as_postgresql_bool: std::option::Option<
         postgresql_crud::Value<postgresql_crud::StdOptionOptionStdPrimitiveBoolWithSerializeDeserialize>,
     >,
-    #[serde(skip_serializing_if = "Option::is_none")]
+    // #[serde(skip_serializing_if = "Option::is_none")]
     pub std_primitive_i16_as_postgresql_small_int: std::option::Option<
         postgresql_crud::Value<postgresql_crud::StdOptionOptionStdPrimitiveI16WithSerializeDeserialize>,
     >,
-    #[serde(skip_serializing_if = "Option::is_none")]
+    // #[serde(skip_serializing_if = "Option::is_none")]
     pub std_primitive_i32_as_postgresql_int: std::option::Option<
         postgresql_crud::Value<postgresql_crud::StdOptionOptionStdPrimitiveI32WithSerializeDeserialize>,
     >,
@@ -1601,6 +1601,7 @@ impl crate::server::postgres::generate_query::GenerateQuery for std::vec::Vec<Do
 }
 //
 //modification
+#[derive(Debug)]
 struct WrapperVecColumn(std::vec::Vec<DogColumn>);
 //modification
 impl WrapperVecColumn {
