db6d718345ecc9f2782437af0b44f6f33876af44
kuqmua <serezhashmakov@outlook.com>
2024-04-13 15:55:52 +0300

    next steps

--- common/src/server/middleware/commit_checker.rs
+++ common/src/server/middleware/commit_checker.rs
@@ -26,29 +26,30 @@ pub enum CommitExtractorCheckErrorNamed {
     },
 }
 
-// pub async fn commit_checker_wrapper(
-//     app_state: CommitCheckerAppState,
-//     req: &axum::http::Request<axum::body::Body>,
-// ) -> Result<(), CommitExtractorCheckErrorNamed> {
-//     match app_state.get_enable_api_git_commit_check() {
-//         true => match req
-//             .headers()
-//             .get(<naming_constants::Commit as naming_constants::Naming>::snake_case_stringified())
-//             .and_then(|header| header.to_str().ok())
-//         {
-//             Some(commit_checker_header) => match commit_checker_header == git_info::PROJECT_GIT_INFO.commit {
-//                 true => Ok(()),
-//                 false => Err(CommitExtractorCheckErrorNamed::CommitExtractorNotEqual {
-//                     commit_not_equal: std::string::String::from("different project commit provided, services must work only with equal project commits"),
-//                     commit_to_use: crate::common::git::get_git_commit_link::GetGitCommitLink::get_git_commit_link(&git_info::PROJECT_GIT_INFO),
-//                     code_occurence: error_occurence_lib::code_occurence!(),
-//                 })
-//             },
-//             None => Err(CommitExtractorCheckErrorNamed::NoCommitExtractorHeader {
-//                 no_commit_header: std::string::String::from("commit header is not provided"),
-//                 code_occurence: error_occurence_lib::code_occurence!(),
-//             }),
-//         },
-//         false => Ok(()),
-//     }
-// }
+pub async fn commit_checker_wrapper(
+    app_state: CommitCheckerAppState,
+    headers: &http::header::HeaderMap<http::header::HeaderValue>,
+) -> Result<(), CommitExtractorCheckErrorNamed> {
+    // match app_state.get_enable_api_git_commit_check() {
+    //     true => match req
+    //         .headers()
+    //         .get(<naming_constants::Commit as naming_constants::Naming>::snake_case_stringified())
+    //         .and_then(|header| header.to_str().ok())
+    //     {
+    //         Some(commit_checker_header) => match commit_checker_header == git_info::PROJECT_GIT_INFO.commit {
+    //             true => Ok(()),
+    //             false => Err(CommitExtractorCheckErrorNamed::CommitExtractorNotEqual {
+    //                 commit_not_equal: std::string::String::from("different project commit provided, services must work only with equal project commits"),
+    //                 commit_to_use: crate::common::git::get_git_commit_link::GetGitCommitLink::get_git_commit_link(&git_info::PROJECT_GIT_INFO),
+    //                 code_occurence: error_occurence_lib::code_occurence!(),
+    //             })
+    //         },
+    //         None => Err(CommitExtractorCheckErrorNamed::NoCommitExtractorHeader {
+    //             no_commit_header: std::string::String::from("commit header is not provided"),
+    //             code_occurence: error_occurence_lib::code_occurence!(),
+    //         }),
+    //     },
+    //     false => Ok(()),
+    // }
+    todo!()
+}
