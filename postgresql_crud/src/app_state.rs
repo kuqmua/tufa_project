b7d259be6115cb5beb8dcacb837f1c183b0df511
kuqmua <serezhashmakov@outlook.com>
2024-04-14 14:23:31 +0300

    next steps

--- postgresql_crud/src/app_state.rs
+++ postgresql_crud/src/app_state.rs
@@ -13,7 +13,6 @@ pub trait AppState:
     + config_lib::config_fields::GetSourcePlaceType
     + config_lib::config_fields::GetTimezone
     + app_state::get_postgres_pool::GetPostgresPool
-    + config_lib::config_fields::GetEnableApiGitCommitCheck
 {
 }
 
