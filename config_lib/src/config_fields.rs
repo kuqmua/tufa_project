2c7bb6942fc8ea9e241d1c821857aed15c601f7e
kuqmua <serezhashmakov@outlook.com>
2024-04-14 15:48:33 +0300

    next steps

--- config_lib/src/config_fields.rs
+++ config_lib/src/config_fields.rs
@@ -1,5 +1,42 @@
+#[derive(Debug, Clone, serde::Serialize, serde::Deserialize, getset::Getters)]
+pub struct ServerPort {
+    #[getset(get = "pub")]
+    port: std::primitive::u16,
+}
+impl std::fmt::Display for ServerPort {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.port)
+    }
+}
+impl std::convert::TryFrom<std::primitive::u16> for ServerPort {
+    type Error = std::primitive::u16;
+    fn try_from(value: std::primitive::u16) -> Result<Self, Self::Error> {
+        if value < 1024 {
+            Err(value)
+        } else if value < 49152 {
+            Ok(Self { port: value })
+        } else {
+            Err(value)
+        }
+    }
+}
+// macro_rules! user_port_try_from_u16_with_possible_runtime_panic{
+//     ($possible_port:expr) => {
+//         if $possible_port < 1024 {
+//             panic!("failed to user_port_try_from_u16!(), reason: system port range 0-1023");
+//         }
+//         else if $possible_port < 49152 {
+//             ServerPort {
+//                 port: $possible_port
+//             }
+//         }
+//         else {
+//             panic!("failed to user_port_try_from_u16!(), reason: ephemeral port range 49152-65535");
+//         }
+//     }
+// }
 pub trait GetServerPort {
-    fn get_server_port(&self) -> &crate::user_port::UserPort;
+    fn get_server_port(&self) -> &ServerPort;
 }
 
 pub trait GetSocketAddr {
--- config_lib/src/lib.rs
+++ config_lib/src/lib.rs
@@ -1,2 +1,1 @@
 pub mod config_fields;
-pub mod user_port;
