a0dfca90486ce55a2b444e1a42ddc8a1a32c5ff2
kuqmua <serezhashmakov@outlook.com>
2024-04-15 09:11:34 +0300

    little tweaks

--- init_from_env/src/lib.rs
+++ init_from_env/src/lib.rs
@@ -135,12 +135,6 @@ pub fn init_from_env(input: proc_macro::TokenStream) -> proc_macro::TokenStream 
                     },
                 });
             });
-            let generated_init_struct_fields_iter = generated_init_struct_fields.iter();
-            let generated_functions_iter = generated_functions.iter();
-            let generated_enum_error_std_env_var_variants_iter =
-                generated_enum_error_std_env_var_variants.iter();
-            let generated_enum_error_parse_variants_iter =
-                generated_enum_error_parse_variants.iter();
             let gen = quote::quote! {
                 #[derive(Debug)]
                 pub struct #error_ident {
@@ -154,20 +148,20 @@ pub fn init_from_env(input: proc_macro::TokenStream) -> proc_macro::TokenStream 
                 }
                 #[derive(Debug)]
                 pub enum #error_std_env_var_enum_ident {
-                    #(#generated_enum_error_std_env_var_variants_iter)*
+                    #(#generated_enum_error_std_env_var_variants)*
                 }
                 #[derive(Debug)]
                 pub enum #error_parse_enum_ident {
-                    #(#generated_enum_error_parse_variants_iter)*
+                    #(#generated_enum_error_parse_variants)*
                 }
                 impl #ident {
                     #[must_use]
                     pub fn new() -> Result<Self, #error_ident> {
                         let was_dotenv_enable = dotenv::dotenv().is_ok();
-                        #(#generated_functions_iter)*
+                        #(#generated_functions)*
                         Ok(
                             Self {
-                                #(#generated_init_struct_fields_iter,)*
+                                #(#generated_init_struct_fields,)*
                             }
                         )
                     }
