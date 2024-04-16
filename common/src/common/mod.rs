8a39df37a236bfb1ae6754ec386d3fdba7281dc9
kuqmua <serezhashmakov@outlook.com>
2024-04-16 20:20:15 +0300

    next steps

--- common/src/common/mod.rs
+++ common/src/common/mod.rs
@@ -1,7 +1,6 @@
 pub mod api_request_unexpected_error;
 pub mod change_password_form_data;
 pub mod config;
-pub mod declarative_macros;
 pub mod numeric;
 pub mod postgres_credentials;
 pub mod to_default_stringified_json;
