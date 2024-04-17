4e2aeabc72eaa83d68290c4956a2094ac41e3d67
kuqmua <serezhashmakov@outlook.com>
2024-04-17 10:18:25 +0300

    next steps

--- enum_extension/src/lib.rs
+++ enum_extension/src/lib.rs
@@ -53,32 +53,32 @@ pub fn enum_extension(input: proc_macro::TokenStream) -> proc_macro::TokenStream
             }
             pub fn into_vec() -> std::vec::Vec<Self> {
                 let mut self_vec = std::vec::Vec::with_capacity(Self::get_length());
-                for self_variant in {
+                for element in {
                     use strum::IntoEnumIterator;
                     Self::iter()
                 } {
-                    self_vec.push(self_variant);
+                    self_vec.push(element);
                 }
                 self_vec
             }
             pub fn into_string_name_and_variant_hashmap() -> std::collections::HashMap<std::string::String, Self> {
                 let mut variants_hashmap: std::collections::HashMap<std::string::String, Self> =
                     std::collections::HashMap::with_capacity(Self::get_length());
-                for variant in {
+                for element in {
                     use strum::IntoEnumIterator;
                     Self::iter()
                 } {
-                    variants_hashmap.insert(format!("{}", variant), variant);
+                    variants_hashmap.insert(format!("{}", element), element);
                 }
                 variants_hashmap
             }
             pub fn into_string_name_and_variant_tuple_vec() -> std::vec::Vec<(std::string::String, Self)> {
                 let mut variants_vec = std::vec::Vec::with_capacity(Self::get_length());
-                for variant in {
+                for element in {
                     use strum::IntoEnumIterator;
                     Self::iter()
                 } {
-                    variants_vec.push((format!("{}", variant), variant));
+                    variants_vec.push((format!("{}", element), element));
                 }
                 variants_vec
             }
