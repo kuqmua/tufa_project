20d11c5d3182f9b093a73d1a4d5a207da28fd26a
kuqmua <serezhashmakov@outlook.com>
2024-04-18 19:16:41 +0300

    next steps

--- enum_extension_lib/enum_extension/src/lib.rs
+++ enum_extension_lib/enum_extension/src/lib.rs
@@ -6,11 +6,6 @@
 )]
 #![allow(clippy::too_many_arguments)]
 
-//todo maybe reexport from crates
-///you need to install this crates to use this macro(check on work was only with this versions)
-/// strum = {version = "=0.24.1"}
-/// strum_macros = {version = "=0.24.3"}
-/// convert_case = {version = "=0.6.0"}
 #[proc_macro_derive(EnumExtension)]
 pub fn enum_extension(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     proc_macro_common::panic_location::panic_location();
