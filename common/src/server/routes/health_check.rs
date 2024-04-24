f7a992b1dd3a70bb2c1bf0ec09494cdd265f382a
kuqmua <serezhashmakov@outlook.com>
2024-04-24 20:55:03 +0300

    next steps

--- common/src/server/routes/health_check.rs
+++ common/src/server/routes/health_check.rs
@@ -1,8 +1,6 @@
-async fn health_check() -> impl axum::response::IntoResponse {
-    println!("health_check");
-    axum::http::StatusCode::OK
-}
-
 pub(crate) fn health_check_route() -> axum::Router {
-    axum::Router::new().route("/health_check", axum::routing::get(health_check))
+    axum::Router::new().route("/health_check", axum::routing::get(|| async {
+        println!("health_check");
+        axum::http::StatusCode::OK
+    }))
 }
