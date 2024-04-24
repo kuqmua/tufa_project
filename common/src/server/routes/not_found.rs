9d22a63076ae1b26a2b33e406c2af4fa8a42d4b1
kuqmua <serezhashmakov@outlook.com>
2024-04-24 20:59:36 +0300

    next steps

--- common/src/server/routes/not_found.rs
+++ common/src/server/routes/not_found.rs
@@ -11,22 +11,20 @@ struct NotFoundHandle {
 }
 
 //todo maybe use swagger instead
-async fn not_found(
-    uri: axum::http::Uri,
-    axum::extract::State(app_state): axum::extract::State<DynArcNotFoundRouteParametersSendSync>,
-) -> impl axum::response::IntoResponse {
-    (
-        axum::http::StatusCode::NOT_FOUND,
-        axum::Json(NotFoundHandle {
-            message: format!("No route for {uri}"),
-            commit: app_state.get_git_commit_link(),
-            open_api_specification: constants::SLASH_SWAGGER_UI,
-        }),
-    )
-}
-
 pub fn not_found_route(app_state: DynArcNotFoundRouteParametersSendSync) -> axum::Router {
     axum::Router::new()
-        .fallback(not_found)
+        .fallback(|
+            uri: axum::http::Uri,
+            axum::extract::State(app_state): axum::extract::State<DynArcNotFoundRouteParametersSendSync>
+        | async move {
+            (
+                axum::http::StatusCode::NOT_FOUND,
+                axum::Json(NotFoundHandle {
+                    message: format!("No route for {uri}"),
+                    commit: app_state.get_git_commit_link(),
+                    open_api_specification: constants::SLASH_SWAGGER_UI,
+                }),
+            )
+        })
         .with_state(app_state)
 }
