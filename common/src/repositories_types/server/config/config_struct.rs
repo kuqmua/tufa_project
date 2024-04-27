3ad068bba6256130010a5d391bc89c6cb463e49a
kuqmua <serezhashmakov@outlook.com>
2024-04-27 22:13:28 +0300

    next steps

--- common/src/repositories_types/server/config/config_struct.rs
+++ common/src/repositories_types/server/config/config_struct.rs
@@ -21,160 +21,101 @@ pub struct Config {
     maximum_size_of_http_body_in_bytes: std::primitive::usize,
 }
 
-#[derive(Debug, thiserror::Error, error_occurence_lib::ErrorOccurence)]
-pub enum ConfigCheckErrorNamed {
-    ServiceSocketAddress {
-        #[eo_display]
-        server_port: std::net::AddrParseError,
-        code_occurence: error_occurence_lib::code_occurence::CodeOccurence,
-    },
-    //
-    RequireSsl {
-        #[eo_display_with_serialize_deserialize]
-        require_ssl: bool,
-        code_occurence: error_occurence_lib::code_occurence::CodeOccurence,
-    },
-    Timezone {
-        #[eo_display_with_serialize_deserialize]
-        timezone: i32,
-        code_occurence: error_occurence_lib::code_occurence::CodeOccurence,
-    },
-    RedisUrl {
-        #[eo_display_with_serialize_deserialize]
-        redis_url: std::string::String,
-        code_occurence: error_occurence_lib::code_occurence::CodeOccurence,
-    },
-    MongoUrl {
-        #[eo_display_with_serialize_deserialize]
-        mongo_url: std::string::String,
-        code_occurence: error_occurence_lib::code_occurence::CodeOccurence,
-    },
-    DatabaseUrl {
-        #[eo_display_with_serialize_deserialize]
-        database_url: std::string::String,
-        code_occurence: error_occurence_lib::code_occurence::CodeOccurence,
-    },
-    StartingCheckLink {
-        #[eo_display_with_serialize_deserialize]
-        starting_check_link: std::string::String,
-        code_occurence: error_occurence_lib::code_occurence::CodeOccurence,
-    },
-    TracingType {
-        #[eo_display_with_serialize_deserialize]
-        tracing_type: app_state::TracingType,
-        code_occurence: error_occurence_lib::code_occurence::CodeOccurence,
-    },
-    SourcePlaceType {
-        #[eo_display_with_serialize_deserialize]
-        source_place_type: app_state::SourcePlaceType,
-        code_occurence: error_occurence_lib::code_occurence::CodeOccurence,
-    },
+// #[derive(Debug, thiserror::Error, error_occurence_lib::ErrorOccurence)]
+// pub enum ConfigCheckErrorNamed {
+//     ServiceSocketAddress {
+//         #[eo_display]
+//         server_port: std::net::AddrParseError,
+//         code_occurence: error_occurence_lib::code_occurence::CodeOccurence,
+//     },
+//     //
+//     RequireSsl {
+//         #[eo_display_with_serialize_deserialize]
+//         require_ssl: bool,
+//         code_occurence: error_occurence_lib::code_occurence::CodeOccurence,
+//     },
+//     Timezone {
+//         #[eo_display_with_serialize_deserialize]
+//         timezone: i32,
+//         code_occurence: error_occurence_lib::code_occurence::CodeOccurence,
+//     },
+//     RedisUrl {
+//         #[eo_display_with_serialize_deserialize]
+//         redis_url: std::string::String,
+//         code_occurence: error_occurence_lib::code_occurence::CodeOccurence,
+//     },
+//     MongoUrl {
+//         #[eo_display_with_serialize_deserialize]
+//         mongo_url: std::string::String,
+//         code_occurence: error_occurence_lib::code_occurence::CodeOccurence,
+//     },
+//     DatabaseUrl {
+//         #[eo_display_with_serialize_deserialize]
+//         database_url: std::string::String,
+//         code_occurence: error_occurence_lib::code_occurence::CodeOccurence,
+//     },
+//     StartingCheckLink {
+//         #[eo_display_with_serialize_deserialize]
+//         starting_check_link: std::string::String,
+//         code_occurence: error_occurence_lib::code_occurence::CodeOccurence,
+//     },
+//     TracingType {
+//         #[eo_display_with_serialize_deserialize]
+//         tracing_type: app_state::TracingType,
+//         code_occurence: error_occurence_lib::code_occurence::CodeOccurence,
+//     },
+//     SourcePlaceType {
+//         #[eo_display_with_serialize_deserialize]
+//         source_place_type: app_state::SourcePlaceType,
+//         code_occurence: error_occurence_lib::code_occurence::CodeOccurence,
+//     },
+// }
+#[derive(Debug, config_lib::TryFromEnv)]
+struct ConfigHandle {
+    //todo maybe auto generate .env and docker-compose environment variables. and maybe write in directly into files
+    service_socket_address: ServiceSocketAddress,
+    
+    timezone: Timezone, //for some reason chrono::FixedOffset::east_opt uses i32 but i16 is enough
+    
+    redis_url: RedisUrl,
+    
+    mongo_url: MongoUrl,
+    
+    database_url: DatabaseUrl, //postgres_url, naming required by sqlx::query::query!
+    
+    starting_check_link: StartingCheckLink, //todo add browser url limit check
+    
+    tracing_type: TracingType,
+    source_place_type: SourcePlaceType,
+    enable_api_git_commit_check: EnableApiGitCommitCheck,
+    maximum_size_of_http_body_in_bytes: MaximumSizeOfHttpBodyInBytes,
 }
 
 impl Config {
-    pub fn try_from_env() -> Result<Self, ConfigCheckErrorNamed> {
-        #[derive(Debug, Default, PartialEq, Eq, config_lib::InitFromEnv)]
-        struct ConfigUnchecked {
-            //todo maybe auto generate .env and docker-compose environment variables. and maybe write in directly into files
-            service_socket_address: std::string::String,
-        
-            timezone: std::primitive::i32, //for some reason chrono::FixedOffset::east_opt uses i32 but i16 is enough
-        
-            redis_url: std::string::String,
-        
-            mongo_url: std::string::String,
-        
-            database_url: std::string::String, //postgres_url, naming required by sqlx::query::query!
-        
-            starting_check_link: std::string::String, //todo add browser url limit check
-        
-            tracing_type: app_state::TracingType,
-            source_place_type: app_state::SourcePlaceType,
-            enable_api_git_commit_check: std::primitive::bool,
-            maximum_size_of_http_body_in_bytes: std::primitive::usize,
-        }
-        ///////////////
-
-        ////////////////
-        let value = ConfigUnchecked::new().unwrap_or_else(|error| panic!("failed to ConfigUnchecked::new(), reason: {error:#?}"));
-        // Self::try_from(value).unwrap_or_else(|error| panic!("failed to Config try_from ConfigUnchecked, reason: {error}"))
-        let service_socket_address =  match <std::net::SocketAddr as std::str::FromStr>::from_str(&value.service_socket_address) {
+    pub fn try_from_env() -> Result<Self, ConfigHandleTryFromEnvErrorNamed> {
+        let config_handle = match ConfigHandle::try_from_env() {
             Ok(value) => value,
             Err(error) => {
-                return Err(ConfigCheckErrorNamed::ServiceSocketAddress {
-                    server_port: error,
-                    code_occurence: error_occurence_lib::code_occurence!(),
-                });
+                return Err(error);
             }
         };
-        let timezone = match chrono::FixedOffset::east_opt(value.timezone) {
-            Some(fixed_offset) => fixed_offset,
-            None => {
-                return Err(ConfigCheckErrorNamed::Timezone {
-                    timezone: value.timezone,
-                    code_occurence: error_occurence_lib::code_occurence!(),
-                });
-            }
-        };
-        let redis_url = if value.redis_url.is_empty() {
-            return Err(ConfigCheckErrorNamed::RedisUrl {
-                redis_url: value.redis_url,
-                code_occurence: error_occurence_lib::code_occurence!(),
-            });
-        }
-        else {
-            secrecy::Secret::new(value.redis_url)
-        };
-        let mongo_url = if value.mongo_url.is_empty() {
-            return Err(ConfigCheckErrorNamed::MongoUrl {
-                mongo_url: value.mongo_url,
-                code_occurence: error_occurence_lib::code_occurence!(),
-            });
-        }
-        else {
-            secrecy::Secret::new(value.mongo_url)
-        };
-        let database_url = if value.database_url.is_empty() {
-            return Err(ConfigCheckErrorNamed::DatabaseUrl {
-                database_url: value.database_url,
-                code_occurence: error_occurence_lib::code_occurence!(),
-            });
-        }
-        else {
-            secrecy::Secret::new(value.database_url)
-        }; //postgres_url = value.; naming required by sqlx::query::query!
-        let starting_check_link = if value.starting_check_link.is_empty() {
-            return Err(ConfigCheckErrorNamed::StartingCheckLink {
-                starting_check_link: value.starting_check_link,
-                code_occurence: error_occurence_lib::code_occurence!(),
-            });
-        }
-        else {
-            value.starting_check_link
-        }; //todo add browser url limit check
-        let tracing_type = value.tracing_type;
-        let source_place_type = value.source_place_type;
-        let enable_api_git_commit_check = value.enable_api_git_commit_check;
-        let maximum_size_of_http_body_in_bytes = value.maximum_size_of_http_body_in_bytes;
-        let j = Something::try_from_env();
-        println!("j {j:#?}");
         Ok(Self {
-            service_socket_address,
+            service_socket_address: config_handle.service_socket_address.0,
         
-            timezone,
+            timezone: config_handle.timezone.0,
         
-            redis_url,
+            redis_url: config_handle.redis_url.0,
         
-            mongo_url,
+            mongo_url: config_handle.mongo_url.0,
         
-            database_url, //postgres_url, naming required by sqlx::query::query!
+            database_url: config_handle.database_url.0, //postgres_url, naming required by sqlx::query::query!
         
-            starting_check_link, //todo add browser url limit check
+            starting_check_link: config_handle.starting_check_link.0, //todo add browser url limit check
         
-            tracing_type,
-            source_place_type,
-            enable_api_git_commit_check,
-            maximum_size_of_http_body_in_bytes,
+            tracing_type: config_handle.tracing_type.0,
+            source_place_type: config_handle.source_place_type.0,
+            enable_api_git_commit_check: config_handle.enable_api_git_commit_check.0,
+            maximum_size_of_http_body_in_bytes: config_handle.maximum_size_of_http_body_in_bytes.0,
         })
     }
 }
@@ -449,25 +390,3 @@ impl TryFromStdEnvVarOk for MaximumSizeOfHttpBodyInBytes {
         Ok(Self(value))
     }
 }
-
-/////////////
-        #[derive(Debug, config_lib::TryFromEnv)]
-        struct Something {
-            //todo maybe auto generate .env and docker-compose environment variables. and maybe write in directly into files
-            service_socket_address: ServiceSocketAddress,
-        
-            timezone: Timezone, //for some reason chrono::FixedOffset::east_opt uses i32 but i16 is enough
-        
-            redis_url: RedisUrl,
-        
-            mongo_url: MongoUrl,
-        
-            database_url: DatabaseUrl, //postgres_url, naming required by sqlx::query::query!
-        
-            starting_check_link: StartingCheckLink, //todo add browser url limit check
-        
-            tracing_type: TracingType,
-            source_place_type: SourcePlaceType,
-            enable_api_git_commit_check: EnableApiGitCommitCheck,
-            maximum_size_of_http_body_in_bytes: MaximumSizeOfHttpBodyInBytes,
-        }
