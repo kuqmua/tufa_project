87ce3c1d34614dad00df16ce296bfb810e4a9abe
kuqmua <serezhashmakov@outlook.com>
2024-04-27 11:37:46 +0300

    fix

--- common/src/repositories_types/server/config/config_struct.rs
+++ common/src/repositories_types/server/config/config_struct.rs
@@ -28,8 +28,88 @@ pub struct Config {
     maximum_size_of_http_body_in_bytes: std::primitive::usize,
 }
 
+#[derive(Debug, thiserror::Error, error_occurence_lib::ErrorOccurence)]
+pub enum ConfigCheckErrorNamed {
+    // ConfigUnchecked {
+    //     #[eo_display]
+    //     config_unchecked_error: ConfigUncheckedError,
+    //     code_occurence: error_occurence_lib::code_occurence::CodeOccurence,
+    // },
+    ServiceSocketAddress {
+        #[eo_display]
+        server_port: std::net::AddrParseError,
+        code_occurence: error_occurence_lib::code_occurence::CodeOccurence,
+    },
+    HmacSecret {
+        #[eo_display_with_serialize_deserialize]
+        hmac_secret: std::string::String,
+        code_occurence: error_occurence_lib::code_occurence::CodeOccurence,
+    },
+    BaseUrl {
+        #[eo_display_with_serialize_deserialize]
+        base_url: std::string::String,
+        code_occurence: error_occurence_lib::code_occurence::CodeOccurence,
+    },
+    //
+    RequireSsl {
+        #[eo_display_with_serialize_deserialize]
+        require_ssl: bool,
+        code_occurence: error_occurence_lib::code_occurence::CodeOccurence,
+    },
+    AccessControlAllowOrigin {
+        #[eo_display_with_serialize_deserialize]
+        access_control_allow_origin: std::string::String,
+        code_occurence: error_occurence_lib::code_occurence::CodeOccurence,
+    },
+    GithubName {
+        #[eo_display_with_serialize_deserialize]
+        github_name: std::string::String,
+        code_occurence: error_occurence_lib::code_occurence::CodeOccurence,
+    },
+    GithubToken {
+        #[eo_display_with_serialize_deserialize]
+        github_token: std::string::String,
+        code_occurence: error_occurence_lib::code_occurence::CodeOccurence,
+    },
+    Timezone {
+        #[eo_display_with_serialize_deserialize]
+        timezone: i32,
+        code_occurence: error_occurence_lib::code_occurence::CodeOccurence,
+    },
+    RedisUrl {
+        #[eo_display_with_serialize_deserialize]
+        redis_url: std::string::String,
+        code_occurence: error_occurence_lib::code_occurence::CodeOccurence,
+    },
+    MongoUrl {
+        #[eo_display_with_serialize_deserialize]
+        mongo_url: std::string::String,
+        code_occurence: error_occurence_lib::code_occurence::CodeOccurence,
+    },
+    DatabaseUrl {
+        #[eo_display_with_serialize_deserialize]
+        database_url: std::string::String,
+        code_occurence: error_occurence_lib::code_occurence::CodeOccurence,
+    },
+    StartingCheckLink {
+        #[eo_display_with_serialize_deserialize]
+        starting_check_link: std::string::String,
+        code_occurence: error_occurence_lib::code_occurence::CodeOccurence,
+    },
+    TracingType {
+        #[eo_display_with_serialize_deserialize]
+        tracing_type: app_state::TracingType,
+        code_occurence: error_occurence_lib::code_occurence::CodeOccurence,
+    },
+    SourcePlaceType {
+        #[eo_display_with_serialize_deserialize]
+        source_place_type: app_state::SourcePlaceType,
+        code_occurence: error_occurence_lib::code_occurence::CodeOccurence,
+    },
+}
+
 impl Config {
-    pub fn new() -> Self {
+    pub fn try_from_env() -> Result<Self, ConfigCheckErrorNamed> {
         #[derive(Debug, Default, PartialEq, Eq, config_lib::InitFromEnv)]
         struct ConfigUnchecked {
             //todo maybe auto generate .env and docker-compose environment variables. and maybe write in directly into files
@@ -57,145 +137,60 @@ impl Config {
             enable_api_git_commit_check: std::primitive::bool,
             maximum_size_of_http_body_in_bytes: std::primitive::usize,
         }
-        #[derive(Debug, thiserror::Error, error_occurence_lib::ErrorOccurence)]
-        enum ConfigCheckErrorNamed {
-            // ConfigUnchecked {
-            //     #[eo_display]
-            //     config_unchecked_error: ConfigUncheckedError,
-            //     code_occurence: error_occurence_lib::code_occurence::CodeOccurence,
-            // },
-            ServiceSocketAddress {
-                #[eo_display]
-                server_port: std::net::AddrParseError,
-                code_occurence: error_occurence_lib::code_occurence::CodeOccurence,
-            },
-            HmacSecret {
-                #[eo_display_with_serialize_deserialize]
-                hmac_secret: std::string::String,
-                code_occurence: error_occurence_lib::code_occurence::CodeOccurence,
-            },
-            BaseUrl {
-                #[eo_display_with_serialize_deserialize]
-                base_url: std::string::String,
-                code_occurence: error_occurence_lib::code_occurence::CodeOccurence,
-            },
-            //
-            RequireSsl {
-                #[eo_display_with_serialize_deserialize]
-                require_ssl: bool,
-                code_occurence: error_occurence_lib::code_occurence::CodeOccurence,
-            },
-            AccessControlAllowOrigin {
-                #[eo_display_with_serialize_deserialize]
-                access_control_allow_origin: std::string::String,
-                code_occurence: error_occurence_lib::code_occurence::CodeOccurence,
-            },
-            GithubName {
-                #[eo_display_with_serialize_deserialize]
-                github_name: std::string::String,
-                code_occurence: error_occurence_lib::code_occurence::CodeOccurence,
-            },
-            GithubToken {
-                #[eo_display_with_serialize_deserialize]
-                github_token: std::string::String,
-                code_occurence: error_occurence_lib::code_occurence::CodeOccurence,
-            },
-            Timezone {
-                #[eo_display_with_serialize_deserialize]
-                timezone: i32,
-                code_occurence: error_occurence_lib::code_occurence::CodeOccurence,
-            },
-            RedisUrl {
-                #[eo_display_with_serialize_deserialize]
-                redis_url: std::string::String,
-                code_occurence: error_occurence_lib::code_occurence::CodeOccurence,
-            },
-            MongoUrl {
-                #[eo_display_with_serialize_deserialize]
-                mongo_url: std::string::String,
-                code_occurence: error_occurence_lib::code_occurence::CodeOccurence,
-            },
-            DatabaseUrl {
-                #[eo_display_with_serialize_deserialize]
-                database_url: std::string::String,
-                code_occurence: error_occurence_lib::code_occurence::CodeOccurence,
-            },
-            StartingCheckLink {
-                #[eo_display_with_serialize_deserialize]
-                starting_check_link: std::string::String,
-                code_occurence: error_occurence_lib::code_occurence::CodeOccurence,
-            },
-            TracingType {
-                #[eo_display_with_serialize_deserialize]
-                tracing_type: app_state::TracingType,
-                code_occurence: error_occurence_lib::code_occurence::CodeOccurence,
-            },
-            SourcePlaceType {
-                #[eo_display_with_serialize_deserialize]
-                source_place_type: app_state::SourcePlaceType,
-                code_occurence: error_occurence_lib::code_occurence::CodeOccurence,
-            },
-        }
         let value = ConfigUnchecked::new().unwrap_or_else(|error| panic!("failed to ConfigUnchecked::new(), reason: {error:#?}"));
         // Self::try_from(value).unwrap_or_else(|error| panic!("failed to Config try_from ConfigUnchecked, reason: {error}"))
         let service_socket_address =  match <std::net::SocketAddr as std::str::FromStr>::from_str(&value.service_socket_address) {
             Ok(value) => value,
             Err(error) => {
-                // return Err(Self::Error::ServiceSocketAddress {
-                //     server_port: error,
-                //     code_occurence: error_occurence_lib::code_occurence!(),
-                // });
-                panic!("1");
+                return Err(ConfigCheckErrorNamed::ServiceSocketAddress {
+                    server_port: error,
+                    code_occurence: error_occurence_lib::code_occurence!(),
+                });
             }
         };
         let hmac_secret = if value.hmac_secret.is_empty() {
-            // return Err(Self::Error::HmacSecret {
-            //     hmac_secret: value.hmac_secret,
-            //     code_occurence: error_occurence_lib::code_occurence!(),
-            // });
-            panic!("2");
+            return Err(ConfigCheckErrorNamed::HmacSecret {
+                hmac_secret: value.hmac_secret,
+                code_occurence: error_occurence_lib::code_occurence!(),
+            });
         }
         else {
             secrecy::Secret::new(value.hmac_secret)
         };
         let base_url = if value.base_url.is_empty() {
-            // return Err(Self::Error::BaseUrl {
-            //     base_url: value.base_url,
-            //     code_occurence: error_occurence_lib::code_occurence!(),
-            // });
-            panic!("3");
+            return Err(ConfigCheckErrorNamed::BaseUrl {
+                base_url: value.base_url,
+                code_occurence: error_occurence_lib::code_occurence!(),
+            });
         }
         else {
             value.base_url
         };
         let access_control_max_age = value.access_control_max_age;
         let access_control_allow_origin = if value.access_control_allow_origin.is_empty() {
-            // return Err(Self::Error::AccessControlAllowOrigin {
-            //     access_control_allow_origin: value.access_control_allow_origin,
-            //     code_occurence: error_occurence_lib::code_occurence!(),
-            // }); //todo - maybe add check if its uri\url
-            panic!("4");
+            return Err(ConfigCheckErrorNamed::AccessControlAllowOrigin {
+                access_control_allow_origin: value.access_control_allow_origin,
+                code_occurence: error_occurence_lib::code_occurence!(),
+            }); //todo - maybe add check if its uri\url
         }
         else {
             value.access_control_allow_origin
         };
         
         let github_name = if value.github_name.is_empty() {
-            // return Err(Self::Error::GithubName {
-            //     github_name: value.github_name,
-            //     code_occurence: error_occurence_lib::code_occurence!(),
-            // });
-            panic!("5");
+            return Err(ConfigCheckErrorNamed::GithubName {
+                github_name: value.github_name,
+                code_occurence: error_occurence_lib::code_occurence!(),
+            });
         }
         else {
             value.github_name
         };
         let github_token = if value.github_token.is_empty() {
-            // return Err(Self::Error::GithubToken {
-            //     github_token: value.github_token,
-            //     code_occurence: error_occurence_lib::code_occurence!(),
-            // });
-            panic!("6");
+            return Err(ConfigCheckErrorNamed::GithubToken {
+                github_token: value.github_token,
+                code_occurence: error_occurence_lib::code_occurence!(),
+            });
         }
         else {
             value.github_token
@@ -203,50 +198,44 @@ impl Config {
         let timezone = match chrono::FixedOffset::east_opt(value.timezone) {
             Some(fixed_offset) => fixed_offset,
             None => {
-                // return Err(Self::Error::Timezone {
-                //     timezone: value.timezone,
-                //     code_occurence: error_occurence_lib::code_occurence!(),
-                // });
-                panic!("7");
+                return Err(ConfigCheckErrorNamed::Timezone {
+                    timezone: value.timezone,
+                    code_occurence: error_occurence_lib::code_occurence!(),
+                });
             }
         };
         let redis_url = if value.redis_url.is_empty() {
-            // return Err(Self::Error::RedisUrl {
-            //     redis_url: value.redis_url,
-            //     code_occurence: error_occurence_lib::code_occurence!(),
-            // });
-            panic!("8");
+            return Err(ConfigCheckErrorNamed::RedisUrl {
+                redis_url: value.redis_url,
+                code_occurence: error_occurence_lib::code_occurence!(),
+            });
         }
         else {
             secrecy::Secret::new(value.redis_url)
         };
         let mongo_url = if value.mongo_url.is_empty() {
-            // return Err(Self::Error::MongoUrl {
-            //     mongo_url: value.mongo_url,
-            //     code_occurence: error_occurence_lib::code_occurence!(),
-            // });
-            panic!("9");
+            return Err(ConfigCheckErrorNamed::MongoUrl {
+                mongo_url: value.mongo_url,
+                code_occurence: error_occurence_lib::code_occurence!(),
+            });
         }
         else {
             secrecy::Secret::new(value.mongo_url)
         };
         let database_url = if value.database_url.is_empty() {
-            // return Err(Self::Error::DatabaseUrl {
-            //     database_url: value.database_url,
-            //     code_occurence: error_occurence_lib::code_occurence!(),
-            // });
-            panic!("10");
+            return Err(ConfigCheckErrorNamed::DatabaseUrl {
+                database_url: value.database_url,
+                code_occurence: error_occurence_lib::code_occurence!(),
+            });
         }
         else {
             secrecy::Secret::new(value.database_url)
         }; //postgres_url = value.; naming required by sqlx::query::query!
-        
         let starting_check_link = if value.starting_check_link.is_empty() {
-            // return Err(Self::Error::StartingCheckLink {
-            //     starting_check_link: value.starting_check_link,
-            //     code_occurence: error_occurence_lib::code_occurence!(),
-            // });
-            panic!("11");
+            return Err(ConfigCheckErrorNamed::StartingCheckLink {
+                starting_check_link: value.starting_check_link,
+                code_occurence: error_occurence_lib::code_occurence!(),
+            });
         }
         else {
             value.starting_check_link
@@ -255,7 +244,7 @@ impl Config {
         let source_place_type = value.source_place_type;
         let enable_api_git_commit_check = value.enable_api_git_commit_check;
         let maximum_size_of_http_body_in_bytes = value.maximum_size_of_http_body_in_bytes;
-        Self {
+        Ok(Self {
             service_socket_address,
             hmac_secret,
             base_url,
@@ -279,13 +268,7 @@ impl Config {
             source_place_type,
             enable_api_git_commit_check,
             maximum_size_of_http_body_in_bytes,
-        }
-    }
-}
-
-impl std::default::Default for Config {
-    fn default() -> Self {
-        Self::new()
+        })
     }
 }
 
--- server/src/main.rs
+++ server/src/main.rs
@@ -24,6 +24,8 @@ pub mod dev;
 fn main() {
     println!("commit {}", git_info::PROJECT_GIT_INFO.commit);
     crate::entry::entry(
-        crate::global_variables::runtime::config::CONFIG.get_or_init(|| common::repositories_types::server::config::config_struct::Config::new()),
+        crate::global_variables::runtime::config::CONFIG.get_or_init(
+            || common::repositories_types::server::config::config_struct::Config::try_from_env().unwrap()
+        ),
     );
 }
