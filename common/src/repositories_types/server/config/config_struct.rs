b86183882113d841edd9817a599890386631807e
kuqmua <serezhashmakov@outlook.com>
2024-04-28 13:20:19 +0300

    next steps

--- common/src/repositories_types/server/config/config_struct.rs
+++ common/src/repositories_types/server/config/config_struct.rs
@@ -22,7 +22,8 @@ pub struct Config {
 }
 
 #[derive(Debug, 
-    config_lib::TryFromEnv
+    config_lib::TryFromEnv,
+    generate_getter_traits_for_struct_fields::GenerateGetterTraitsForStructFieldsHandle
 )]
 struct ConfigHandle {
     //todo maybe auto generate .env and docker-compose environment variables. and maybe write in directly into files
@@ -44,33 +45,33 @@ struct ConfigHandle {
     maximum_size_of_http_body_in_bytes: config_lib::MaximumSizeOfHttpBodyInBytesWrapper,
 }
 
-impl Config {
-    pub fn try_from_env() -> Result<Self, ConfigHandleTryFromEnvErrorNamed> {
-        let config_handle = match ConfigHandle::try_from_env() {
-            Ok(value) => value,
-            Err(error) => {
-                return Err(error);
-            }
-        };
-        Ok(Self {
-            service_socket_address: config_handle.service_socket_address.0,
+// impl Config {
+//     pub fn try_from_env() -> Result<Self, ConfigHandleTryFromEnvErrorNamed> {
+//         let config_handle = match ConfigHandle::try_from_env() {
+//             Ok(value) => value,
+//             Err(error) => {
+//                 return Err(error);
+//             }
+//         };
+//         Ok(Self {
+//             service_socket_address: config_handle.service_socket_address.0,
         
-            timezone: config_handle.timezone.0,
+//             timezone: config_handle.timezone.0,
         
-            redis_url: config_handle.redis_url.0,
+//             redis_url: config_handle.redis_url.0,
         
-            mongo_url: config_handle.mongo_url.0,
+//             mongo_url: config_handle.mongo_url.0,
         
-            database_url: config_handle.database_url.0, //postgres_url, naming required by sqlx::query::query!
+//             database_url: config_handle.database_url.0, //postgres_url, naming required by sqlx::query::query!
         
-            starting_check_link: config_handle.starting_check_link.0, //todo add browser url limit check
+//             starting_check_link: config_handle.starting_check_link.0, //todo add browser url limit check
         
-            tracing_type: config_handle.tracing_type.0,
-            source_place_type: config_handle.source_place_type.0,
-            enable_api_git_commit_check: config_handle.enable_api_git_commit_check.0,
-            maximum_size_of_http_body_in_bytes: config_handle.maximum_size_of_http_body_in_bytes.0,
-        })
-    }
-}
+//             tracing_type: config_handle.tracing_type.0,
+//             source_place_type: config_handle.source_place_type.0,
+//             enable_api_git_commit_check: config_handle.enable_api_git_commit_check.0,
+//             maximum_size_of_http_body_in_bytes: config_handle.maximum_size_of_http_body_in_bytes.0,
+//         })
+//     }
+// }
 ////////////////////
 
--- generate_getter_traits_for_struct_fields/src/lib.rs
+++ generate_getter_traits_for_struct_fields/src/lib.rs
@@ -51,48 +51,63 @@ pub fn generate_getter_traits_for_struct_fields(
     let gen = quote::quote! {
         #(#generated_traits_implementations)*
     };
+    // println!("{gen}");
+    gen.into()
+}
+////////////////////////
+#[proc_macro_derive(GenerateGetterTraitsForStructFieldsHandle)]
+pub fn generate_getter_traits_for_struct_fields_handle(
+    input: proc_macro::TokenStream,
+) -> proc_macro::TokenStream {
+    proc_macro_common::panic_location::panic_location();
+    let ast: syn::DeriveInput =
+        syn::parse(input).expect("GenerateGetterTraitsForStructFields syn::parse(input) failed");
+    let ident = &ast.ident;
+    let generated_traits_implementations = match ast.data {
+        syn::Data::Struct(datastruct) => datastruct.fields.into_iter().map(|field| {
+            let (field_ident, upper_camel_case_field_ident) = {
+                let field_ident = field
+                    .ident
+                    .as_ref()
+                    .unwrap_or_else(|| panic!("{ident} {}", naming_constants::FIELD_IDENT_IS_NONE));
+                (
+                    field_ident,
+                    syn::Ident::new(
+                        &convert_case::Casing::to_case(
+                            &format!("{field_ident}"),
+                            convert_case::Case::UpperCamel,
+                        ),
+                        ident.span(),
+                    ),
+                )
+            };
+            let type_ident = field.ty;
+            let path_trait_ident =
+                format!("app_state::Get{upper_camel_case_field_ident}")
+                    .parse::<proc_macro2::TokenStream>()
+                    .expect("path_trait_ident parse failed");
+            let function_name_ident = format!("get_{field_ident}")
+                .parse::<proc_macro2::TokenStream>()
+                .expect("function_name_ident parse failed");
+            quote::quote! {
+                impl #path_trait_ident for #ident {
+                    fn #function_name_ident (&self) -> &#type_ident {
+                        &self.#field_ident.into_inner()
+                    }
+                }
+                impl #path_trait_ident for &#ident {
+                    fn #function_name_ident (&self) -> &#type_ident {
+                        &self.#field_ident.into_inner()
+                    }
+                }
+            }
+        }),
+        syn::Data::Enum(_) | 
+        syn::Data::Union(_) => panic!("GenerateGetterTraitsForStructFields only works on Struct"),
+    };
+    let gen = quote::quote! {
+        #(#generated_traits_implementations)*
+    };
     println!("{gen}");
     gen.into()
 }
-// its for maybe future type selection
-// let type_ident_string = match type_ident.clone() {
-//                 syn::Type::Path(path) => {
-//                     //todo not a full logic
-//                     match path.path().segments.len() {
-//                         1 => format!("{}", path.path().segments[0].ident),
-//                         _ => panic!(
-//                             "GenerateGetterTraitsForStructFields path.path().segments.len() != 1"
-//                         ),
-//                     }
-//                 }
-//                 // syn::Type::Array(_) => todo!(),
-//                 // syn::Type::BareFn(_) => todo!(),
-//                 // syn::Type::Group(_) => todo!(),
-//                 // syn::Type::ImplTrait(_) => todo!(),
-//                 // syn::Type::Infer(_) => todo!(),
-//                 // syn::Type::Macro(_) => todo!(),
-//                 // syn::Type::Never(_) => todo!(),
-//                 // syn::Type::Paren(_) => todo!(),
-//                 // syn::Type::Ptr(_) => todo!(),
-//                 // syn::Type::Reference(_) => todo!(),
-//                 // syn::Type::Slice(_) => todo!(),
-//                 // syn::Type::TraitObject(_) => todo!(),
-//                 // syn::Type::Tuple(_) => todo!(),
-//                 // syn::Type::Verbatim(_) => todo!(),
-//                 _ => panic!("GenerateGetterTraitsForStructFields supports only syn::Type::Path"),
-//             };
-//             let u8_handle = "u8";
-//             let (type_ident_gen, field_ident_gen) = match type_ident_string.eq(u8_handle) {
-//                 true => (quote::quote! {u8}, quote::quote! {self.#field_ident}),
-//                 false => (
-//                     quote::quote! {&#type_ident},
-//                     quote::quote! {&self.#field_ident},
-//                 ),
-//             };
-//             quote::quote! {
-//                 impl #path_trait_ident for #ident {
-//                     fn #function_name_ident (&self) -> #type_ident_gen {
-//                         #field_ident_gen
-//                     }
-//                 }
-//             }
