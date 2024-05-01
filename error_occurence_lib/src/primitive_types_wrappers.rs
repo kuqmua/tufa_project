248437dfce2b356aa6d9a3e9b509846a18b5cd69
kuqmua <serezhashmakov@outlook.com>
2024-05-01 16:25:05 +0300

    next steps

--- error_occurence_lib/src/primitive_types_wrappers.rs
+++ error_occurence_lib/src/primitive_types_wrappers.rs
@@ -1,5 +1,20 @@
-#[derive(Debug, PartialEq, Eq, Hash, error_occurence::PrimitiveErrorOccurencePartialEqEqHash)]
-pub struct StdStringString(pub std::string::String);
 #[derive(Debug, PartialEq, Eq, Hash, Clone, Copy, error_occurence::PrimitiveErrorOccurencePartialEqEqHashCloneCopy)]
 pub struct StdPrimitiveU8(pub std::primitive::u8);
-
+#[derive(Debug, PartialEq, Eq, Hash, Clone, Copy, error_occurence::PrimitiveErrorOccurencePartialEqEqHashCloneCopy)]
+pub struct StdPrimitiveI8(pub std::primitive::i8);
+#[derive(Debug, PartialEq, Eq, Hash, Clone, Copy, error_occurence::PrimitiveErrorOccurencePartialEqEqHashCloneCopy)]
+pub struct StdPrimitiveU16(pub std::primitive::u16);
+#[derive(Debug, PartialEq, Eq, Hash, Clone, Copy, error_occurence::PrimitiveErrorOccurencePartialEqEqHashCloneCopy)]
+pub struct StdPrimitiveI16(pub std::primitive::i16);
+#[derive(Debug, PartialEq, Eq, Hash, Clone, Copy, error_occurence::PrimitiveErrorOccurencePartialEqEqHashCloneCopy)]
+pub struct StdPrimitiveU32(pub std::primitive::u32);
+#[derive(Debug, PartialEq, Eq, Hash, Clone, Copy, error_occurence::PrimitiveErrorOccurencePartialEqEqHashCloneCopy)]
+pub struct StdPrimitiveI32(pub std::primitive::i32);
+#[derive(Debug, PartialEq, Eq, Hash, Clone, Copy, error_occurence::PrimitiveErrorOccurencePartialEqEqHashCloneCopy)]
+pub struct StdPrimitiveU64(pub std::primitive::u64);
+#[derive(Debug, PartialEq, Eq, Hash, Clone, Copy, error_occurence::PrimitiveErrorOccurencePartialEqEqHashCloneCopy)]
+pub struct StdPrimitiveI64(pub std::primitive::i64);
+#[derive(Debug, PartialEq, Eq, Hash, Clone, Copy, error_occurence::PrimitiveErrorOccurencePartialEqEqHashCloneCopy)]
+pub struct StdPrimitiveBool(pub std::primitive::bool);
+#[derive(Debug, PartialEq, Eq, Hash, error_occurence::PrimitiveErrorOccurencePartialEqEqHash)]
+pub struct StdStringString(pub std::string::String);
