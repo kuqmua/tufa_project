294181aa24719939a5766254b749ad134c3ab6d9
kuqmua <serezhashmakov@outlook.com>
2024-05-07 12:41:48 +0300

    next steps

--- error_occurence_lib/src/source_to_string_with_config.rs
+++ error_occurence_lib/src/source_to_string_with_config.rs
@@ -1,10 +1,10 @@
-pub trait SourceToStringWithConfig<'a> {
-    fn source_to_string_with_config<
-        ConfigGeneric: app_state::GetSourcePlaceType
-            + app_state::GetTimezone
-            + ?Sized,
-    >(
-        &self,
-        config: &ConfigGeneric,
-    ) -> std::string::String;
-}
+// pub trait SourceToStringWithConfig<'a> {
+//     fn source_to_string_with_config<
+//         ConfigGeneric: app_state::GetSourcePlaceType
+//             + app_state::GetTimezone
+//             + ?Sized,
+//     >(
+//         &self,
+//         config: &ConfigGeneric,
+//     ) -> std::string::String;
+// }
