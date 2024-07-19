2fa1844699b1b256ce974a8457074c588b0a2929
kuqmua <serezhashmakov@outlook.com>
2024-07-19 22:05:55 +0300

    next steps

--- postgresql_crud/postgresql_crud_common/src/lib.rs
+++ postgresql_crud/postgresql_crud_common/src/lib.rs
@@ -6676,11 +6676,281 @@ impl std::fmt::Display for SqlxPostgresTypesPgRangeSqlxTypesTimeDateWithSerializ
     }
 }
 impl AsPostgresqlDateRange for SqlxPostgresTypesPgRangeSqlxTypesTimeDate {}
-//here8
+
 #[derive(Debug, Clone, PartialEq, Eq, postgresql_crud_types_macro_logic_reuse::CommonWithEqImpl, postgresql_crud_types_macro_logic_reuse::CommonFrom, postgresql_crud_types_macro_logic_reuse::CommonSpecificFromWithEqImpl)]
 pub struct SqlxPostgresTypesPgRangeSqlxTypesBigDecimal(
     pub sqlx::postgres::types::PgRange<sqlx::types::BigDecimal>,
 );
+impl serde::Serialize for SqlxPostgresTypesPgRangeSqlxTypesBigDecimal {
+    fn serialize<__S>(
+        &self,
+        __serializer: __S,
+    ) -> serde::__private::Result<__S::Ok, __S::Error>
+    where
+        __S: serde::Serializer,
+    {
+        let mut __serde_state = serde::Serializer::serialize_struct(
+            __serializer,
+            "SqlxPostgresTypesPgRangeSqlxTypesBigDecimal",
+            false as usize + 1 + 1,
+        )?;
+        serde::ser::SerializeStruct::serialize_field(
+            &mut __serde_state,
+            "start",
+            &match std::ops::RangeBounds::start_bound(&self.0) {
+                std::ops::Bound::Included(value) => std::ops::Bound::Included(SqlxTypesBigDecimal(value.clone())),
+                std::ops::Bound::Excluded(value) => std::ops::Bound::Excluded(SqlxTypesBigDecimal(value.clone())),
+                std::ops::Bound::Unbounded => std::ops::Bound::Unbounded,
+            }
+        )?;
+        serde::ser::SerializeStruct::serialize_field(
+            &mut __serde_state,
+            "end",
+            &match std::ops::RangeBounds::end_bound(&self.0) {
+                std::ops::Bound::Included(value) => std::ops::Bound::Included(SqlxTypesBigDecimal(value.clone())),
+                std::ops::Bound::Excluded(value) => std::ops::Bound::Excluded(SqlxTypesBigDecimal(value.clone())),
+                std::ops::Bound::Unbounded => std::ops::Bound::Unbounded,
+            }
+        )?;
+        serde::ser::SerializeStruct::end(__serde_state)
+    }
+}
+impl<'de> serde::Deserialize<'de> for SqlxPostgresTypesPgRangeSqlxTypesBigDecimal {
+    fn deserialize<__D>(
+        __deserializer: __D,
+    ) -> serde::__private::Result<Self, __D::Error>
+    where
+        __D: serde::Deserializer<'de>,
+    {
+        #[allow(non_camel_case_types)]
+        #[doc(hidden)]
+        enum __Field {
+            __field0,
+            __field1,
+            __ignore,
+        }
+        #[doc(hidden)]
+        struct __FieldVisitor;
+        impl serde::de::Visitor<'_> for __FieldVisitor {
+            type Value = __Field;
+            fn expecting(
+                &self,
+                __formatter: &mut serde::__private::Formatter<'_>,
+            ) -> serde::__private::fmt::Result {
+                serde::__private::Formatter::write_str(
+                    __formatter,
+                    "field identifier",
+                )
+            }
+            fn visit_u64<__E>(
+                self,
+                __value: u64,
+            ) -> serde::__private::Result<Self::Value, __E>
+            where
+                __E: serde::de::Error,
+            {
+                match __value {
+                    0u64 => serde::__private::Ok(__Field::__field0),
+                    1u64 => serde::__private::Ok(__Field::__field1),
+                    _ => serde::__private::Ok(__Field::__ignore),
+                }
+            }
+            fn visit_str<__E>(
+                self,
+                __value: &str,
+            ) -> serde::__private::Result<Self::Value, __E>
+            where
+                __E: serde::de::Error,
+            {
+                match __value {
+                    "start" => serde::__private::Ok(__Field::__field0),
+                    "end" => serde::__private::Ok(__Field::__field1),
+                    _ => serde::__private::Ok(__Field::__ignore),
+                }
+            }
+            fn visit_bytes<__E>(
+                self,
+                __value: &[u8],
+            ) -> serde::__private::Result<Self::Value, __E>
+            where
+                __E: serde::de::Error,
+            {
+                match __value {
+                    b"start" => serde::__private::Ok(__Field::__field0),
+                    b"end" => serde::__private::Ok(__Field::__field1),
+                    _ => serde::__private::Ok(__Field::__ignore),
+                }
+            }
+        }
+        impl<'de> serde::Deserialize<'de> for __Field {
+            #[inline]
+            fn deserialize<__D>(
+                __deserializer: __D,
+            ) -> serde::__private::Result<Self, __D::Error>
+            where
+                __D: serde::Deserializer<'de>,
+            {
+                serde::Deserializer::deserialize_identifier(
+                    __deserializer,
+                    __FieldVisitor,
+                )
+            }
+        }
+        #[doc(hidden)]
+        struct __Visitor<'de> {
+            marker: serde::__private::PhantomData<
+                SqlxPostgresTypesPgRangeSqlxTypesBigDecimal,
+            >,
+            lifetime: serde::__private::PhantomData<&'de ()>,
+        }
+        impl<'de> serde::de::Visitor<'de> for __Visitor<'de> {
+            type Value = SqlxPostgresTypesPgRangeSqlxTypesBigDecimal;
+            fn expecting(
+                &self,
+                __formatter: &mut serde::__private::Formatter<'_>,
+            ) -> serde::__private::fmt::Result {
+                serde::__private::Formatter::write_str(
+                    __formatter,
+                    "struct SqlxPostgresTypesPgRangeSqlxTypesBigDecimal",
+                )
+            }
+            #[inline]
+            fn visit_seq<__A>(
+                self,
+                mut __seq: __A,
+            ) -> serde::__private::Result<Self::Value, __A::Error>
+            where
+                __A: serde::de::SeqAccess<'de>,
+            {
+                let __field0 = match serde::de::SeqAccess::next_element::<
+                    std::ops::Bound<SqlxTypesBigDecimal>,
+                >(&mut __seq)? {
+                    serde::__private::Some(__value) => __value,
+                    serde::__private::None => {
+                        return serde::__private::Err(
+                            serde::de::Error::invalid_length(
+                                0usize,
+                                &"struct SqlxPostgresTypesPgRangeSqlxTypesBigDecimal with 2 elements",
+                            ),
+                        );
+                    }
+                };
+                let __field1 = match serde::de::SeqAccess::next_element::<
+                    std::ops::Bound<SqlxTypesBigDecimal>,
+                >(&mut __seq)? {
+                    serde::__private::Some(__value) => __value,
+                    serde::__private::None => {
+                        return serde::__private::Err(
+                            serde::de::Error::invalid_length(
+                                1usize,
+                                &"struct SqlxPostgresTypesPgRangeSqlxTypesBigDecimal with 2 elements",
+                            ),
+                        );
+                    }
+                };
+                serde::__private::Ok(SqlxPostgresTypesPgRangeSqlxTypesBigDecimal(sqlx::postgres::types::PgRange { 
+                    start: match __field0 {
+                        std::ops::Bound::Included(value) => std::ops::Bound::Included(value.0),
+                        std::ops::Bound::Excluded(value) => std::ops::Bound::Excluded(value.0),
+                        std::ops::Bound::Unbounded => std::ops::Bound::Unbounded,
+                    },
+                    end: match __field1 {
+                        std::ops::Bound::Included(value) => std::ops::Bound::Included(value.0),
+                        std::ops::Bound::Excluded(value) => std::ops::Bound::Excluded(value.0),
+                        std::ops::Bound::Unbounded => std::ops::Bound::Unbounded,
+                    }
+                }))
+            }
+            #[inline]
+            fn visit_map<__A>(
+                self,
+                mut __map: __A,
+            ) -> serde::__private::Result<Self::Value, __A::Error>
+            where
+                __A: serde::de::MapAccess<'de>,
+            {
+                let mut __field0: serde::__private::Option<
+                    std::ops::Bound<SqlxTypesBigDecimal>,
+                > = serde::__private::None;
+                let mut __field1: serde::__private::Option<
+                    std::ops::Bound<SqlxTypesBigDecimal>,
+                > = serde::__private::None;
+                while let serde::__private::Some(__key) = serde::de::MapAccess::next_key::<
+                    __Field,
+                >(&mut __map)? {
+                    match __key {
+                        __Field::__field0 => {
+                            if serde::__private::Option::is_some(&__field0) {
+                                return serde::__private::Err(
+                                    <__A::Error as serde::de::Error>::duplicate_field("start"),
+                                );
+                            }
+                            __field0 = serde::__private::Some(
+                                serde::de::MapAccess::next_value::<
+                                    std::ops::Bound<SqlxTypesBigDecimal>,
+                                >(&mut __map)?,
+                            );
+                        }
+                        __Field::__field1 => {
+                            if serde::__private::Option::is_some(&__field1) {
+                                return serde::__private::Err(
+                                    <__A::Error as serde::de::Error>::duplicate_field("end"),
+                                );
+                            }
+                            __field1 = serde::__private::Some(
+                                serde::de::MapAccess::next_value::<
+                                    std::ops::Bound<SqlxTypesBigDecimal>,
+                                >(&mut __map)?,
+                            );
+                        }
+                        _ => {
+                            let _ = serde::de::MapAccess::next_value::<
+                                serde::de::IgnoredAny,
+                            >(&mut __map)?;
+                        }
+                    }
+                }
+                let __field0 = match __field0 {
+                    serde::__private::Some(__field0) => __field0,
+                    serde::__private::None => {
+                        serde::__private::de::missing_field("start")?
+                    }
+                };
+                let __field1 = match __field1 {
+                    serde::__private::Some(__field1) => __field1,
+                    serde::__private::None => {
+                        serde::__private::de::missing_field("end")?
+                    }
+                };
+                serde::__private::Ok(SqlxPostgresTypesPgRangeSqlxTypesBigDecimal(sqlx::postgres::types::PgRange { 
+                    start: match __field0 {
+                        std::ops::Bound::Included(value) => std::ops::Bound::Included(value.0),
+                        std::ops::Bound::Excluded(value) => std::ops::Bound::Excluded(value.0),
+                        std::ops::Bound::Unbounded => std::ops::Bound::Unbounded,
+                    },
+                    end: match __field1 {
+                        std::ops::Bound::Included(value) => std::ops::Bound::Included(value.0),
+                        std::ops::Bound::Excluded(value) => std::ops::Bound::Excluded(value.0),
+                        std::ops::Bound::Unbounded => std::ops::Bound::Unbounded,
+                    }
+                }))
+            }
+        }
+        #[doc(hidden)]
+        const FIELDS: &'static [&'static str] = &["start", "end"];
+        serde::Deserializer::deserialize_struct(
+            __deserializer,
+            "SqlxPostgresTypesPgRangeSqlxTypesBigDecimal",
+            FIELDS,
+            __Visitor {
+                marker: serde::__private::PhantomData::<
+                    SqlxPostgresTypesPgRangeSqlxTypesBigDecimal,
+                >,
+                lifetime: serde::__private::PhantomData,
+            },
+        )
+    }
+}
 #[derive(Debug, Clone, PartialEq, Eq, serde::Serialize, serde::Deserialize, utoipa::ToSchema)]
 pub struct SqlxPostgresTypesPgRangeSqlxTypesBigDecimalWithSerializeDeserialize {
     start: std::ops::Bound<SqlxTypesBigDecimal>,
