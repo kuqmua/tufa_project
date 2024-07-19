f6cbb74d3af011024f7e51f18cfbb377d325bc4b
kuqmua <serezhashmakov@outlook.com>
2024-07-19 16:14:01 +0300

    next steps

--- postgresql_crud/postgresql_crud_common/src/lib.rs
+++ postgresql_crud/postgresql_crud_common/src/lib.rs
@@ -4288,6 +4288,248 @@ impl std::convert::From<num_bigint::Sign> for NumBigintSignWithSerializeDeserial
     }
 }
 //todo pub or not for all - think
+#[derive(Debug)]
+pub struct NumBigintBigInt(pub num_bigint::BigInt);
+
+impl serde::Serialize for NumBigintBigInt {
+    fn serialize<__S>(
+        &self,
+        __serializer: __S,
+    ) -> serde::__private::Result<__S::Ok, __S::Error>
+    where
+        __S: serde::Serializer,
+    {
+        let (sign, digits) = self.0.to_u32_digits();
+        let mut __serde_state = serde::Serializer::serialize_struct(
+            __serializer,
+            "NumBigintBigInt",
+            false as usize + 1 + 1,
+        )?;
+        serde::ser::SerializeStruct::serialize_field(&mut __serde_state, "sign", &match sign {
+            num_bigint::Sign::Minus => NumBigintSignWithSerializeDeserialize::Minus,
+            num_bigint::Sign::NoSign => NumBigintSignWithSerializeDeserialize::NoSign,
+            num_bigint::Sign::Plus => NumBigintSignWithSerializeDeserialize::Plus,
+        })?;
+        serde::ser::SerializeStruct::serialize_field(
+            &mut __serde_state,
+            "digits",
+            &digits,
+        )?;
+        serde::ser::SerializeStruct::end(__serde_state)
+    }
+}
+impl<'de> serde::Deserialize<'de> for NumBigintBigInt {
+    fn deserialize<__D>(__deserializer: __D) -> serde::__private::Result<Self, __D::Error>
+    where
+        __D: serde::Deserializer<'de>,
+    {
+        #[allow(non_camel_case_types)]
+        #[doc(hidden)]
+        enum __Field {
+            __field0,
+            __field1,
+            __ignore,
+        }
+        #[doc(hidden)]
+        struct __FieldVisitor;
+        impl serde::de::Visitor<'_> for __FieldVisitor {
+            type Value = __Field;
+            fn expecting(
+                &self,
+                __formatter: &mut serde::__private::Formatter<'_>,
+            ) -> serde::__private::fmt::Result {
+                serde::__private::Formatter::write_str(__formatter, "field identifier")
+            }
+            fn visit_u64<__E>(self, __value: u64) -> serde::__private::Result<Self::Value, __E>
+            where
+                __E: serde::de::Error,
+            {
+                match __value {
+                    0u64 => serde::__private::Ok(__Field::__field0),
+                    1u64 => serde::__private::Ok(__Field::__field1),
+                    _ => serde::__private::Ok(__Field::__ignore),
+                }
+            }
+            fn visit_str<__E>(
+                self,
+                __value: &str,
+            ) -> serde::__private::Result<Self::Value, __E>
+            where
+                __E: serde::de::Error,
+            {
+                match __value {
+                    "sign" => serde::__private::Ok(__Field::__field0),
+                    "digits" => serde::__private::Ok(__Field::__field1),
+                    _ => serde::__private::Ok(__Field::__ignore),
+                }
+            }
+            fn visit_bytes<__E>(
+                self,
+                __value: &[u8],
+            ) -> serde::__private::Result<Self::Value, __E>
+            where
+                __E: serde::de::Error,
+            {
+                match __value {
+                    b"sign" => serde::__private::Ok(__Field::__field0),
+                    b"digits" => serde::__private::Ok(__Field::__field1),
+                    _ => serde::__private::Ok(__Field::__ignore),
+                }
+            }
+        }
+        impl<'de> serde::Deserialize<'de> for __Field {
+            #[inline]
+            fn deserialize<__D>(
+                __deserializer: __D,
+            ) -> serde::__private::Result<Self, __D::Error>
+            where
+                __D: serde::Deserializer<'de>,
+            {
+                serde::Deserializer::deserialize_identifier(__deserializer, __FieldVisitor)
+            }
+        }
+        #[doc(hidden)]
+        struct __Visitor<'de> {
+            marker: serde::__private::PhantomData<NumBigintBigInt>,
+            lifetime: serde::__private::PhantomData<&'de ()>,
+        }
+        impl<'de> serde::de::Visitor<'de> for __Visitor<'de> {
+            type Value = NumBigintBigInt;
+            fn expecting(
+                &self,
+                __formatter: &mut serde::__private::Formatter<'_>,
+            ) -> serde::__private::fmt::Result {
+                serde::__private::Formatter::write_str(
+                    __formatter,
+                    "struct NumBigintBigInt",
+                )
+            }
+            #[inline]
+            fn visit_seq<__A>(
+                self,
+                mut __seq: __A,
+            ) -> serde::__private::Result<Self::Value, __A::Error>
+            where
+                __A: serde::de::SeqAccess<'de>,
+            {
+                let __field0 = match serde::de::SeqAccess::next_element::<
+                    NumBigintSignWithSerializeDeserialize,
+                >(&mut __seq)?
+                {
+                    serde::__private::Some(__value) => __value,
+                    serde::__private::None => {
+                        return serde::__private::Err(serde::de::Error::invalid_length(
+                            0usize,
+                            &"struct NumBigintBigInt with 2 elements",
+                        ))
+                    }
+                };
+                let __field1 = match serde::de::SeqAccess::next_element::<
+                    std::vec::Vec<std::primitive::u32>,
+                >(&mut __seq)?
+                {
+                    serde::__private::Some(__value) => __value,
+                    serde::__private::None => {
+                        return serde::__private::Err(serde::de::Error::invalid_length(
+                            1usize,
+                            &"struct NumBigintBigInt with 2 elements",
+                        ))
+                    }
+                };
+                serde::__private::Ok(NumBigintBigInt(num_bigint::BigInt::new(
+                    match __field0 {
+                        NumBigintSignWithSerializeDeserialize::Minus => num_bigint::Sign::Minus,
+                        NumBigintSignWithSerializeDeserialize::NoSign => num_bigint::Sign::NoSign,
+                        NumBigintSignWithSerializeDeserialize::Plus => num_bigint::Sign::Plus,
+                    },
+                    __field1
+                )))
+            }
+            #[inline]
+            fn visit_map<__A>(
+                self,
+                mut __map: __A,
+            ) -> serde::__private::Result<Self::Value, __A::Error>
+            where
+                __A: serde::de::MapAccess<'de>,
+            {
+                let mut __field0: serde::__private::Option<
+                    NumBigintSignWithSerializeDeserialize,
+                > = serde::__private::None;
+                let mut __field1: serde::__private::Option<
+                    std::vec::Vec<std::primitive::u32>,
+                > = serde::__private::None;
+                while let serde::__private::Some(__key) =
+                    serde::de::MapAccess::next_key::<__Field>(&mut __map)?
+                {
+                    match __key {
+                        __Field::__field0 => {
+                            if serde::__private::Option::is_some(&__field0) {
+                                return serde::__private::Err(
+                                    <__A::Error as serde::de::Error>::duplicate_field("sign"),
+                                );
+                            }
+                            __field0 =
+                                serde::__private::Some(serde::de::MapAccess::next_value::<
+                                    NumBigintSignWithSerializeDeserialize,
+                                >(
+                                    &mut __map
+                                )?);
+                        }
+                        __Field::__field1 => {
+                            if serde::__private::Option::is_some(&__field1) {
+                                return serde::__private::Err(
+                                    <__A::Error as serde::de::Error>::duplicate_field(
+                                        "digits",
+                                    ),
+                                );
+                            }
+                            __field1 =
+                                serde::__private::Some(serde::de::MapAccess::next_value::<
+                                    std::vec::Vec<std::primitive::u32>,
+                                >(
+                                    &mut __map
+                                )?);
+                        }
+                        _ => {
+                            let _ = serde::de::MapAccess::next_value::<serde::de::IgnoredAny>(
+                                &mut __map,
+                            )?;
+                        }
+                    }
+                }
+                let __field0 = match __field0 {
+                    serde::__private::Some(__field0) => __field0,
+                    serde::__private::None => serde::__private::de::missing_field("sign")?,
+                };
+                let __field1 = match __field1 {
+                    serde::__private::Some(__field1) => __field1,
+                    serde::__private::None => serde::__private::de::missing_field("digits")?,
+                };
+                serde::__private::Ok(NumBigintBigInt(num_bigint::BigInt::new(
+                    match __field0 {
+                        NumBigintSignWithSerializeDeserialize::Minus => num_bigint::Sign::Minus,
+                        NumBigintSignWithSerializeDeserialize::NoSign => num_bigint::Sign::NoSign,
+                        NumBigintSignWithSerializeDeserialize::Plus => num_bigint::Sign::Plus,
+                    },
+                    __field1
+                )))
+            }
+        }
+        #[doc(hidden)]
+        const FIELDS: &'static [&'static str] = &["sign", "digits"];
+        serde::Deserializer::deserialize_struct(
+            __deserializer,
+            "NumBigintBigInt",
+            FIELDS,
+            __Visitor {
+                marker: serde::__private::PhantomData::<NumBigintBigInt>,
+                lifetime: serde::__private::PhantomData,
+            },
+        )
+    }
+}
+
 #[derive(Debug, Clone, PartialEq, Eq, serde::Serialize, serde::Deserialize, utoipa::ToSchema)]
 pub struct NumBigintBigIntWithSerializeDeserialize {
     sign: NumBigintSignWithSerializeDeserialize,
