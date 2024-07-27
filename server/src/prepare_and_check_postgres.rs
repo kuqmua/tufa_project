b1c706d3eeaec08ccfece2d6c4785e33c3ecc0a6
kuqmua <serezhashmakov@outlook.com>
2024-07-27 16:50:09 +0300

    next steps

--- server/src/prepare_and_check_postgres.rs
+++ server/src/prepare_and_check_postgres.rs
@@ -1,4 +1,5 @@
 pub (crate) async fn prepare_and_check_postgres(pool: &sqlx::Pool<sqlx::Postgres>) {
+    //todo how to check if table schema to potentially create equals to actual postgresql table schema if it exists?
 	let _ = sqlx::query(r#"
         CREATE TABLE IF NOT EXISTS jsongeneric (
           std_primitive_i64_as_postgresql_big_serial_not_null_primary_key BIGSERIAL PRIMARY KEY,
