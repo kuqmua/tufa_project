86cb28f9cd4f8662573bf2cf68f5379bc441f0a3
kuqmua <serezhashmakov@outlook.com>
2024-07-28 16:34:30 +0300

    next steps

--- postgresql_crud/generate_postgresql_crud/src/lib.rs
+++ postgresql_crud/generate_postgresql_crud/src/lib.rs
@@ -3686,7 +3686,7 @@ pub fn generate_postgresql_crud(input: proc_macro::TokenStream) -> proc_macro::T
             quote::quote! {
                 #parameters_token_stream
                 #try_operation_route_logic_token_stream
-                #try_operation_token_stream
+                // #try_operation_token_stream
             },
             // try_operation_test_token_stream,
             quote::quote! {}
@@ -5089,7 +5089,7 @@ pub fn generate_postgresql_crud(input: proc_macro::TokenStream) -> proc_macro::T
 
             #create_many_token_stream
             #create_one_token_stream
-            // #read_many_token_stream
+            #read_many_token_stream
             #read_one_token_stream
             #update_many_token_stream
             #update_one_token_stream
