756c4a9fdfe588ddeca6db5931dd2c64401589df
kuqmua <serezhashmakov@outlook.com>
2024-09-05 19:27:40 +0300

    next steps

--- postgresql_crud/generate_postgresql_query_part/src/lib.rs
+++ postgresql_crud/generate_postgresql_query_part/src/lib.rs
@@ -1993,7 +1993,7 @@ pub fn generate_postgresql_query_part(input: proc_macro::TokenStream) -> proc_ma
             }
         }
     };
-    let pub_struct_ident_options_token_stream = {
+    let pub_struct_ident_options_to_read_token_stream = {
         let fields_token_stream = vec_syn_field.iter().map(|element|{
             let element_ident = element.ident.as_ref().unwrap_or_else(|| {
                 panic!(
@@ -4550,7 +4550,7 @@ pub fn generate_postgresql_query_part(input: proc_macro::TokenStream) -> proc_ma
         #impl_error_occurence_lib_to_std_string_string_for_ident_field_token_stream
         #pub_enum_field_generate_postgresql_query_part_error_named_token_stream
         #impl_generate_postgresql_query_part_to_read_for_ident_field_token_stream
-        #pub_struct_ident_options_token_stream
+        #pub_struct_ident_options_to_read_token_stream
         #impl_std_convert_from_ident_for_ident_options_token_stream
         #impl_serde_deserialize_for_ident_options_token_stream
         #ident_wrapper_token_stream
