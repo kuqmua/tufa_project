dc36bdcfa1d2e695af883455350b0e10ab5c39dd
kuqmua <serezhashmakov@outlook.com>
2024-09-16 09:15:11 +0300

    next steps

--- common/src/repositories_types/server/routes/api/cats/mod.rs
+++ common/src/repositories_types/server/routes/api/cats/mod.rs
@@ -295,7 +295,7 @@ pub struct Something {
     pub std_option_option_std_vec_vec_std_option_option_std_primitive_bool: postgresql_crud::JsonStdOptionOptionStdVecVecStdOptionOptionStdPrimitiveBool,
     pub std_option_option_std_vec_vec_std_option_option_std_string_string: postgresql_crud::JsonStdOptionOptionStdVecVecStdOptionOptionStdStringString,
 
-    // pub generic: postgresql_crud::JsonGeneric<Rat>,
+    pub generic: postgresql_crud::JsonGeneric<Rat>,
     // pub std_option_option_generic: postgresql_crud::JsonStdOptionOptionGeneric<Doggie>,
 
     // pub std_vec_vec_generic_with_id: postgresql_crud::JsonStdVecVecGenericWithId<Doggie>,
@@ -382,12 +382,12 @@ impl<'a> postgresql_crud::BindQuery<'a> for Something {
 //     // pub generic: postgresql_crud::JsonGeneric<Cat>,
 // }
 
-// #[derive(Debug, Clone, PartialEq, Eq, Default, serde::Serialize, serde::Deserialize, utoipa::ToSchema, schemars::JsonSchema,
-//      postgresql_crud::GeneratePostgresqlQueryPart
-// )] //user type must implement utoipa::ToSchema trait
-// pub struct Rat {
-//     pub std_primitive_i16: postgresql_crud::JsonStdPrimitiveI64,
-// }
+#[derive(Debug, Clone, PartialEq, Eq, Default, serde::Serialize, serde::Deserialize, utoipa::ToSchema, schemars::JsonSchema,
+     postgresql_crud::GeneratePostgresqlQueryPart
+)] //user type must implement utoipa::ToSchema trait
+pub struct Rat {
+    pub std_primitive_i16: postgresql_crud::JsonStdPrimitiveI64,
+}
 
 // #[derive(Debug, Clone, PartialEq, Eq, Default, serde::Serialize, serde::Deserialize, utoipa::ToSchema, schemars::JsonSchema,
 //     postgresql_crud::GeneratePostgresqlQueryPart
