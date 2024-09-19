36485b12251e9cbb92a978811c4795c73dd7dcf0
kuqmua <serezhashmakov@outlook.com>
2024-09-19 22:25:56 +0300

    prep for tomorrow

--- postgresql_crud/postgresql_crud_common/src/generate_postgresql_query_part.rs
+++ postgresql_crud/postgresql_crud_common/src/generate_postgresql_query_part.rs
@@ -510,3 +510,320 @@ pub trait JsonArrayElementCreateBindQuery {
 //     )
 // )
 // WHERE id = 1 returning id;
+
+
+
+
+
+///////////////////
+// #[derive(serde::Deserialize)]
+// pub struct Value<T> {
+//     pub value: T
+// }
+
+// #[derive(serde::Deserialize)]
+// pub enum DoggieOptionToUpdate {
+//     StdPrimitiveI16(Value<std::primitive::i16>),
+// }
+
+// #[derive(serde::Deserialize)]
+// pub struct JsonStdPrimitiveI16(pub std::primitive::i16);
+
+// #[derive(serde::Deserialize)]
+// pub struct JsonUuid(pub uuid::Uuid);
+
+// #[derive(serde::Deserialize)]
+// pub struct DoggieToCreate {
+//     pub std_primitive_i16: JsonStdPrimitiveI16,
+// }
+
+// #[derive(serde::Deserialize)]
+// pub struct DoggieOptionsToUpdate {
+//     pub id: JsonUuid,
+//     pub fields: std::vec::Vec<DoggieOptionToUpdate>,
+// }
+
+// pub trait GetJsonId {
+//     fn get_json_id(&self) -> &JsonUuid;
+// }
+
+// // #[derive(serde::Deserialize)]
+// pub struct JsonArrayChange<CreateGeneric, UpdateGeneric> {
+//     pub create: std::vec::Vec<CreateGeneric>,
+//     pub update: std::vec::Vec<UpdateGeneric>,
+//     pub delete: std::vec::Vec<JsonUuid>,
+// }
+
+// impl<'de, CreateGeneric, UpdateGeneric> serde::Deserialize<'de> for JsonArrayChange<CreateGeneric, UpdateGeneric>
+// where
+//     CreateGeneric: serde::Deserialize<'de> + GetJsonId,
+//     UpdateGeneric: serde::Deserialize<'de>,
+// {
+//     fn deserialize<__D>(
+//         __deserializer: __D,
+//     ) -> serde::__private::Result<Self, __D::Error>
+//     where
+//         __D: serde::Deserializer<'de>,
+//     {
+//         #[allow(non_camel_case_types)]
+//         #[doc(hidden)]
+//         enum __Field {
+//             __field0,
+//             __field1,
+//             __field2,
+//             __ignore,
+//         }
+//         #[doc(hidden)]
+//         struct __FieldVisitor;
+//         impl<'de> serde::de::Visitor<'de> for __FieldVisitor {
+//             type Value = __Field;
+//             fn expecting(
+//                 &self,
+//                 __formatter: &mut serde::__private::Formatter,
+//             ) -> serde::__private::fmt::Result {
+//                 serde::__private::Formatter::write_str(
+//                     __formatter,
+//                     "field identifier",
+//                 )
+//             }
+//             fn visit_u64<__E>(
+//                 self,
+//                 __value: u64,
+//             ) -> serde::__private::Result<Self::Value, __E>
+//             where
+//                 __E: serde::de::Error,
+//             {
+//                 match __value {
+//                     0u64 => serde::__private::Ok(__Field::__field0),
+//                     1u64 => serde::__private::Ok(__Field::__field1),
+//                     2u64 => serde::__private::Ok(__Field::__field2),
+//                     _ => serde::__private::Ok(__Field::__ignore),
+//                 }
+//             }
+//             fn visit_str<__E>(
+//                 self,
+//                 __value: &str,
+//             ) -> serde::__private::Result<Self::Value, __E>
+//             where
+//                 __E: serde::de::Error,
+//             {
+//                 match __value {
+//                     "create" => serde::__private::Ok(__Field::__field0),
+//                     "update" => serde::__private::Ok(__Field::__field1),
+//                     "delete" => serde::__private::Ok(__Field::__field2),
+//                     _ => serde::__private::Ok(__Field::__ignore),
+//                 }
+//             }
+//             fn visit_bytes<__E>(
+//                 self,
+//                 __value: &[u8],
+//             ) -> serde::__private::Result<Self::Value, __E>
+//             where
+//                 __E: serde::de::Error,
+//             {
+//                 match __value {
+//                     b"create" => serde::__private::Ok(__Field::__field0),
+//                     b"update" => serde::__private::Ok(__Field::__field1),
+//                     b"delete" => serde::__private::Ok(__Field::__field2),
+//                     _ => serde::__private::Ok(__Field::__ignore),
+//                 }
+//             }
+//         }
+//         impl<'de> serde::Deserialize<'de> for __Field {
+//             #[inline]
+//             fn deserialize<__D>(
+//                 __deserializer: __D,
+//             ) -> serde::__private::Result<Self, __D::Error>
+//             where
+//                 __D: serde::Deserializer<'de>,
+//             {
+//                 serde::Deserializer::deserialize_identifier(
+//                     __deserializer,
+//                     __FieldVisitor,
+//                 )
+//             }
+//         }
+//         #[doc(hidden)]
+//         struct __Visitor<'de, CreateGeneric, UpdateGeneric>
+//         where
+//             CreateGeneric: serde::Deserialize<'de>,
+//             UpdateGeneric: serde::Deserialize<'de>,
+//         {
+//             marker: serde::__private::PhantomData<
+//                 JsonArrayChange<CreateGeneric, UpdateGeneric>,
+//             >,
+//             lifetime: serde::__private::PhantomData<&'de ()>,
+//         }
+//         impl<'de, CreateGeneric, UpdateGeneric> serde::de::Visitor<'de>
+//         for __Visitor<'de, CreateGeneric, UpdateGeneric>
+//         where
+//             CreateGeneric: serde::Deserialize<'de>,
+//             UpdateGeneric: serde::Deserialize<'de>,
+//         {
+//             type Value = JsonArrayChange<CreateGeneric, UpdateGeneric>;
+//             fn expecting(
+//                 &self,
+//                 __formatter: &mut serde::__private::Formatter,
+//             ) -> serde::__private::fmt::Result {
+//                 serde::__private::Formatter::write_str(
+//                     __formatter,
+//                     "struct JsonArrayChange",
+//                 )
+//             }
+//             #[inline]
+//             fn visit_seq<__A>(
+//                 self,
+//                 mut __seq: __A,
+//             ) -> serde::__private::Result<Self::Value, __A::Error>
+//             where
+//                 __A: serde::de::SeqAccess<'de>,
+//             {
+//                 let __field0 = match serde::de::SeqAccess::next_element::<
+//                     std::vec::Vec<CreateGeneric>,
+//                 >(&mut __seq)? {
+//                     serde::__private::Some(__value) => __value,
+//                     serde::__private::None => {
+//                         return serde::__private::Err(
+//                             serde::de::Error::invalid_length(
+//                                 0usize,
+//                                 &"struct JsonArrayChange with 3 elements",
+//                             ),
+//                         );
+//                     }
+//                 };
+//                 let __field1 = match serde::de::SeqAccess::next_element::<
+//                     std::vec::Vec<UpdateGeneric>,
+//                 >(&mut __seq)? {
+//                     serde::__private::Some(__value) => __value,
+//                     serde::__private::None => {
+//                         return serde::__private::Err(
+//                             serde::de::Error::invalid_length(
+//                                 1usize,
+//                                 &"struct JsonArrayChange with 3 elements",
+//                             ),
+//                         );
+//                     }
+//                 };
+//                 let __field2 = match serde::de::SeqAccess::next_element::<
+//                     std::vec::Vec<JsonUuid>,
+//                 >(&mut __seq)? {
+//                     serde::__private::Some(__value) => __value,
+//                     serde::__private::None => {
+//                         return serde::__private::Err(
+//                             serde::de::Error::invalid_length(
+//                                 2usize,
+//                                 &"struct JsonArrayChange with 3 elements",
+//                             ),
+//                         );
+//                     }
+//                 };
+//                 serde::__private::Ok(JsonArrayChange {
+//                     create: __field0,
+//                     update: __field1,
+//                     delete: __field2,
+//                 })
+//             }
+//             #[inline]
+//             fn visit_map<__A>(
+//                 self,
+//                 mut __map: __A,
+//             ) -> serde::__private::Result<Self::Value, __A::Error>
+//             where
+//                 __A: serde::de::MapAccess<'de>,
+//             {
+//                 let mut __field0: serde::__private::Option<
+//                     std::vec::Vec<CreateGeneric>,
+//                 > = serde::__private::None;
+//                 let mut __field1: serde::__private::Option<
+//                     std::vec::Vec<UpdateGeneric>,
+//                 > = serde::__private::None;
+//                 let mut __field2: serde::__private::Option<
+//                     std::vec::Vec<JsonUuid>,
+//                 > = serde::__private::None;
+//                 while let serde::__private::Some(__key) = serde::de::MapAccess::next_key::<
+//                     __Field,
+//                 >(&mut __map)? {
+//                     match __key {
+//                         __Field::__field0 => {
+//                             if serde::__private::Option::is_some(&__field0) {
+//                                 return serde::__private::Err(
+//                                     <__A::Error as serde::de::Error>::duplicate_field("create"),
+//                                 );
+//                             }
+//                             __field0 = serde::__private::Some(
+//                                 serde::de::MapAccess::next_value::<
+//                                     std::vec::Vec<CreateGeneric>,
+//                                 >(&mut __map)?,
+//                             );
+//                         }
+//                         __Field::__field1 => {
+//                             if serde::__private::Option::is_some(&__field1) {
+//                                 return serde::__private::Err(
+//                                     <__A::Error as serde::de::Error>::duplicate_field("update"),
+//                                 );
+//                             }
+//                             __field1 = serde::__private::Some(
+//                                 serde::de::MapAccess::next_value::<
+//                                     std::vec::Vec<UpdateGeneric>,
+//                                 >(&mut __map)?,
+//                             );
+//                         }
+//                         __Field::__field2 => {
+//                             if serde::__private::Option::is_some(&__field2) {
+//                                 return serde::__private::Err(
+//                                     <__A::Error as serde::de::Error>::duplicate_field("delete"),
+//                                 );
+//                             }
+//                             __field2 = serde::__private::Some(
+//                                 serde::de::MapAccess::next_value::<
+//                                     std::vec::Vec<JsonUuid>,
+//                                 >(&mut __map)?,
+//                             );
+//                         }
+//                         _ => {
+//                             let _ = serde::de::MapAccess::next_value::<
+//                                 serde::de::IgnoredAny,
+//                             >(&mut __map)?;
+//                         }
+//                     }
+//                 }
+//                 let __field0 = match __field0 {
+//                     serde::__private::Some(__field0) => __field0,
+//                     serde::__private::None => {
+//                         serde::__private::de::missing_field("create")?
+//                     }
+//                 };
+//                 let __field1 = match __field1 {
+//                     serde::__private::Some(__field1) => __field1,
+//                     serde::__private::None => {
+//                         serde::__private::de::missing_field("update")?
+//                     }
+//                 };
+//                 let __field2 = match __field2 {
+//                     serde::__private::Some(__field2) => __field2,
+//                     serde::__private::None => {
+//                         serde::__private::de::missing_field("delete")?
+//                     }
+//                 };
+//                 serde::__private::Ok(JsonArrayChange {
+//                     create: __field0,
+//                     update: __field1,
+//                     delete: __field2,
+//                 })
+//             }
+//         }
+//         #[doc(hidden)]
+//         const FIELDS: &'static [&'static str] = &["create", "update", "delete"];
+//         serde::Deserializer::deserialize_struct(
+//             __deserializer,
+//             "JsonArrayChange",
+//             FIELDS,
+//             __Visitor {
+//                 marker: serde::__private::PhantomData::<
+//                     JsonArrayChange<CreateGeneric, UpdateGeneric>,
+//                 >,
+//                 lifetime: serde::__private::PhantomData,
+//             },
+//         )
+//     }
+// }
