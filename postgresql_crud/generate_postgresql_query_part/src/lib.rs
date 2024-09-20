5144686f2c00a31932e5c5c91b7da17ebc1fab3e
kuqmua <serezhashmakov@outlook.com>
2024-09-20 20:10:15 +0300

    next steps

--- postgresql_crud/generate_postgresql_query_part/src/lib.rs
+++ postgresql_crud/generate_postgresql_query_part/src/lib.rs
@@ -4928,7 +4928,28 @@ pub fn generate_postgresql_query_part(input: proc_macro::TokenStream) -> proc_ma
                     }
                 },
                 SupportedPredefinedType::JsonStdOptionOptionGeneric(type_path) => {
-                    todo!()
+                    //here just copypaste
+                    let format_handle_token_stream = proc_macro_common::generate_quotes::double_quotes_token_stream(
+                        &format!("'{{{{{}}}}}',${{increment}}", element_ident),
+                        &proc_macro_name_upper_camel_case_ident_stringified
+                    );
+                    quote::quote!{
+                        #ident_option_to_update_upper_camel_case_token_stream::#element_ident_upper_camel_case_token_stream(_) => {
+                            match increment.checked_add(1) {
+                                Some(value) => {
+                                    *increment = value;
+                                    acc.push_str(&format!(#format_handle_token_stream));
+                                }
+                                None => {
+                                    return Err(
+                                        #ident_try_generate_json_array_element_update_bind_increments_error_named_upper_camel_case_token_stream::CheckedAdd {
+                                            code_occurence: error_occurence_lib::code_occurence!(),
+                                        },
+                                    );
+                                }
+                            }
+                        }
+                    }
                 },
                 SupportedPredefinedType::JsonStdVecVecGenericWithId(type_path) => {
                     todo!()
@@ -5057,7 +5078,12 @@ pub fn generate_postgresql_query_part(input: proc_macro::TokenStream) -> proc_ma
                     }
                 },
                 SupportedPredefinedType::JsonStdOptionOptionGeneric(type_path) => {
-                    todo!()
+                    //here just copypaste
+                    quote::quote!{
+                        #ident_option_to_update_upper_camel_case_token_stream::#element_ident_upper_camel_case_token_stream(value) => {
+                            query = query.bind(sqlx::types::Json(value.value));
+                        }
+                    }
                 },
                 SupportedPredefinedType::JsonStdVecVecGenericWithId(type_path) => {
                     todo!()
