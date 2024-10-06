9e16943252f93cdb56eb85193f02c0721b9e4698
kuqmua <serezhashmakov@outlook.com>
2024-10-06 12:17:37 +0300

    next steps

--- postgresql_crud/postgresql_crud_common/src/generate_postgresql_query_part.rs
+++ postgresql_crud/postgresql_crud_common/src/generate_postgresql_query_part.rs
@@ -16,6 +16,7 @@
     postgresql_crud_types_macro_logic_reuse::GenerateIdentOptionsToRead,
     postgresql_crud_types_macro_logic_reuse::GenerateStdDefaultDefaultButStdOptionOptionIsAlwaysSomeAndStdVecVecAlwaysContainsOneElementFullTypePathOptionsToRead,
     postgresql_crud_types_macro_logic_reuse::GenerateImplGeneratePostgresqlQueryPartFieldToReadForIdentFieldReaderNumber,
+    // postgresql_crud_types_macro_logic_reuse::GenerateStdDefaultDefaultButStdOptionOptionIsAlwaysSomeAndStdVecVecAlwaysContainsOneElementFullTypePathFieldReader,
 )]
 pub struct JsonStdPrimitiveI8(#[validate(range(min = -128i8, max = 127i8))] pub std::primitive::i8);
 
