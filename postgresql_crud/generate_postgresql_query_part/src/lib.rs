3a5d1d72017bb86d2dac6c0d7b541b9ab145d974
kuqmua <serezhashmakov@outlook.com>
2024-10-23 12:32:38 +0300

    impl_postgresql_crud_json_create_bind_query_for_ident_to_create_origin_without_generated_id_token_stream

--- postgresql_crud/generate_postgresql_query_part/src/lib.rs
+++ postgresql_crud/generate_postgresql_query_part/src/lib.rs
@@ -1437,6 +1437,10 @@ pub fn generate_postgresql_query_part(input: proc_macro::TokenStream) -> proc_ma
         &ident_to_create_origin_with_generated_id_upper_camel_case,
         true
     );
+    let impl_postgresql_crud_json_create_bind_query_for_ident_to_create_origin_without_generated_id_token_stream = generate_impl_postgresql_crud_json_create_bind_query_for_tokens_to_create_token_stream(
+        &ident_to_create_origin_without_generated_id_upper_camel_case,
+        false
+    );
     
     
     let ident_options_to_update_upper_camel_case = naming_conventions::SelfOptionsToUpdateUpperCamelCase::from_dyn_quote_to_tokens(&ident);
@@ -5175,6 +5179,7 @@ pub fn generate_postgresql_query_part(input: proc_macro::TokenStream) -> proc_ma
         #impl_postgresql_crud_std_default_default_but_std_option_option_is_always_some_and_std_vec_vec_always_contains_one_element_for_ident_to_create_origin_with_generated_id_token_stream
         #impl_postgresql_crud_std_default_default_but_std_option_option_is_always_some_and_std_vec_vec_always_contains_one_element_for_ident_to_create_origin_without_generated_id_token_stream
         #impl_postgresql_crud_json_create_bind_query_for_ident_to_create_origin_with_generated_id_token_stream
+        #impl_postgresql_crud_json_create_bind_query_for_ident_to_create_origin_without_generated_id_token_stream 
 
 
         #ident_field_to_read_token_stream
