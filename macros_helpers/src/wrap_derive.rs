483bf55bbfc3f539ba093876b6c60c19fc98dbfb
kuqmua <serezhashmakov@outlook.com>
2024-11-11 16:21:11 +0300

    renaming

--- macros_helpers/src/wrap_derive.rs
+++ macros_helpers/src/wrap_derive.rs
@@ -1,3 +1,3 @@
-pub fn token_stream(values: &[&proc_macro2::TokenStream]) -> proc_macro2::TokenStream {
+pub fn wrap_derive(values: &[&proc_macro2::TokenStream]) -> proc_macro2::TokenStream {
     quote::quote! {#[derive(#(#values),*)]}
 }
