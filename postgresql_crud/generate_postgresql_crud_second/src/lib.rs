497de7e8a680b3de4abe38c51bc2f4588ca888ce
kuqmua <serezhashmakov@outlook.com>
2024-11-21 13:38:40 +0300

    read_many_payload_example

--- postgresql_crud/generate_postgresql_crud_second/src/lib.rs
+++ postgresql_crud/generate_postgresql_crud_second/src/lib.rs
@@ -1505,7 +1505,7 @@ pub fn generate_postgresql_crud_second(input: proc_macro::TokenStream) -> proc_m
     let response_snake_case = naming_conventions::ResponseSnakeCase;
     let status_code_snake_case = naming_conventions::StatusCodeSnakeCase;
     let body_snake_case = naming_conventions::BodySnakeCase;
-    let limit_and_offset_type_token_stream = quote::quote!{postgresql_crud::StdPrimitiveI64};
+    let limit_and_offset_type_token_stream = quote::quote!{postgresql_crud::postgresql_types::StdPrimitiveI64};
     // {
     //     let value = postgresql_crud_common::SupportedSqlxPostgresType::StdPrimitiveI64.get_inner_type_stringified("");
     //     value
@@ -3782,11 +3782,32 @@ pub fn generate_postgresql_crud_second(input: proc_macro::TokenStream) -> proc_m
             )
         };
         // println!("{try_operation_token_stream}");
+        // let impl_postgresql_crud_std_default_default_but_std_option_option_is_always_some_and_std_vec_vec_always_contains_one_element_for_operation_payload_token_stream = {
+        //     let impl_postgresql_crud_std_default_default_but_std_option_option_is_always_some_and_std_vec_vec_always_contains_one_element_for_operation_payload_element_token_stream =
+        //         wrap_into_impl_postgresql_crud_std_default_default_but_std_option_option_is_always_some_and_std_vec_vec_always_contains_one_element_for_value_token_stream(
+        //             &naming_conventions::SelfPayloadElementUpperCamelCase::from_dyn_std_fmt_display(&operation),
+        //             &fields_initialiation_excluding_primary_key_with_default_but_std_option_option_is_always_some_and_std_vec_vec_always_contains_one_element_curly_braces_token_stream,
+        //         );
+        //     let impl_postgresql_crud_std_default_default_but_std_option_option_is_always_some_and_std_vec_vec_always_contains_one_element_for_operation_payload_token_stream =
+        //         wrap_into_impl_postgresql_crud_std_default_default_but_std_option_option_is_always_some_and_std_vec_vec_always_contains_one_element_for_value_token_stream(
+        //             &naming_conventions::SelfPayloadUpperCamelCase::from_dyn_std_fmt_display(&operation),
+        //             &quote::quote! {(vec![postgresql_crud::StdDefaultDefaultButStdOptionOptionIsAlwaysSomeAndStdVecVecAlwaysContainsOneElement::default_but_std_option_option_is_always_some_and_std_vec_vec_always_contains_one_element()])},
+        //         );
+        //     quote::quote! {
+        //         #impl_postgresql_crud_std_default_default_but_std_option_option_is_always_some_and_std_vec_vec_always_contains_one_element_for_operation_payload_element_token_stream
+        //         #impl_postgresql_crud_std_default_default_but_std_option_option_is_always_some_and_std_vec_vec_always_contains_one_element_for_operation_payload_token_stream
+        //     }
+        // };
+        // println!("{impl_postgresql_crud_std_default_default_but_std_option_option_is_always_some_and_std_vec_vec_always_contains_one_element_for_operation_payload_token_stream}");
+        let operation_payload_example_route_logic_token_stream = generate_operation_payload_example_route_logic_token_stream(&operation);
+        // println!("{operation_payload_example_route_logic_token_stream}");
         (
             quote::quote! {
                 #parameters_token_stream
                 #try_operation_route_logic_token_stream
                 #try_operation_token_stream
+                // #impl_postgresql_crud_std_default_default_but_std_option_option_is_always_some_and_std_vec_vec_always_contains_one_element_for_operation_payload_token_stream
+                #operation_payload_example_route_logic_token_stream
             },
             // try_operation_test_token_stream,
             quote::quote! {},
