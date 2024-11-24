58737e2dd1066df1845f8f5cd1844bac9ab2dd2d
kuqmua <serezhashmakov@outlook.com>
2024-11-24 20:49:03 +0300

    next steps

--- postgresql_crud/postgresql_crud_common/src/postgresql_types/base.rs
+++ postgresql_crud/postgresql_crud_common/src/postgresql_types/base.rs
@@ -8,7 +8,6 @@
     serde::Deserialize,
     utoipa::ToSchema,
     postgresql_crud_types_macro_logic_reuse::PostgresqlCrudBaseTypeTokens,
-    // postgresql_crud_types_macro_logic_reuse::GenerateStdDefaultDefaultButStdOptionOptionIsAlwaysSomeAndStdVecVecAlwaysContainsOneElementFullTypePath,
 )]
 pub(crate) struct StdPrimitiveBool(std::primitive::bool); //todo maybe make it private? //todo column "std_primitive_bool_as_postgresql_bool" is of type boolean but expression is of type bigint
 
@@ -23,6 +22,62 @@ pub(crate) struct StdPrimitiveBool(std::primitive::bool); //todo maybe make it p
     serde::Serialize,
     serde::Deserialize,
     postgresql_crud_types_macro_logic_reuse::PostgresqlCrudBaseTypeTokens,
-    // postgresql_crud_types_macro_logic_reuse::GenerateStdDefaultDefaultButStdOptionOptionIsAlwaysSomeAndStdVecVecAlwaysContainsOneElementFullTypePath,
 )]
 pub(crate) struct StdPrimitiveI64(std::primitive::i64);
+
+
+
+
+//
+// pub struct StdPrimitiveI16(pub std::primitive::i16);
+// pub struct StdPrimitiveI32(pub std::primitive::i32);
+// pub struct StdPrimitiveF32(pub std::primitive::f32);
+// pub struct StdPrimitiveF64(pub std::primitive::f64);
+// pub struct StdStringString(pub std::string::String);
+// // #[derive(Debug, Clone, PartialEq, Eq,
+// //     serde::Serialize,
+// //     serde::Deserialize, postgresql_crud_types_macro_logic_reuse::CommonWithEqImpl)]
+// // pub struct StdVecVecStdPrimitiveU8(pub std::vec::Vec<std::primitive::u8>);
+// // impl AsPostgresqlBytea for StdVecVecStdPrimitiveU8 {}
+// pub struct SqlxPostgresTypesPgInterval(pub sqlx::postgres::types::PgInterval);
+// pub struct SqlxPostgresTypesPgRangeStdPrimitiveI64(pub sqlx::postgres::types::PgRange<std::primitive::i64>);
+// pub struct SqlxPostgresTypesPgRangeStdPrimitiveI32(pub sqlx::postgres::types::PgRange<std::primitive::i32>);
+// pub struct SqlxPostgresTypesPgRangeSqlxTypesChronoDateTimeSqlxTypesChronoUtc(pub sqlx::postgres::types::PgRange<sqlx::types::chrono::DateTime<sqlx::types::chrono::Utc>>);
+// pub struct SqlxPostgresTypesPgRangeSqlxTypesChronoDateTimeSqlxTypesChronoLocal(pub sqlx::postgres::types::PgRange<sqlx::types::chrono::DateTime<sqlx::types::chrono::Local>>);
+// pub struct SqlxPostgresTypesPgRangeSqlxTypesTimeOffsetDateTime(pub sqlx::postgres::types::PgRange<sqlx::types::time::OffsetDateTime>);
+// pub struct SqlxPostgresTypesPgRangeSqlxTypesChronoNaiveDateTime(pub sqlx::postgres::types::PgRange<sqlx::types::chrono::NaiveDateTime>);
+// pub struct SqlxPostgresTypesPgRangeSqlxTypesTimePrimitiveDateTime(pub sqlx::postgres::types::PgRange<sqlx::types::time::PrimitiveDateTime>);
+// pub struct SqlxPostgresTypesPgRangeSqlxTypesChronoNaiveDate(pub sqlx::postgres::types::PgRange<sqlx::types::chrono::NaiveDate>);
+// pub struct SqlxPostgresTypesPgRangeSqlxTypesTimeDate(pub sqlx::postgres::types::PgRange<sqlx::types::time::Date>);
+// pub struct SqlxPostgresTypesPgRangeSqlxTypesBigDecimal(pub sqlx::postgres::types::PgRange<sqlx::types::BigDecimal>);
+// pub struct SqlxPostgresTypesPgRangeSqlxTypesDecimal(pub sqlx::postgres::types::PgRange<sqlx::types::Decimal>);
+// pub struct SqlxPostgresTypesPgMoney(pub sqlx::postgres::types::PgMoney);
+// pub struct SqlxPostgresTypesPgCiText(pub sqlx::postgres::types::PgCiText);
+// pub struct SqlxTypesBigDecimal(pub sqlx::types::BigDecimal);
+// pub struct SqlxTypesDecimal(pub sqlx::types::Decimal);
+// pub struct SqlxTypesChronoDateTimeSqlxTypesChronoUtc(pub sqlx::types::chrono::DateTime<sqlx::types::chrono::Utc>);
+// pub struct SqlxTypesChronoDateTimeSqlxTypesChronoLocal(pub sqlx::types::chrono::DateTime<sqlx::types::chrono::Local>);
+// pub struct SqlxTypesChronoNaiveDateTime(pub sqlx::types::chrono::NaiveDateTime);
+// pub struct SqlxTypesChronoNaiveDate(pub sqlx::types::chrono::NaiveDate);
+// pub struct SqlxTypesChronoNaiveTime(pub sqlx::types::chrono::NaiveTime);
+// pub struct SqlxPostgresTypesPgTimeTz(pub sqlx::postgres::types::PgTimeTz);
+// pub struct SqlxTypesTimePrimitiveDateTime(pub sqlx::types::time::PrimitiveDateTime);
+// pub struct SqlxTypesTimeOffsetDateTime(pub sqlx::types::time::OffsetDateTime);
+// pub struct SqlxTypesTimeDate(pub sqlx::types::time::Date);
+// pub struct SqlxTypesTimeTime(pub sqlx::types::time::Time);
+// pub struct SqlxTypesUuidUuid(pub sqlx::types::uuid::Uuid);
+// pub struct SqlxTypesIpnetworkIpNetwork(pub sqlx::types::ipnetwork::IpNetwork);
+// pub struct StdNetIpAddr(pub std::net::IpAddr);
+// pub struct SqlxTypesMacAddressMacAddress(pub sqlx::types::mac_address::MacAddress);
+// pub struct SqlxTypesBitVec(pub sqlx::types::BitVec);
+// pub struct SqlxTypesJson<T>(pub sqlx::types::Json<T>);
+// pub struct WhereSqlxTypesJson<T> {
+//     pub value: SqlxTypesJson<T>,
+//     pub conjuctive_operator: ConjunctiveOperator,
+// }
+// pub struct StdOptionOptionSqlxTypesJson<T>(pub std::option::Option<sqlx::types::Json<T>>);
+// pub struct WhereStdOptionOptionSqlxTypesJson<T> {
+//     pub value: StdOptionOptionSqlxTypesJson<T>,
+//     pub conjuctive_operator: ConjunctiveOperator,
+// }
+// pub struct SerdeJsonValue(pub serde_json::Value);
