e6acb5e890d89dfcf687f15adb401e103de83d70
kuqmua <serezhashmakov@outlook.com>
2024-12-22 11:45:43 +0300

    next steps

--- naming/src/parameter.rs
+++ naming/src/parameter.rs
@@ -233,7 +233,8 @@ naming_macros::generate_self_upper_camel_and_snake_case_stringified_and_token_st
     ["postgresql", "type", "self", "where", "element", "in"],
     ["postgresql", "type", "self", "where", "element", "in", "try", "new", "error", "named"],
     ["postgresql", "type", "std", "option", "option", "self", "where", "element", "equal"],
-    ["postgresql", "type", "std", "option", "option", "self", "where", "element", "greater", "than"]
+    ["postgresql", "type", "std", "option", "option", "self", "where", "element", "greater", "than"],
+    ["postgresql", "type", "std", "option", "option", "self", "where", "element", "between"]
 ]);
 
 ////////////////////////////////////////////////////
