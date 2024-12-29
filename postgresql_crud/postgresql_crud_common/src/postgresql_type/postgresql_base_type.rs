d45cd360aea03093fab42b88c9a1fe32779e6c90
kuqmua <serezhashmakov@outlook.com>
2024-12-29 20:09:58 +0300

    next steps

--- postgresql_crud/postgresql_crud_common/src/postgresql_type/postgresql_base_type.rs
+++ postgresql_crud/postgresql_crud_common/src/postgresql_type/postgresql_base_type.rs
@@ -1772,24 +1772,107 @@ const _: () = {
     }
 };
 
+//todo another ranges
+
+#[derive(
+    Debug,
+    Clone,
+    PartialEq,
+    Eq,
+    // postgresql_crud_types_macro_logic_reuse::PostgresqlBaseTypeTokens,
+    // postgresql_crud_types_macro_logic_reuse::PostgresqlBaseTypeTokensWhereElement,
+)]
+pub struct SqlxTypesTimeOffsetDateTime(pub sqlx::types::time::OffsetDateTime);
+impl serde::Serialize for SqlxTypesTimeOffsetDateTime {
+    fn serialize<__S>(&self, __serializer: __S) -> serde::__private::Result<__S::Ok, __S::Error>
+    where
+        __S: serde::Serializer,
+    {
+        serde::Serializer::serialize_newtype_struct(__serializer, "SqlxTypesTimeOffsetDateTime", &self.0.unix_timestamp())
+    }
+}
+impl<'de> serde::Deserialize<'de> for SqlxTypesTimeOffsetDateTime {
+    fn deserialize<__D>(__deserializer: __D) -> serde::__private::Result<Self, __D::Error>
+    where
+        __D: serde::Deserializer<'de>,
+    {
+        #[doc(hidden)]
+        struct __Visitor<'de> {
+            marker: serde::__private::PhantomData<SqlxTypesTimeOffsetDateTime>,
+            lifetime: serde::__private::PhantomData<&'de ()>,
+        }
+        impl<'de> serde::de::Visitor<'de> for __Visitor<'de> {
+            type Value = SqlxTypesTimeOffsetDateTime;
+            fn expecting(&self, __formatter: &mut serde::__private::Formatter<'_>) -> serde::__private::fmt::Result {
+                serde::__private::Formatter::write_str(__formatter, "tuple struct SqlxTypesTimeOffsetDateTime")
+            }
+            #[inline]
+            fn visit_newtype_struct<__E>(self, __e: __E) -> serde::__private::Result<Self::Value, __E::Error>
+            where
+                __E: serde::Deserializer<'de>,
+            {
+                let __field0: std::primitive::i64 = <std::primitive::i64 as serde::Deserialize>::deserialize(__e)?;
+                serde::__private::Ok(SqlxTypesTimeOffsetDateTime(match sqlx::types::time::OffsetDateTime::from_unix_timestamp(__field0) {
+                    Ok(value) => value,
+                    Err(error) => {
+                        return Err(serde::de::Error::custom(error));
+                    }
+                }))
+            }
+            #[inline]
+            fn visit_seq<__A>(self, mut __seq: __A) -> serde::__private::Result<Self::Value, __A::Error>
+            where
+                __A: serde::de::SeqAccess<'de>,
+            {
+                let __field0 = match serde::de::SeqAccess::next_element::<std::primitive::i64>(&mut __seq)? {
+                    serde::__private::Some(__value) => __value,
+                    serde::__private::None => {
+                        return serde::__private::Err(serde::de::Error::invalid_length(0usize, &"tuple struct SqlxTypesTimeOffsetDateTime with 1 element"));
+                    }
+                };
+                serde::__private::Ok(SqlxTypesTimeOffsetDateTime(match sqlx::types::time::OffsetDateTime::from_unix_timestamp(__field0) {
+                    Ok(value) => value,
+                    Err(error) => {
+                        return Err(serde::de::Error::custom(error));
+                    }
+                }))
+            }
+        }
+        serde::Deserializer::deserialize_newtype_struct(
+            __deserializer,
+            "SqlxTypesTimeOffsetDateTime",
+            __Visitor {
+                marker: serde::__private::PhantomData::<SqlxTypesTimeOffsetDateTime>,
+                lifetime: serde::__private::PhantomData,
+            },
+        )
+    }
+}
+impl crate::generate_postgresql_json_type::StdDefaultDefaultButStdOptionOptionIsAlwaysSomeAndStdVecVecAlwaysContainsOneElement for SqlxTypesTimeOffsetDateTime {
+    #[inline]
+    fn std_default_default_but_std_option_option_is_always_some_and_std_vec_vec_always_contains_one_element() -> Self {
+        Self(sqlx::types::time::OffsetDateTime::UNIX_EPOCH)
+    }
+}
+
+
+
+
+// pub struct SqlxTypesTimePrimitiveDateTime(pub sqlx::types::time::PrimitiveDateTime);
+// pub struct SqlxTypesTimeDate(pub sqlx::types::time::Date);
+// pub struct SqlxTypesBigDecimal(pub sqlx::types::BigDecimal);
 
 
 // pub struct SqlxPostgresTypesPgRangeSqlxTypesTimeOffsetDateTime(pub sqlx::postgres::types::PgRange<sqlx::types::time::OffsetDateTime>);
 // pub struct SqlxPostgresTypesPgRangeSqlxTypesTimePrimitiveDateTime(pub sqlx::postgres::types::PgRange<sqlx::types::time::PrimitiveDateTime>);
 // pub struct SqlxPostgresTypesPgRangeSqlxTypesTimeDate(pub sqlx::postgres::types::PgRange<sqlx::types::time::Date>);
-
 // pub struct SqlxPostgresTypesPgRangeSqlxTypesBigDecimal(pub sqlx::postgres::types::PgRange<sqlx::types::BigDecimal>);
 
 
 // pub struct SqlxPostgresTypesPgMoney(pub sqlx::postgres::types::PgMoney);
 // pub struct SqlxPostgresTypesPgCiText(pub sqlx::postgres::types::PgCiText);
 
-// pub struct SqlxTypesBigDecimal(pub sqlx::types::BigDecimal);
 // pub struct SqlxTypesDecimal(pub sqlx::types::Decimal);
-
-
-
-
 
 
 // pub struct SqlxTypesChronoDateTimeSqlxTypesChronoUtc(pub sqlx::types::chrono::DateTime<sqlx::types::chrono::Utc>);
@@ -1798,9 +1881,9 @@ const _: () = {
 // pub struct SqlxTypesChronoNaiveDate(pub sqlx::types::chrono::NaiveDate);
 // pub struct SqlxTypesChronoNaiveTime(pub sqlx::types::chrono::NaiveTime);
 // pub struct SqlxPostgresTypesPgTimeTz(pub sqlx::postgres::types::PgTimeTz);
-// pub struct SqlxTypesTimePrimitiveDateTime(pub sqlx::types::time::PrimitiveDateTime);
-// pub struct SqlxTypesTimeOffsetDateTime(pub sqlx::types::time::OffsetDateTime);
-// pub struct SqlxTypesTimeDate(pub sqlx::types::time::Date);
+
+
+
 // pub struct SqlxTypesTimeTime(pub sqlx::types::time::Time);
 
 
