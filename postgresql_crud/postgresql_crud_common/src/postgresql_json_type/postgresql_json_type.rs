6ef376dbd49005db8d42684709f5f87bb7602fe5
kuqmua <serezhashmakov@outlook.com>
2025-01-19 11:51:57 +0300

    next steps

--- postgresql_crud/postgresql_crud_common/src/postgresql_json_type/postgresql_json_type.rs
+++ postgresql_crud/postgresql_crud_common/src/postgresql_json_type/postgresql_json_type.rs
@@ -14,163 +14,174 @@
     postgresql_crud_types_macro_logic_reuse::GeneratePostgresqlJsonTypeWhereElement,
 )]
 pub struct StdPrimitiveI8(#[validate(range(min = -128i8, max = 127i8))] pub std::primitive::i8);
-// #[derive(
-//     Debug,
-//     Clone,
-//     Copy,
-//     PartialEq,
-//     Eq,
-//     Default,
-//     serde::Serialize,
-//     serde::Deserialize,
-//     utoipa::ToSchema,
-//     schemars::JsonSchema,
-//     postgresql_crud_types_macro_logic_reuse::GeneratePostgresqlJsonTypeFullTypePath,
-// )]
-// pub struct StdPrimitiveI16(#[validate(range(min = -32_768i16, max = 32_767i16))] pub std::primitive::i16);
-// #[derive(
-//     Debug,
-//     Clone,
-//     Copy,
-//     PartialEq,
-//     Eq,
-//     Default,
-//     serde::Serialize,
-//     serde::Deserialize,
-//     utoipa::ToSchema,
-//     schemars::JsonSchema,
-//     postgresql_crud_types_macro_logic_reuse::GeneratePostgresqlJsonTypeFullTypePath,
-// )]
-// pub struct StdPrimitiveI32(#[validate(range(min = -2_147_483_648i32, max = 2_147_483_647i32))] pub std::primitive::i32);
-// #[derive(
-//     Debug,
-//     Clone,
-//     Copy,
-//     PartialEq,
-//     Eq,
-//     Default,
-//     serde::Serialize,
-//     serde::Deserialize,
-//     utoipa::ToSchema,
-//     schemars::JsonSchema,
-//     postgresql_crud_types_macro_logic_reuse::GeneratePostgresqlJsonTypeFullTypePath,
-// )]
-// pub struct StdPrimitiveI64(#[validate(range(min = -9_223_372_036_854_775_808i64, max = 9_223_372_036_854_775_807i64))] pub std::primitive::i64);
-// #[derive(
-//     Debug,
-//     Clone,
-//     Copy,
-//     PartialEq,
-//     Eq,
-//     Default,
-//     serde::Serialize,
-//     serde::Deserialize,
-//     utoipa::ToSchema,
-//     schemars::JsonSchema,
-//     postgresql_crud_types_macro_logic_reuse::GeneratePostgresqlJsonTypeFullTypePath,
-// )]
-// pub struct StdPrimitiveU8(#[validate(range(min = 0u8, max = 255u8))] pub std::primitive::u8);
-// #[derive(
-//     Debug,
-//     Clone,
-//     Copy,
-//     PartialEq,
-//     Eq,
-//     Default,
-//     serde::Serialize,
-//     serde::Deserialize,
-//     utoipa::ToSchema,
-//     schemars::JsonSchema,
-//     postgresql_crud_types_macro_logic_reuse::GeneratePostgresqlJsonTypeFullTypePath,
-// )]
-// pub struct StdPrimitiveU16(#[validate(range(min = 0u16, max = 65_535u16))] pub std::primitive::u16);
-// #[derive(
-//     Debug,
-//     Clone,
-//     Copy,
-//     PartialEq,
-//     Eq,
-//     Default,
-//     serde::Serialize,
-//     serde::Deserialize,
-//     utoipa::ToSchema,
-//     schemars::JsonSchema,
-//     postgresql_crud_types_macro_logic_reuse::GeneratePostgresqlJsonTypeFullTypePath,
-// )]
-// pub struct StdPrimitiveU32(#[validate(range(min = 0u32, max = 4_294_967_295u32))] pub std::primitive::u32);
-// #[derive(
-//     Debug,
-//     Clone,
-//     Copy,
-//     PartialEq,
-//     Eq,
-//     Default,
-//     serde::Serialize,
-//     serde::Deserialize,
-//     utoipa::ToSchema,
-//     schemars::JsonSchema,
-//     postgresql_crud_types_macro_logic_reuse::GeneratePostgresqlJsonTypeFullTypePath,
-// )]
-// pub struct StdPrimitiveU64(#[validate(range(min = 0u64, max = 18_446_744_073_709_551_615u64))] pub std::primitive::u64);
-// #[derive(
-//     Debug,
-//     Clone,
-//     Copy,
-//     PartialEq,
-//     Default,
-//     serde::Serialize,
-//     serde::Deserialize,
-//     utoipa::ToSchema,
-//     schemars::JsonSchema,
-//     postgresql_crud_types_macro_logic_reuse::GeneratePostgresqlJsonTypeFullTypePath,
-// )]
-// pub struct StdPrimitiveF32(
-//     #[validate(range(min = -3.40282347E+38f32, max = 3.40282347E+38f32))] //todo maybe its not correct. https://doc.rust-lang.org/std/primitive.f32.html
-//     pub std::primitive::f32,
-// );
-// #[derive(
-//     Debug,
-//     Clone,
-//     Copy,
-//     PartialEq,
-//     Default,
-//     serde::Serialize,
-//     serde::Deserialize,
-//     utoipa::ToSchema,
-//     schemars::JsonSchema,
-//     postgresql_crud_types_macro_logic_reuse::GeneratePostgresqlJsonTypeFullTypePath,
-// )]
-// pub struct StdPrimitiveF64(
-//     #[validate(range(min = -1.7976931348623157E+308f64, max = 1.7976931348623157E+308f64))] //todo maybe its not correct. https://doc.rust-lang.org/core/primitive.f64.html
-//     pub std::primitive::f64,
-// );
-// #[derive(
-//     Debug,
-//     Clone,
-//     Copy,
-//     PartialEq,
-//     Eq,
-//     Default,
-//     serde::Serialize,
-//     serde::Deserialize,
-//     utoipa::ToSchema,
-//     schemars::JsonSchema,
-//     postgresql_crud_types_macro_logic_reuse::GeneratePostgresqlJsonTypeFullTypePath,
-// )]
-// pub struct StdPrimitiveBool(pub std::primitive::bool);
-// #[derive(
-//     Debug,
-//     Clone,
-//     PartialEq,
-//     Eq,
-//     Default,
-//     serde::Serialize,
-//     serde::Deserialize,
-//     utoipa::ToSchema,
-//     schemars::JsonSchema,
-//     postgresql_crud_types_macro_logic_reuse::GeneratePostgresqlJsonTypeFullTypePath,
-// )]
-// pub struct StdStringString(pub std::string::String);
+#[derive(
+    Debug,
+    Clone,
+    Copy,
+    PartialEq,
+    Eq,
+    Default,
+    serde::Serialize,
+    serde::Deserialize,
+    utoipa::ToSchema,
+    schemars::JsonSchema,
+    postgresql_crud_types_macro_logic_reuse::GeneratePostgresqlJsonTypeFullTypePath,
+    postgresql_crud_types_macro_logic_reuse::GeneratePostgresqlJsonTypeWhereElement,
+)]
+pub struct StdPrimitiveI16(#[validate(range(min = -32_768i16, max = 32_767i16))] pub std::primitive::i16);
+#[derive(
+    Debug,
+    Clone,
+    Copy,
+    PartialEq,
+    Eq,
+    Default,
+    serde::Serialize,
+    serde::Deserialize,
+    utoipa::ToSchema,
+    schemars::JsonSchema,
+    postgresql_crud_types_macro_logic_reuse::GeneratePostgresqlJsonTypeFullTypePath,
+    postgresql_crud_types_macro_logic_reuse::GeneratePostgresqlJsonTypeWhereElement,
+)]
+pub struct StdPrimitiveI32(#[validate(range(min = -2_147_483_648i32, max = 2_147_483_647i32))] pub std::primitive::i32);
+#[derive(
+    Debug,
+    Clone,
+    Copy,
+    PartialEq,
+    Eq,
+    Default,
+    serde::Serialize,
+    serde::Deserialize,
+    utoipa::ToSchema,
+    schemars::JsonSchema,
+    postgresql_crud_types_macro_logic_reuse::GeneratePostgresqlJsonTypeFullTypePath,
+    postgresql_crud_types_macro_logic_reuse::GeneratePostgresqlJsonTypeWhereElement,
+)]
+pub struct StdPrimitiveI64(#[validate(range(min = -9_223_372_036_854_775_808i64, max = 9_223_372_036_854_775_807i64))] pub std::primitive::i64);
+#[derive(
+    Debug,
+    Clone,
+    Copy,
+    PartialEq,
+    Eq,
+    Default,
+    serde::Serialize,
+    serde::Deserialize,
+    utoipa::ToSchema,
+    schemars::JsonSchema,
+    postgresql_crud_types_macro_logic_reuse::GeneratePostgresqlJsonTypeFullTypePath,
+    postgresql_crud_types_macro_logic_reuse::GeneratePostgresqlJsonTypeWhereElement,
+)]
+pub struct StdPrimitiveU8(#[validate(range(min = 0u8, max = 255u8))] pub std::primitive::u8);
+#[derive(
+    Debug,
+    Clone,
+    Copy,
+    PartialEq,
+    Eq,
+    Default,
+    serde::Serialize,
+    serde::Deserialize,
+    utoipa::ToSchema,
+    schemars::JsonSchema,
+    postgresql_crud_types_macro_logic_reuse::GeneratePostgresqlJsonTypeFullTypePath,
+    postgresql_crud_types_macro_logic_reuse::GeneratePostgresqlJsonTypeWhereElement,
+)]
+pub struct StdPrimitiveU16(#[validate(range(min = 0u16, max = 65_535u16))] pub std::primitive::u16);
+#[derive(
+    Debug,
+    Clone,
+    Copy,
+    PartialEq,
+    Eq,
+    Default,
+    serde::Serialize,
+    serde::Deserialize,
+    utoipa::ToSchema,
+    schemars::JsonSchema,
+    postgresql_crud_types_macro_logic_reuse::GeneratePostgresqlJsonTypeFullTypePath,
+    postgresql_crud_types_macro_logic_reuse::GeneratePostgresqlJsonTypeWhereElement,
+)]
+pub struct StdPrimitiveU32(#[validate(range(min = 0u32, max = 4_294_967_295u32))] pub std::primitive::u32);
+#[derive(
+    Debug,
+    Clone,
+    Copy,
+    PartialEq,
+    Eq,
+    Default,
+    serde::Serialize,
+    serde::Deserialize,
+    utoipa::ToSchema,
+    schemars::JsonSchema,
+    postgresql_crud_types_macro_logic_reuse::GeneratePostgresqlJsonTypeFullTypePath,
+    postgresql_crud_types_macro_logic_reuse::GeneratePostgresqlJsonTypeWhereElement,
+)]
+pub struct StdPrimitiveU64(#[validate(range(min = 0u64, max = 18_446_744_073_709_551_615u64))] pub std::primitive::u64);
+#[derive(
+    Debug,
+    Clone,
+    Copy,
+    PartialEq,
+    Default,
+    serde::Serialize,
+    serde::Deserialize,
+    utoipa::ToSchema,
+    schemars::JsonSchema,
+    postgresql_crud_types_macro_logic_reuse::GeneratePostgresqlJsonTypeFullTypePath,
+    postgresql_crud_types_macro_logic_reuse::GeneratePostgresqlJsonTypeWhereElement,
+)]
+pub struct StdPrimitiveF32(
+    #[validate(range(min = -3.40282347E+38f32, max = 3.40282347E+38f32))] //todo maybe its not correct. https://doc.rust-lang.org/std/primitive.f32.html
+    pub std::primitive::f32,
+);
+#[derive(
+    Debug,
+    Clone,
+    Copy,
+    PartialEq,
+    Default,
+    serde::Serialize,
+    serde::Deserialize,
+    utoipa::ToSchema,
+    schemars::JsonSchema,
+    postgresql_crud_types_macro_logic_reuse::GeneratePostgresqlJsonTypeFullTypePath,
+    postgresql_crud_types_macro_logic_reuse::GeneratePostgresqlJsonTypeWhereElement,
+)]
+pub struct StdPrimitiveF64(
+    #[validate(range(min = -1.7976931348623157E+308f64, max = 1.7976931348623157E+308f64))] //todo maybe its not correct. https://doc.rust-lang.org/core/primitive.f64.html
+    pub std::primitive::f64,
+);
+#[derive(
+    Debug,
+    Clone,
+    Copy,
+    PartialEq,
+    Eq,
+    Default,
+    serde::Serialize,
+    serde::Deserialize,
+    utoipa::ToSchema,
+    schemars::JsonSchema,
+    postgresql_crud_types_macro_logic_reuse::GeneratePostgresqlJsonTypeFullTypePath,
+    postgresql_crud_types_macro_logic_reuse::GeneratePostgresqlJsonTypeWhereElement,
+)]
+pub struct StdPrimitiveBool(pub std::primitive::bool);
+#[derive(
+    Debug,
+    Clone,
+    PartialEq,
+    Eq,
+    Default,
+    serde::Serialize,
+    serde::Deserialize,
+    utoipa::ToSchema,
+    schemars::JsonSchema,
+    postgresql_crud_types_macro_logic_reuse::GeneratePostgresqlJsonTypeFullTypePath,
+    postgresql_crud_types_macro_logic_reuse::GeneratePostgresqlJsonTypeWhereElement,
+)]
+pub struct StdStringString(pub std::string::String);
 
 
 // #[derive(
