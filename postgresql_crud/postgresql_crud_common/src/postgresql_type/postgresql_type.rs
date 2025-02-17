95cec5fabe134583fde276f6f330eb04cb9bb650
kuqmua <serezhashmakov@outlook.com>
2025-02-17 11:13:00 +0300

    next steps

--- postgresql_crud/postgresql_crud_common/src/postgresql_type/postgresql_type.rs
+++ postgresql_crud/postgresql_crud_common/src/postgresql_type/postgresql_type.rs
@@ -42,21 +42,21 @@ const BIT: &std::primitive::str = "bit";
 //     // postgresql_crud_types_macro_logic_reuse::PostgresqlTypeCreateTableColumnQueryPartTokens,
 // )]
 // struct StdPrimitiveI16AsPostgresqlInt2(std::primitive::i16);
-#[derive(
-    Debug,
-    Clone,
-    PartialEq,
-    serde::Serialize,
-    serde::Deserialize,
-    postgresql_crud_types_macro_logic_reuse::PostgresqlTypeInitializedByClientTokens,
-    postgresql_crud_types_macro_logic_reuse::PostgresqlTypeCreateTableColumnQueryPartTokens,
-)]
-struct StdPrimitiveI32AsPostgresqlInt4(crate::postgresql_type::postgresql_base_type::StdPrimitiveI32);
-impl crate::CreateTableColumnQueryPart for StdPrimitiveI32AsPostgresqlInt4 {
-    fn create_table_column_query_part(column: &dyn std::fmt::Display, _: std::primitive::bool) -> impl std::fmt::Display {
-        format!("{column} {INT4}")
-    }
-}
+// #[derive(
+//     Debug,
+//     Clone,
+//     PartialEq,
+//     serde::Serialize,
+//     serde::Deserialize,
+//     postgresql_crud_types_macro_logic_reuse::PostgresqlTypeInitializedByClientTokens,
+//     postgresql_crud_types_macro_logic_reuse::PostgresqlTypeCreateTableColumnQueryPartTokens,
+// )]
+// struct StdPrimitiveI32AsPostgresqlInt4(crate::postgresql_type::postgresql_base_type::StdPrimitiveI32);
+// impl crate::CreateTableColumnQueryPart for StdPrimitiveI32AsPostgresqlInt4 {
+//     fn create_table_column_query_part(column: &dyn std::fmt::Display, _: std::primitive::bool) -> impl std::fmt::Display {
+//         format!("{column} {INT4}")
+//     }
+// }
 #[derive(
     Debug,
     Clone,
