99e1506d84b61c2e6ade850b4d5d3732f4c098c4
kuqmua <serezhashmakov@outlook.com>
2025-02-23 15:54:20 +0300

    next steps

--- postgresql_crud/postgresql_crud_common/src/postgresql_type/mod.rs
+++ postgresql_crud/postgresql_crud_common/src/postgresql_type/mod.rs
@@ -1,4 +1,3 @@
 pub mod postgresql_base_type;
-mod postgresql_base_type_trait;
 pub mod postgresql_type;
 pub mod postgresql_type_trait;
--- postgresql_crud/postgresql_crud_common/src/postgresql_type/postgresql_type_trait.rs
+++ postgresql_crud/postgresql_crud_common/src/postgresql_type/postgresql_type_trait.rs
@@ -90,3 +90,37 @@ pub(crate) trait PostgresqlTypePrimaryKey<'a> {
         + sqlx::Type<sqlx::Postgres>
         + crate::generate_postgresql_json_type::StdDefaultDefaultButStdOptionOptionIsAlwaysSomeAndStdVecVecAlwaysContainsOneElement;
 }
+
+
+
+// pub(crate) trait PostgresqlBaseTypeSelfTraits<'a>: std::fmt::Debug
+//     + Clone
+//     + PartialEq
+//     + serde::Serialize
+//     + serde::Deserialize<'a>
+//     + sqlx::Type<sqlx::Postgres>
+//     + sqlx::Decode<'a, sqlx::Postgres>
+//     + crate::BindQuery<'a>
+//     + crate::generate_postgresql_json_type::StdDefaultDefaultButStdOptionOptionIsAlwaysSomeAndStdVecVecAlwaysContainsOneElement {}
+
+
+
+// pub(crate) trait PostgresqlBaseType<'a> {
+//     type PostgresqlBaseTypeSelf: PostgresqlBaseTypeSelfTraits<'a>;
+//     type PostgresqlBaseTypeStdOptionOptionSelf: std::fmt::Debug
+//         + Clone
+//         + PartialEq
+//         + serde::Serialize
+//         + serde::Deserialize<'a>
+//         + sqlx::Type<sqlx::Postgres>
+//         + sqlx::Decode<'a, sqlx::Postgres>
+//         + crate::BindQuery<'a>
+//         + crate::generate_postgresql_json_type::StdDefaultDefaultButStdOptionOptionIsAlwaysSomeAndStdVecVecAlwaysContainsOneElement;
+// }
+
+
+// pub(crate) trait PostgresqlBaseTypePrimaryKey<'a> {
+//     type PostgresqlBaseTypeSelf: PostgresqlBaseTypeSelfTraits<'a>
+//         + sqlx::Encode<'a, sqlx::Postgres>
+//         + sqlx::postgres::PgHasArrayType;
+// }
