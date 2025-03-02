e8826c5deb5afc83deb9503115fcd4832e7d0bcf
kuqmua <serezhashmakov@outlook.com>
2025-03-02 13:56:30 +0300

    next steps

--- token_patterns/src/lib.rs
+++ token_patterns/src/lib.rs
@@ -313,8 +313,26 @@ impl quote::ToTokens for StdDefaultDefaultButStdOptionOptionIsAlwaysSomeAndStdVe
 pub struct StdDefaultDefaultButStdOptionOptionIsAlwaysSomeAndStdVecVecAlwaysContainsOneElementCall;
 impl quote::ToTokens for StdDefaultDefaultButStdOptionOptionIsAlwaysSomeAndStdVecVecAlwaysContainsOneElementCall {
     fn to_tokens(&self, tokens: &mut proc_macro2::TokenStream) {
+        let std_default_default_but_std_option_option_is_always_some_and_std_vec_vec_always_contains_one_element_token_stream = StdDefaultDefaultButStdOptionOptionIsAlwaysSomeAndStdVecVecAlwaysContainsOneElement;
         quote::quote! {
-            crate::generate_postgresql_json_type::StdDefaultDefaultButStdOptionOptionIsAlwaysSomeAndStdVecVecAlwaysContainsOneElement::std_default_default_but_std_option_option_is_always_some_and_std_vec_vec_always_contains_one_element()
+            #std_default_default_but_std_option_option_is_always_some_and_std_vec_vec_always_contains_one_element_token_stream::std_default_default_but_std_option_option_is_always_some_and_std_vec_vec_always_contains_one_element()
+        }.to_tokens(tokens)
+    }
+}
+pub struct AllEnumVariantsArrayStdDefaultDefaultButStdOptionOptionIsAlwaysSomeAndStdVecVecAlwaysContainsOneElement;
+impl quote::ToTokens for AllEnumVariantsArrayStdDefaultDefaultButStdOptionOptionIsAlwaysSomeAndStdVecVecAlwaysContainsOneElement {
+    fn to_tokens(&self, tokens: &mut proc_macro2::TokenStream) {
+        quote::quote! {
+            crate::generate_postgresql_json_type::AllEnumVariantsArrayStdDefaultDefaultButStdOptionOptionIsAlwaysSomeAndStdVecVecAlwaysContainsOneElement
+        }.to_tokens(tokens)
+    }
+}
+pub struct AllEnumVariantsArrayStdDefaultDefaultButStdOptionOptionIsAlwaysSomeAndStdVecVecAlwaysContainsOneElementCall;
+impl quote::ToTokens for AllEnumVariantsArrayStdDefaultDefaultButStdOptionOptionIsAlwaysSomeAndStdVecVecAlwaysContainsOneElementCall {
+    fn to_tokens(&self, tokens: &mut proc_macro2::TokenStream) {
+        let all_enum_variants_array_std_default_default_but_std_option_option_is_always_some_and_std_vec_vec_always_contains_one_element_token_stream = AllEnumVariantsArrayStdDefaultDefaultButStdOptionOptionIsAlwaysSomeAndStdVecVecAlwaysContainsOneElement;
+        quote::quote! {
+            #all_enum_variants_array_std_default_default_but_std_option_option_is_always_some_and_std_vec_vec_always_contains_one_element_token_stream::all_enum_variants_array_default_but_std_option_option_is_always_some_and_std_vec_vec_always_contains_one_element()
         }.to_tokens(tokens)
     }
 }
