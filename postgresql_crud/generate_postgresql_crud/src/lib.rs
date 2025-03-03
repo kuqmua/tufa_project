042783ec799f3e120e408b6166475280a3451aed
kuqmua <serezhashmakov@outlook.com>
2025-03-03 14:55:00 +0300

    next steps

--- postgresql_crud/generate_postgresql_crud/src/lib.rs
+++ postgresql_crud/generate_postgresql_crud/src/lib.rs
@@ -3032,16 +3032,16 @@ pub fn generate_postgresql_crud(input: proc_macro::TokenStream) -> proc_macro::T
     // //     }
     // //     acc
     // // });
-    fn generate_impl_postgresql_crud_std_default_default_but_std_option_option_is_always_some_and_std_vec_vec_always_contains_one_element_for_tokens_token_stream(
+    let generate_impl_postgresql_crud_std_default_default_but_std_option_option_is_always_some_and_std_vec_vec_always_contains_one_element_for_tokens_token_stream = |
         ident: &dyn quote::ToTokens,
         content_token_stream: &dyn quote::ToTokens,
-    ) -> proc_macro2::TokenStream {
+    | {
         postgresql_crud_macros_common::generate_impl_std_default_default_but_std_option_option_is_always_some_and_std_vec_vec_always_contains_one_element_for_tokens_token_stream(
             &postgresql_crud_macros_common::PathDefaultButOptionIsAlwaysSomeAndVecAlwaysContainsOneElement::PostgresqlCrud,
             &ident,
             &content_token_stream,
         )
-    }
+    };
     //todo generate test_token_stream
     let create_many_token_stream = {
         let operation = Operation::CreateMany;
