5aaeda4c91a2642a6121e23f964bdd96e8230e1f
kuqmua <serezhashmakov@outlook.com>
2025-03-07 14:33:22 +0300

    next steps

--- postgresql_crud/postgresql_crud_common/postgresql_crud_types_macro_logic_reuse/src/lib.rs
+++ postgresql_crud/postgresql_crud_common/postgresql_crud_types_macro_logic_reuse/src/lib.rs
@@ -4568,7 +4568,6 @@ pub fn generate_postgresql_types(_input_token_stream: proc_macro::TokenStream) -
                             let postgresql_type_tokens_where_element_greater_than_lower_bound_token_stream = greater_than_lower_bound.generate_postgresql_type_tokens_where_element_variant_handle_token_stream(&postgresql_type_not_null_upper_camel_case);
                             let postgresql_type_tokens_where_element_overlap_with_range_token_stream = overlap_with_range.generate_postgresql_type_tokens_where_element_variant_handle_token_stream(&postgresql_type_not_null_upper_camel_case);
                             let postgresql_type_tokens_where_element_adjacent_with_range_token_stream = adjacent_with_range.generate_postgresql_type_tokens_where_element_variant_handle_token_stream(&postgresql_type_not_null_upper_camel_case);
-                            //todo find out maximum length of range(INT8RANGE, INT4RANGE) in postgresql
                             let maybe_postgresql_type_tokens_where_element_range_length_token_stream: &dyn quote::ToTokens = match &range_type_should_impl_range_length {
                                 ShouldImplRangeLength::True => &range_length.generate_postgresql_type_tokens_where_element_variant_handle_token_stream(&postgresql_type_not_null_upper_camel_case),
                                 ShouldImplRangeLength::False => &proc_macro2_token_stream_new,
@@ -5048,7 +5047,7 @@ pub fn generate_postgresql_types(_input_token_stream: proc_macro::TokenStream) -
 
     // macros_helpers::write_token_stream_into_file::write_token_stream_into_file(
     //     "PostgresqlTypeTokens",
-    //     &h,
+    //     &h1,
     // );
 
     let generated = quote::quote! {
