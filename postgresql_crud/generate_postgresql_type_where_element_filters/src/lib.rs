77a0e3b7c3ccd3970fea9238859e4088a5e7284a
kuqmua <serezhashmakov@outlook.com>
2025-03-20 22:17:51 +0300

    next steps

--- postgresql_crud/generate_postgresql_type_where_element_filters/src/lib.rs
+++ postgresql_crud/generate_postgresql_type_where_element_filters/src/lib.rs
@@ -48,12 +48,107 @@ pub fn generate_postgresql_type_where_element_filters(_input_token_stream: proc_
     }
     let generate_filters_token_stream = |filter: &Filter|{
         let ident = naming::parameter::PostgresqlTypeWhereElementSelfUpperCamelCase::from_display(&filter);
+        let is_fields_are_public = true;
         let struct_token_stream = {
+            let maybe_pub_token_stream = if is_fields_are_public {
+                quote::quote! {pub}
+            }
+            else {
+                proc_macro2::TokenStream::new()
+            };
+            let maybe_serde_deserialize_token_stream = match &filter {
+                Filter::Equal => quote::quote!{, serde::Deserialize},
+                Filter::GreaterThan => quote::quote!{},
+                Filter::Between => quote::quote!{},
+                Filter::In => quote::quote!{},
+                Filter::CaseSensitiveRegularExpression => quote::quote!{},
+                Filter::CaseInsensitiveRegularExpression => quote::quote!{},
+                Filter::Before => quote::quote!{},
+                Filter::CurrentDate => quote::quote!{},
+                Filter::GreaterThanCurrentDate => quote::quote!{},
+                Filter::CurrentTimestamp => quote::quote!{},
+                Filter::GreaterThanCurrentTimestamp => quote::quote!{},
+                Filter::CurrentTime => quote::quote!{},
+                Filter::GreaterThanCurrentTime => quote::quote!{},
+                Filter::LengthEqual => quote::quote!{},
+                Filter::LengthMoreThan => quote::quote!{},
+                Filter::EqualToEncodedStringRepresentation => quote::quote!{},
+                Filter::ValueIsContainedWithinRange => quote::quote!{},
+                Filter::ContainsAnotherRange => quote::quote!{},
+                Filter::StrictlyToLeftOfRange => quote::quote!{},
+                Filter::StrictlyToRightOfRange => quote::quote!{},
+                Filter::IncludedLowerBound => quote::quote!{},
+                Filter::ExcludedUpperBound => quote::quote!{},
+                Filter::GreaterThanLowerBound => quote::quote!{},
+                Filter::OverlapWithRange => quote::quote!{},
+                Filter::AdjacentWithRange => quote::quote!{},
+                Filter::RangeLength => quote::quote!{},
+                // Filter:://BitVecPositionEqual => quote::quote!{},
+                Filter::PositionEqual => quote::quote!{},
+                Filter::PositionGreaterThan => quote::quote!{},
+                Filter::PositionCaseSensitiveRegularExpression => quote::quote!{},
+                Filter::PositionCaseInsensitiveRegularExpression => quote::quote!{},
+                Filter::ContainsAllElementsOfArray => quote::quote!{},
+                Filter::ContainedInArray => quote::quote!{},
+                Filter::OverlapsWithArray => quote::quote!{},
+                Filter::AllElementsEqual => quote::quote!{},
+                Filter::ContainsElementGreaterThan => quote::quote!{},
+                Filter::AllElementsGreaterThan => quote::quote!{},
+                Filter::ContainsElementCaseSensitiveRegularExpression => quote::quote!{},
+                Filter::ContainsElementCaseInsensitiveRegularExpression => quote::quote!{},
+                Filter::AllElementsCaseSensitiveRegularExpression => quote::quote!{},
+                Filter::AllElementsCaseInsensitiveRegularExpression => quote::quote!{},
+                Filter::EqualSecondDimension => quote::quote!{},
+            };
+            let content_token_stream = match &filter {
+                Filter::Equal => quote::quote!{pub value: T},
+                Filter::GreaterThan => quote::quote!{},
+                Filter::Between => quote::quote!{},
+                Filter::In => quote::quote!{},
+                Filter::CaseSensitiveRegularExpression => quote::quote!{},
+                Filter::CaseInsensitiveRegularExpression => quote::quote!{},
+                Filter::Before => quote::quote!{},
+                Filter::CurrentDate => quote::quote!{},
+                Filter::GreaterThanCurrentDate => quote::quote!{},
+                Filter::CurrentTimestamp => quote::quote!{},
+                Filter::GreaterThanCurrentTimestamp => quote::quote!{},
+                Filter::CurrentTime => quote::quote!{},
+                Filter::GreaterThanCurrentTime => quote::quote!{},
+                Filter::LengthEqual => quote::quote!{},
+                Filter::LengthMoreThan => quote::quote!{},
+                Filter::EqualToEncodedStringRepresentation => quote::quote!{},
+                Filter::ValueIsContainedWithinRange => quote::quote!{},
+                Filter::ContainsAnotherRange => quote::quote!{},
+                Filter::StrictlyToLeftOfRange => quote::quote!{},
+                Filter::StrictlyToRightOfRange => quote::quote!{},
+                Filter::IncludedLowerBound => quote::quote!{},
+                Filter::ExcludedUpperBound => quote::quote!{},
+                Filter::GreaterThanLowerBound => quote::quote!{},
+                Filter::OverlapWithRange => quote::quote!{},
+                Filter::AdjacentWithRange => quote::quote!{},
+                Filter::RangeLength => quote::quote!{},
+                // Filter:://BitVecPositionEqual => quote::quote!{},
+                Filter::PositionEqual => quote::quote!{},
+                Filter::PositionGreaterThan => quote::quote!{},
+                Filter::PositionCaseSensitiveRegularExpression => quote::quote!{},
+                Filter::PositionCaseInsensitiveRegularExpression => quote::quote!{},
+                Filter::ContainsAllElementsOfArray => quote::quote!{},
+                Filter::ContainedInArray => quote::quote!{},
+                Filter::OverlapsWithArray => quote::quote!{},
+                Filter::AllElementsEqual => quote::quote!{},
+                Filter::ContainsElementGreaterThan => quote::quote!{},
+                Filter::AllElementsGreaterThan => quote::quote!{},
+                Filter::ContainsElementCaseSensitiveRegularExpression => quote::quote!{},
+                Filter::ContainsElementCaseInsensitiveRegularExpression => quote::quote!{},
+                Filter::AllElementsCaseSensitiveRegularExpression => quote::quote!{},
+                Filter::AllElementsCaseInsensitiveRegularExpression => quote::quote!{},
+                Filter::EqualSecondDimension => quote::quote!{},
+            };
             quote::quote! {
-                #[derive(Debug, Clone, PartialEq, serde::Serialize, serde::Deserialize)]
+                #[derive(Debug, Clone, PartialEq, serde::Serialize #maybe_serde_deserialize_token_stream )]
                 pub struct #ident<T> {
-                    pub logical_operator: crate::LogicalOperator,
-                    pub value: T,
+                    #maybe_pub_token_stream logical_operator: crate::LogicalOperator,
+                    #content_token_stream
                 }
             }
         };
@@ -63,8 +158,49 @@ pub fn generate_postgresql_type_where_element_filters(_input_token_stream: proc_
             &ident,
             &quote::quote!{<T>},
             &{
-                let content_token_stream = quote::quote!{
-                    value: crate::DefaultButOptionIsAlwaysSomeAndVecAlwaysContainsOneElement::default_but_option_is_always_some_and_vec_always_contains_one_element(),
+                let content_token_stream = match &filter {
+                    Filter::Equal => quote::quote!{value: crate::DefaultButOptionIsAlwaysSomeAndVecAlwaysContainsOneElement::default_but_option_is_always_some_and_vec_always_contains_one_element()},
+                    Filter::GreaterThan => quote::quote!{},
+                    Filter::Between => quote::quote!{},
+                    Filter::In => quote::quote!{},
+                    Filter::CaseSensitiveRegularExpression => quote::quote!{},
+                    Filter::CaseInsensitiveRegularExpression => quote::quote!{},
+                    Filter::Before => quote::quote!{},
+                    Filter::CurrentDate => quote::quote!{},
+                    Filter::GreaterThanCurrentDate => quote::quote!{},
+                    Filter::CurrentTimestamp => quote::quote!{},
+                    Filter::GreaterThanCurrentTimestamp => quote::quote!{},
+                    Filter::CurrentTime => quote::quote!{},
+                    Filter::GreaterThanCurrentTime => quote::quote!{},
+                    Filter::LengthEqual => quote::quote!{},
+                    Filter::LengthMoreThan => quote::quote!{},
+                    Filter::EqualToEncodedStringRepresentation => quote::quote!{},
+                    Filter::ValueIsContainedWithinRange => quote::quote!{},
+                    Filter::ContainsAnotherRange => quote::quote!{},
+                    Filter::StrictlyToLeftOfRange => quote::quote!{},
+                    Filter::StrictlyToRightOfRange => quote::quote!{},
+                    Filter::IncludedLowerBound => quote::quote!{},
+                    Filter::ExcludedUpperBound => quote::quote!{},
+                    Filter::GreaterThanLowerBound => quote::quote!{},
+                    Filter::OverlapWithRange => quote::quote!{},
+                    Filter::AdjacentWithRange => quote::quote!{},
+                    Filter::RangeLength => quote::quote!{},
+                    // Filter:://BitVecPositionEqual => quote::quote!{},
+                    Filter::PositionEqual => quote::quote!{},
+                    Filter::PositionGreaterThan => quote::quote!{},
+                    Filter::PositionCaseSensitiveRegularExpression => quote::quote!{},
+                    Filter::PositionCaseInsensitiveRegularExpression => quote::quote!{},
+                    Filter::ContainsAllElementsOfArray => quote::quote!{},
+                    Filter::ContainedInArray => quote::quote!{},
+                    Filter::OverlapsWithArray => quote::quote!{},
+                    Filter::AllElementsEqual => quote::quote!{},
+                    Filter::ContainsElementGreaterThan => quote::quote!{},
+                    Filter::AllElementsGreaterThan => quote::quote!{},
+                    Filter::ContainsElementCaseSensitiveRegularExpression => quote::quote!{},
+                    Filter::ContainsElementCaseInsensitiveRegularExpression => quote::quote!{},
+                    Filter::AllElementsCaseSensitiveRegularExpression => quote::quote!{},
+                    Filter::AllElementsCaseInsensitiveRegularExpression => quote::quote!{},
+                    Filter::EqualSecondDimension => quote::quote!{},
                 };
                 quote::quote!{
                     Self {
@@ -76,20 +212,106 @@ pub fn generate_postgresql_type_where_element_filters(_input_token_stream: proc_
         );
         let impl_postgresql_type_self_where_filter_token_stream = {
             let where_query_part_token_stream = {
-                quote::quote! {
-                    match increment.checked_add(1) {
-                        Some(value) => {
-                            *increment = value;
-                            Ok(format!("{}({} = ${})", &self.logical_operator.to_query_part(is_need_to_add_logical_operator), column, increment))
+                match &filter {
+                    Filter::Equal => quote::quote!{
+                        match increment.checked_add(1) {
+                            Some(value) => {
+                                *increment = value;
+                                Ok(format!("{}({} = ${})", &self.logical_operator.to_query_part(is_need_to_add_logical_operator), column, increment))
+                            }
+                            None => Err(crate::QueryPartErrorNamed::CheckedAdd { code_occurence: error_occurence_lib::code_occurence!() }),
                         }
-                        None => Err(crate::QueryPartErrorNamed::CheckedAdd { code_occurence: error_occurence_lib::code_occurence!() }),
-                    }
+                    },
+                    Filter::GreaterThan => quote::quote!{},
+                    Filter::Between => quote::quote!{},
+                    Filter::In => quote::quote!{},
+                    Filter::CaseSensitiveRegularExpression => quote::quote!{},
+                    Filter::CaseInsensitiveRegularExpression => quote::quote!{},
+                    Filter::Before => quote::quote!{},
+                    Filter::CurrentDate => quote::quote!{},
+                    Filter::GreaterThanCurrentDate => quote::quote!{},
+                    Filter::CurrentTimestamp => quote::quote!{},
+                    Filter::GreaterThanCurrentTimestamp => quote::quote!{},
+                    Filter::CurrentTime => quote::quote!{},
+                    Filter::GreaterThanCurrentTime => quote::quote!{},
+                    Filter::LengthEqual => quote::quote!{},
+                    Filter::LengthMoreThan => quote::quote!{},
+                    Filter::EqualToEncodedStringRepresentation => quote::quote!{},
+                    Filter::ValueIsContainedWithinRange => quote::quote!{},
+                    Filter::ContainsAnotherRange => quote::quote!{},
+                    Filter::StrictlyToLeftOfRange => quote::quote!{},
+                    Filter::StrictlyToRightOfRange => quote::quote!{},
+                    Filter::IncludedLowerBound => quote::quote!{},
+                    Filter::ExcludedUpperBound => quote::quote!{},
+                    Filter::GreaterThanLowerBound => quote::quote!{},
+                    Filter::OverlapWithRange => quote::quote!{},
+                    Filter::AdjacentWithRange => quote::quote!{},
+                    Filter::RangeLength => quote::quote!{},
+                    // Filter:://BitVecPositionEqual => quote::quote!{},
+                    Filter::PositionEqual => quote::quote!{},
+                    Filter::PositionGreaterThan => quote::quote!{},
+                    Filter::PositionCaseSensitiveRegularExpression => quote::quote!{},
+                    Filter::PositionCaseInsensitiveRegularExpression => quote::quote!{},
+                    Filter::ContainsAllElementsOfArray => quote::quote!{},
+                    Filter::ContainedInArray => quote::quote!{},
+                    Filter::OverlapsWithArray => quote::quote!{},
+                    Filter::AllElementsEqual => quote::quote!{},
+                    Filter::ContainsElementGreaterThan => quote::quote!{},
+                    Filter::AllElementsGreaterThan => quote::quote!{},
+                    Filter::ContainsElementCaseSensitiveRegularExpression => quote::quote!{},
+                    Filter::ContainsElementCaseInsensitiveRegularExpression => quote::quote!{},
+                    Filter::AllElementsCaseSensitiveRegularExpression => quote::quote!{},
+                    Filter::AllElementsCaseInsensitiveRegularExpression => quote::quote!{},
+                    Filter::EqualSecondDimension => quote::quote!{},
                 }
             };
             let where_query_bind_token_stream = {
-                quote::quote! {
-                    query = query.bind(self.value);
-                    query
+                match &filter {
+                    Filter::Equal => quote::quote!{
+                        query = query.bind(self.value);
+                        query
+                    },
+                    Filter::GreaterThan => quote::quote!{},
+                    Filter::Between => quote::quote!{},
+                    Filter::In => quote::quote!{},
+                    Filter::CaseSensitiveRegularExpression => quote::quote!{},
+                    Filter::CaseInsensitiveRegularExpression => quote::quote!{},
+                    Filter::Before => quote::quote!{},
+                    Filter::CurrentDate => quote::quote!{},
+                    Filter::GreaterThanCurrentDate => quote::quote!{},
+                    Filter::CurrentTimestamp => quote::quote!{},
+                    Filter::GreaterThanCurrentTimestamp => quote::quote!{},
+                    Filter::CurrentTime => quote::quote!{},
+                    Filter::GreaterThanCurrentTime => quote::quote!{},
+                    Filter::LengthEqual => quote::quote!{},
+                    Filter::LengthMoreThan => quote::quote!{},
+                    Filter::EqualToEncodedStringRepresentation => quote::quote!{},
+                    Filter::ValueIsContainedWithinRange => quote::quote!{},
+                    Filter::ContainsAnotherRange => quote::quote!{},
+                    Filter::StrictlyToLeftOfRange => quote::quote!{},
+                    Filter::StrictlyToRightOfRange => quote::quote!{},
+                    Filter::IncludedLowerBound => quote::quote!{},
+                    Filter::ExcludedUpperBound => quote::quote!{},
+                    Filter::GreaterThanLowerBound => quote::quote!{},
+                    Filter::OverlapWithRange => quote::quote!{},
+                    Filter::AdjacentWithRange => quote::quote!{},
+                    Filter::RangeLength => quote::quote!{},
+                    // Filter:://BitVecPositionEqual => quote::quote!{},
+                    Filter::PositionEqual => quote::quote!{},
+                    Filter::PositionGreaterThan => quote::quote!{},
+                    Filter::PositionCaseSensitiveRegularExpression => quote::quote!{},
+                    Filter::PositionCaseInsensitiveRegularExpression => quote::quote!{},
+                    Filter::ContainsAllElementsOfArray => quote::quote!{},
+                    Filter::ContainedInArray => quote::quote!{},
+                    Filter::OverlapsWithArray => quote::quote!{},
+                    Filter::AllElementsEqual => quote::quote!{},
+                    Filter::ContainsElementGreaterThan => quote::quote!{},
+                    Filter::AllElementsGreaterThan => quote::quote!{},
+                    Filter::ContainsElementCaseSensitiveRegularExpression => quote::quote!{},
+                    Filter::ContainsElementCaseInsensitiveRegularExpression => quote::quote!{},
+                    Filter::AllElementsCaseSensitiveRegularExpression => quote::quote!{},
+                    Filter::AllElementsCaseInsensitiveRegularExpression => quote::quote!{},
+                    Filter::EqualSecondDimension => quote::quote!{},
                 }
             };
             quote::quote! {
