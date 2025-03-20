2564aa438f499a1e4be36594ed75a82c4f6b6013
kuqmua <serezhashmakov@outlook.com>
2025-03-20 17:06:58 +0300

    next steps

--- postgresql_crud/generate_postgresql_type_where_element_filters/src/lib.rs
+++ postgresql_crud/generate_postgresql_type_where_element_filters/src/lib.rs
@@ -47,35 +47,50 @@ pub fn generate_postgresql_type_where_element_filters(_input_token_stream: proc_
         EqualSecondDimension,
     }
     let generate_filters_token_stream = |filter: &Filter|{
-        quote::quote! {
-            #[derive(Debug, Clone, PartialEq, serde::Serialize, serde::Deserialize)]
-            pub struct PostgresqlTypeWhereElementEqual<T> {
-                pub logical_operator: crate::LogicalOperator,
-                pub value: T,
-            }
-            impl<T: crate::DefaultButOptionIsAlwaysSomeAndVecAlwaysContainsOneElement> crate::DefaultButOptionIsAlwaysSomeAndVecAlwaysContainsOneElement for PostgresqlTypeWhereElementEqual<T> {
-                fn default_but_option_is_always_some_and_vec_always_contains_one_element() -> Self {
-                    Self {
-                        logical_operator: crate::DefaultButOptionIsAlwaysSomeAndVecAlwaysContainsOneElement::default_but_option_is_always_some_and_vec_always_contains_one_element(),
-                        value: crate::DefaultButOptionIsAlwaysSomeAndVecAlwaysContainsOneElement::default_but_option_is_always_some_and_vec_always_contains_one_element(),
-                    }
+        let struct_token_stream = {
+            quote::quote! {
+                #[derive(Debug, Clone, PartialEq, serde::Serialize, serde::Deserialize)]
+                pub struct PostgresqlTypeWhereElementEqual<T> {
+                    pub logical_operator: crate::LogicalOperator,
+                    pub value: T,
                 }
             }
-            impl<'a, T: sqlx::Encode<'a, sqlx::Postgres> + sqlx::Type<sqlx::Postgres> + 'a + std::marker::Send> crate::postgresql_type_trait::PostgresqlTypeSelfWhereFilter<'a> for PostgresqlTypeWhereElementEqual<T> {
-                fn where_query_part(&self, increment: &mut std::primitive::u64, column: &dyn std::fmt::Display, is_need_to_add_logical_operator: std::primitive::bool) -> Result<std::string::String, crate::QueryPartErrorNamed> {
-                    match increment.checked_add(1) {
-                        Some(value) => {
-                            *increment = value;
-                            Ok(format!("{}({} = ${})", &self.logical_operator.to_query_part(is_need_to_add_logical_operator), column, increment))
+        };
+        let impl_default_but_option_is_always_some_and_vec_always_contains_one_element_token_stream = {
+            quote::quote! {
+                impl<T: crate::DefaultButOptionIsAlwaysSomeAndVecAlwaysContainsOneElement> crate::DefaultButOptionIsAlwaysSomeAndVecAlwaysContainsOneElement for PostgresqlTypeWhereElementEqual<T> {
+                    fn default_but_option_is_always_some_and_vec_always_contains_one_element() -> Self {
+                        Self {
+                            logical_operator: crate::DefaultButOptionIsAlwaysSomeAndVecAlwaysContainsOneElement::default_but_option_is_always_some_and_vec_always_contains_one_element(),
+                            value: crate::DefaultButOptionIsAlwaysSomeAndVecAlwaysContainsOneElement::default_but_option_is_always_some_and_vec_always_contains_one_element(),
                         }
-                        None => Err(crate::QueryPartErrorNamed::CheckedAdd { code_occurence: error_occurence_lib::code_occurence!() }),
                     }
                 }
-                fn where_query_bind(self, mut query: sqlx::query::Query<'a, sqlx::Postgres, sqlx::postgres::PgArguments>) -> sqlx::query::Query<'a, sqlx::Postgres, sqlx::postgres::PgArguments> {
-                    query = query.bind(self.value);
-                    query
+            }
+        };
+        let impl_postgresql_type_self_where_filter_token_stream = {
+            quote::quote! {
+                impl<'a, T: sqlx::Encode<'a, sqlx::Postgres> + sqlx::Type<sqlx::Postgres> + 'a + std::marker::Send> crate::postgresql_type_trait::PostgresqlTypeSelfWhereFilter<'a> for PostgresqlTypeWhereElementEqual<T> {
+                    fn where_query_part(&self, increment: &mut std::primitive::u64, column: &dyn std::fmt::Display, is_need_to_add_logical_operator: std::primitive::bool) -> Result<std::string::String, crate::QueryPartErrorNamed> {
+                        match increment.checked_add(1) {
+                            Some(value) => {
+                                *increment = value;
+                                Ok(format!("{}({} = ${})", &self.logical_operator.to_query_part(is_need_to_add_logical_operator), column, increment))
+                            }
+                            None => Err(crate::QueryPartErrorNamed::CheckedAdd { code_occurence: error_occurence_lib::code_occurence!() }),
+                        }
+                    }
+                    fn where_query_bind(self, mut query: sqlx::query::Query<'a, sqlx::Postgres, sqlx::postgres::PgArguments>) -> sqlx::query::Query<'a, sqlx::Postgres, sqlx::postgres::PgArguments> {
+                        query = query.bind(self.value);
+                        query
+                    }
                 }
             }
+        };
+        quote::quote! {
+            #struct_token_stream
+            #impl_default_but_option_is_always_some_and_vec_always_contains_one_element_token_stream
+            #impl_postgresql_type_self_where_filter_token_stream
         }
     };
     // let filter_array_token_stream = Filter::into_array().map(|element|generate_filters_token_stream(&element));
