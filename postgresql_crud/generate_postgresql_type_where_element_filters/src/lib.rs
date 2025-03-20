c055cfa333063970eba201607a133b9277ebe301
kuqmua <serezhashmakov@outlook.com>
2025-03-20 23:55:47 +0300

    next steps

--- postgresql_crud/generate_postgresql_type_where_element_filters/src/lib.rs
+++ postgresql_crud/generate_postgresql_type_where_element_filters/src/lib.rs
@@ -55,6 +55,31 @@ pub fn generate_postgresql_type_where_element_filters(_input_token_stream: proc_
         EqualSecondDimension,
     }
     let generate_filters_token_stream = |filter: &Filter|{
+        let proc_macro2_token_stream_new = proc_macro2::TokenStream::new();
+        let pub_snake_case_token_stream = {
+            let pub_snake_case = naming::PubSnakeCase;
+            quote::quote!{#pub_snake_case}
+        };
+        let comma_serde_deserialize_token_stream = quote::quote!{, serde::Deserialize};
+        let pub_value_t_token_stream = quote::quote!{pub value: T};
+        let value_default_but_option_is_always_some_and_vec_always_contains_one_element_token_stream = quote::quote!{
+            value: crate::DefaultButOptionIsAlwaysSomeAndVecAlwaysContainsOneElement::default_but_option_is_always_some_and_vec_always_contains_one_element()
+        };
+        let generate_where_query_part_one_value_token_stream = |format_handle_token_stream: &dyn quote::ToTokens|{
+            quote::quote!{
+                match increment.checked_add(1) {
+                    Some(value) => {
+                        *increment = value;
+                        Ok(format!(#format_handle_token_stream, &self.logical_operator.to_query_part(is_need_to_add_logical_operator), column, increment))
+                    }
+                    None => Err(crate::QueryPartErrorNamed::CheckedAdd { code_occurence: error_occurence_lib::code_occurence!() }),
+                }
+            }
+        };
+        let where_query_bind_one_value_token_stream = quote::quote!{
+            query = query.bind(self.value);
+            query
+        };
         let (
             maybe_pub_token_stream,
             maybe_serde_deserialize_token_stream,
@@ -64,28 +89,55 @@ pub fn generate_postgresql_type_where_element_filters(_input_token_stream: proc_
             where_query_bind_content_token_stream,
         ) = match &filter {
             Filter::Equal => (
-                &quote::quote!{pub},
-                &quote::quote!{, serde::Deserialize},
-                &quote::quote!{pub value: T},
+                &pub_snake_case_token_stream,
+                &comma_serde_deserialize_token_stream,
+                &pub_value_t_token_stream,
+                &value_default_but_option_is_always_some_and_vec_always_contains_one_element_token_stream,
+                &generate_where_query_part_one_value_token_stream(&quote::quote!{"{}({} = ${})"}),
+                &where_query_bind_one_value_token_stream,
+            ),
+            Filter::GreaterThan => (
+                &pub_snake_case_token_stream,
+                &comma_serde_deserialize_token_stream,
+                &pub_value_t_token_stream,
+                &value_default_but_option_is_always_some_and_vec_always_contains_one_element_token_stream,
+                &generate_where_query_part_one_value_token_stream(&quote::quote!{"{}({} > ${})"}),
+                &where_query_bind_one_value_token_stream,
+            ),
+            Filter::Between => (
+                &proc_macro2_token_stream_new,
+                &proc_macro2_token_stream_new,
                 &quote::quote!{
-                    value: crate::DefaultButOptionIsAlwaysSomeAndVecAlwaysContainsOneElement::default_but_option_is_always_some_and_vec_always_contains_one_element()
+                    start: T,
+                    end: T,
+                },
+                &quote::quote!{
+                    start: crate::DefaultButOptionIsAlwaysSomeAndVecAlwaysContainsOneElement::default_but_option_is_always_some_and_vec_always_contains_one_element(),
+                    end: crate::DefaultButOptionIsAlwaysSomeAndVecAlwaysContainsOneElement::default_but_option_is_always_some_and_vec_always_contains_one_element(),
                 },
                 &quote::quote!{
                     match increment.checked_add(1) {
-                        Some(value) => {
-                            *increment = value;
-                            Ok(format!("{}({} = ${})", &self.logical_operator.to_query_part(is_need_to_add_logical_operator), column, increment))
+                        Some(first_value) => {
+                            *increment = first_value;
+                            match increment.checked_add(1) {
+                                Some(second_value) => {
+                                    *increment = second_value;
+                                    let between_snake_case = naming::BetweenSnakeCase;
+                                    let and_snake_case = naming::AndSnakeCase;
+                                    Ok(format!("{}({column} {between_snake_case} ${first_value} {and_snake_case} ${second_value})", &self.logical_operator.to_query_part(is_need_to_add_logical_operator)))
+                                }
+                                None => Err(crate::QueryPartErrorNamed::CheckedAdd { code_occurence: error_occurence_lib::code_occurence!() }),
+                            }
                         }
                         None => Err(crate::QueryPartErrorNamed::CheckedAdd { code_occurence: error_occurence_lib::code_occurence!() }),
                     }
                 },
                 &quote::quote!{
-                    query = query.bind(self.value);
+                    query = query.bind(self.start);//here change
+                    query = query.bind(self.end);//here change
                     query
-                },
+                }
             ),
-            Filter::GreaterThan => todo!(),
-            Filter::Between => todo!(),
             Filter::In => todo!(),
             Filter::CaseSensitiveRegularExpression => todo!(),
             Filter::CaseInsensitiveRegularExpression => todo!(),
@@ -136,6 +188,27 @@ pub fn generate_postgresql_type_where_element_filters(_input_token_stream: proc_
                 }
             }
         };
+        let maybe_enum_postgresql_type_where_element_filter_try_new_error_named_token_stream = {
+            quote::quote! {
+                #[derive(
+                    Debug,
+                    Clone,
+                    serde :: Serialize,
+                    serde :: Deserialize,
+                    thiserror ::Error,
+                    // error_occurence_lib :: ErrorOccurence,
+                )]
+                pub enum PostgresqlTypeWhereElementBetweenTryNewErrorNamed<T> {
+                    StartMoreOrEqualToEnd {
+                        // #[eo_to_std_string_string_serialize_deserialize]
+                        start: T,
+                        // #[eo_to_std_string_string_serialize_deserialize]
+                        end: T,
+                        code_occurence: error_occurence_lib::code_occurence::CodeOccurence,
+                    },
+                }
+            }
+        };
         let impl_default_but_option_is_always_some_and_vec_always_contains_one_element_token_stream = postgresql_crud_macros_common::generate_impl_default_but_option_is_always_some_and_vec_always_contains_one_element_for_tokens_token_stream(
             &quote::quote!{<T: crate::DefaultButOptionIsAlwaysSomeAndVecAlwaysContainsOneElement>},
             &postgresql_crud_macros_common::PathDefaultButOptionIsAlwaysSomeAndVecAlwaysContainsOneElement::Crate,
@@ -170,9 +243,11 @@ pub fn generate_postgresql_type_where_element_filters(_input_token_stream: proc_
     };
     // let filter_array_token_stream = Filter::into_array().map(|element|generate_filters_token_stream(&element));
     let equal_token_stream = generate_filters_token_stream(&Filter::Equal);
+    let greater_than_token_stream = generate_filters_token_stream(&Filter::GreaterThan);
     let generated = quote::quote! {
         // #(#filter_array_token_stream)*
         #equal_token_stream
+        #greater_than_token_stream
     };
     // macros_helpers::write_token_stream_into_file::write_token_stream_into_file(
     //     "GeneratePostgresqlTypeWhereElementFilters",
