ceb655fb38fa31dac8b981280a4a66363c8a51dc
kuqmua <serezhashmakov@outlook.com>
2025-04-18 20:04:48 +0300

    next steps

--- postgresql_crud/src/postgresql_json_type.rs
+++ postgresql_crud/src/postgresql_json_type.rs
@@ -1,2 +1,7 @@
-generate_postgresql_json_types::generate_postgresql_json_types!();
-
+generate_postgresql_json_types::generate_postgresql_json_types!([
+    {
+        "postgresql_json_type": "StdPrimitiveI8",
+        "not_null_or_nullable": "NotNull",
+        "postgresql_json_type_pattern": "Standart"
+    }
+]);
