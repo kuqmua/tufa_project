632b5325150caed43ff0314d66aa118be4d46516
kuqmua <serezhashmakov@outlook.com>
2025-04-23 17:27:27 +0300

    next steps

--- postgresql_crud/generate_postgresql_json_types/src/lib.rs
+++ postgresql_crud/generate_postgresql_json_types/src/lib.rs
@@ -1709,14 +1709,15 @@ pub fn generate_postgresql_json_types(input_token_stream: proc_macro::TokenStrea
                                     &generate_jsonb_agg_d3(
                                         &format!("(select jsonb_agg(d4_elem.value) from jsonb_array_elements(d3_elem.value) with ordinality as d4_elem(value, d4_ord) where d4_ord between {{dimension4_start}} and {{dimension4_end}})")
                                     )
-                                    // &format!("(select jsonb_agg() from jsonb_array_elements(d2_elem.value) with ordinality as d3_elem(value, d3_ord) where d3_ord between {{dimension3_start}} and {{dimension3_end}})")
                                 )
                             )
                         );
                         let not_null_nullable_not_null_nullable = generate_jsonb_agg_d1(
                             &generate_case_when_jsonb_typeof_array_then_else_null_end_d1(
                                 &generate_jsonb_agg_d2(
-                                    &format!("(select jsonb_agg(case when jsonb_typeof(d3_elem.value)='array' then ((select jsonb_agg(d4_elem.value) from jsonb_array_elements(d3_elem.value) with ordinality as d4_elem(value, d4_ord) where d4_ord between {{dimension4_start}} and {{dimension4_end}})) else null end) from jsonb_array_elements(d2_elem.value) with ordinality as d3_elem(value, d3_ord) where d3_ord between {{dimension3_start}} and {{dimension3_end}})")
+                                    &generate_jsonb_agg_d3(
+                                        &format!("case when jsonb_typeof(d3_elem.value)='array' then ((select jsonb_agg(d4_elem.value) from jsonb_array_elements(d3_elem.value) with ordinality as d4_elem(value, d4_ord) where d4_ord between {{dimension4_start}} and {{dimension4_end}})) else null end")
+                                    )
                                 )
                             )
                         );
