cda784c296678a9f5fe468dae31f243fcb9bb375
kuqmua <serezhashmakov@outlook.com>
2025-05-06 22:42:40 +0300

    next steps

--- postgresql_crud/generate_postgresql_json_object_type/src/lib.rs
+++ postgresql_crud/generate_postgresql_json_object_type/src/lib.rs
@@ -412,17 +412,17 @@ pub fn generate_postgresql_json_object_type(input_token_stream: proc_macro::Toke
             });
             quote::quote!{{#(#content_token_stream),*}}
         };
+        //todo maybe remove useless derive?
+        let generate_ident_table_type_declaration_or_create_token_stream = |ident_token_stream: &dyn quote::ToTokens, content_token_stream: &dyn quote::ToTokens|{
+            quote::quote! {
+                #[derive(Debug, Clone, PartialEq, Default, serde::Serialize, serde::Deserialize, utoipa::ToSchema, schemars::JsonSchema)]
+                pub struct #ident_token_stream #content_token_stream
+            }
+        };
         
         let ident_table_type_declaration_upper_camel_case = naming::parameter::SelfTableTypeDeclarationUpperCamelCase::from_tokens(&ident);
         let ident_with_id_table_type_declaration_standart_not_null_upper_camel_case = naming::parameter::SelfTableTypeDeclarationUpperCamelCase::from_tokens(&ident_with_id_standart_not_null_upper_camel_case);
         let ident_table_type_declaration_token_stream = {
-            //todo reuse with create and table_type_declaration
-            let generate_ident_table_type_declaration_or_ident_with_id_table_type_declaration_token_stream = |ident_token_stream: &dyn quote::ToTokens, content_token_stream: &dyn quote::ToTokens|{
-                quote::quote! {
-                    #[derive(Debug, Clone, PartialEq, Default, serde::Serialize, serde::Deserialize, utoipa::ToSchema, schemars::JsonSchema)]
-                    pub struct #ident_token_stream #content_token_stream
-                }
-            };
             let generate_ident_create_or_ident_with_id_table_type_declaration_standart_not_null_upper_camel_case = |is_standart_with_id: &IsStandartWithId|{
                 match &is_standart_with_id {
                     IsStandartWithId::False => &ident_table_type_declaration_upper_camel_case,
@@ -477,7 +477,7 @@ pub fn generate_postgresql_json_object_type(input_token_stream: proc_macro::Toke
                 )
             };
 
-            let ident_table_type_declaration_token_stream = generate_ident_table_type_declaration_or_ident_with_id_table_type_declaration_token_stream(
+            let ident_table_type_declaration_token_stream = generate_ident_table_type_declaration_or_create_token_stream(
                 &ident_table_type_declaration_upper_camel_case,
                 &{
                     let prefix_wrapper = |tokens: &dyn quote::ToTokens|{
@@ -659,8 +659,7 @@ pub fn generate_postgresql_json_object_type(input_token_stream: proc_macro::Toke
             let maybe_ident_with_id_standart_not_null_table_type_declaration_token_stream = match &postgresql_json_type_pattern {
                 postgresql_crud_macros_common::PostgresqlJsonTypePattern::Standart => match &not_null_or_nullable {
                     postgresql_crud_macros_common::NotNullOrNullable::NotNull => {
-                        //todo maybe remove useless derive?
-                        let ident_with_id_table_type_declaration_standart_not_null_token_stream = generate_ident_table_type_declaration_or_ident_with_id_table_type_declaration_token_stream(
+                        let ident_with_id_table_type_declaration_standart_not_null_token_stream = generate_ident_table_type_declaration_or_create_token_stream(
                             &ident_with_id_table_type_declaration_standart_not_null_upper_camel_case,
                             &generate_ident_table_type_declaration_or_create_or_ident_with_id_table_type_declaration_or_create_content_token_stream(
                                 &IsStandartWithId::True,
@@ -700,12 +699,6 @@ pub fn generate_postgresql_json_object_type(input_token_stream: proc_macro::Toke
         let ident_with_id_create_standart_not_null_upper_camel_case = naming::parameter::SelfCreateUpperCamelCase::from_tokens(&ident_with_id_standart_not_null_upper_camel_case);
         let ident_create_token_stream = {
             let ident_with_id_table_type_declaration_standart_not_null_upper_camel_case = naming::parameter::SelfTableTypeDeclarationUpperCamelCase::from_tokens(&ident_with_id_standart_not_null_upper_camel_case);
-            let generate_ident_create_token_stream = |ident_token_stream: &dyn quote::ToTokens, content_token_stream: &dyn quote::ToTokens|{
-                quote::quote! {
-                    #[derive(Debug, Clone, PartialEq, Default, serde::Serialize, serde::Deserialize, utoipa::ToSchema, schemars::JsonSchema)]
-                    pub struct #ident_token_stream #content_token_stream
-                }
-            };
             let generate_impl_std_fmt_display_for_ident_create_or_ident_with_id_create_token_stream = |ident_token_stream: &dyn quote::ToTokens|{
                 macros_helpers::generate_impl_std_fmt_display_token_stream(
                     &proc_macro2::TokenStream::new(),
@@ -746,7 +739,7 @@ pub fn generate_postgresql_json_object_type(input_token_stream: proc_macro::Toke
                 )
             };
 
-            let ident_create_token_stream = generate_ident_create_token_stream(
+            let ident_create_token_stream = generate_ident_table_type_declaration_or_create_token_stream(
                 &ident_create_upper_camel_case,
                 &{
                     let prefix_wrapper = |tokens: &dyn quote::ToTokens|{
@@ -1305,7 +1298,7 @@ pub fn generate_postgresql_json_object_type(input_token_stream: proc_macro::Toke
                 postgresql_crud_macros_common::PostgresqlJsonTypePattern::Standart => match &not_null_or_nullable {
                     postgresql_crud_macros_common::NotNullOrNullable::NotNull => {
                         //todo maybe remove useless derive?
-                        let ident_with_id_create_standart_not_null_token_stream = generate_ident_create_token_stream(
+                        let ident_with_id_create_standart_not_null_token_stream = generate_ident_table_type_declaration_or_create_token_stream(
                             &ident_with_id_create_standart_not_null_upper_camel_case,
                             &generate_ident_table_type_declaration_or_create_or_ident_with_id_table_type_declaration_or_create_content_token_stream(
                                 &IsStandartWithId::False,
