efc889081a4616b04657cf75ffee9761f3f42d27
kuqmua <serezhashmakov@outlook.com>
2025-05-08 11:09:15 +0300

    next steps

--- postgresql_crud/generate_postgresql_json_object_type/src/lib.rs
+++ postgresql_crud/generate_postgresql_json_object_type/src/lib.rs
@@ -2323,8 +2323,7 @@ pub fn generate_postgresql_json_object_type(input_token_stream: proc_macro::Toke
                     }
                 }
             };
-            //todo rename
-            let ident_read_or_ident_with_id_read_try_from_error_named_token_stream = generate_ident_read_try_from_error_named_token_stream(&ident_read_try_from_error_named_upper_camel_case);
+            let ident_read_try_from_error_named_token_stream = generate_ident_read_try_from_error_named_token_stream(&ident_read_try_from_error_named_upper_camel_case);
             let generate_impl_try_new_for_ident_read_try_from_error_named_token_stream = |is_standart_with_id: &IsStandartWithId|{
                 let ident_token_stream: &dyn naming::StdFmtDisplayPlusQuoteToTokens = match &is_standart_with_id {
                     IsStandartWithId::False => &ident_read_upper_camel_case,
@@ -2400,8 +2399,8 @@ pub fn generate_postgresql_json_object_type(input_token_stream: proc_macro::Toke
                     }
                 }
             };
-            let impl_try_new_for_ident_read_or_ident_with_id_read_try_from_error_named_token_stream = generate_impl_try_new_for_ident_read_try_from_error_named_token_stream(&IsStandartWithId::False);
-            let generate_impl_serde_deserialize_for_ident_read_or_ident_with_id_read_token_stream = |is_standart_with_id: &IsStandartWithId| {
+            let impl_try_new_for_ident_read_try_from_error_named_token_stream = generate_impl_try_new_for_ident_read_try_from_error_named_token_stream(&IsStandartWithId::False);
+            let generate_impl_serde_deserialize_for_ident_read_token_stream = |is_standart_with_id: &IsStandartWithId| {
                 let ident_token_stream: &dyn naming::StdFmtDisplayPlusQuoteToTokens = match &is_standart_with_id {
                     IsStandartWithId::False => &ident_read_upper_camel_case,
                     IsStandartWithId::True => &ident_with_id_read_standart_not_null_upper_camel_case,
@@ -2770,8 +2769,8 @@ pub fn generate_postgresql_json_object_type(input_token_stream: proc_macro::Toke
                     }
                 }
             };
-            let impl_serde_deserialize_for_ident_read_or_ident_with_id_read_token_stream = generate_impl_serde_deserialize_for_ident_read_or_ident_with_id_read_token_stream(&IsStandartWithId::False);
-            let generate_impl_postgresql_crud_default_but_option_is_always_some_and_vec_always_contains_one_element_for_ident_read_or_ident_with_id_read_token_stream = |is_standart_with_id: &IsStandartWithId|{
+            let impl_serde_deserialize_for_ident_read_token_stream = generate_impl_serde_deserialize_for_ident_read_token_stream(&IsStandartWithId::False);
+            let generate_impl_postgresql_crud_default_but_option_is_always_some_and_vec_always_contains_one_element_for_ident_read_token_stream = |is_standart_with_id: &IsStandartWithId|{
                 postgresql_crud_macros_common::generate_impl_postgresql_crud_default_but_option_is_always_some_and_vec_always_contains_one_element_for_tokens_token_stream(
                     &{
                         let value: &dyn quote::ToTokens = match &is_standart_with_id {
@@ -2796,9 +2795,9 @@ pub fn generate_postgresql_json_object_type(input_token_stream: proc_macro::Toke
                     },
                 )
             };
-            let impl_postgresql_crud_default_but_option_is_always_some_and_vec_always_contains_one_element_for_ident_read_or_ident_with_id_read_token_stream = generate_impl_postgresql_crud_default_but_option_is_always_some_and_vec_always_contains_one_element_for_ident_read_or_ident_with_id_read_token_stream(&IsStandartWithId::False);
-            let impl_sqlx_type_sqlx_postgres_for_ident_read_or_ident_with_id_read_token_stream = generate_sqlx_types_json_type_declaration_wrapper_token_stream(&ident_read_upper_camel_case);
-            let impl_sqlx_decode_sqlx_postgres_for_ident_read_or_ident_with_id_read_token_stream = generate_impl_sqlx_decode_sqlx_postgres_for_ident_wrapper_token_stream(
+            let impl_postgresql_crud_default_but_option_is_always_some_and_vec_always_contains_one_element_for_ident_read_token_stream = generate_impl_postgresql_crud_default_but_option_is_always_some_and_vec_always_contains_one_element_for_ident_read_token_stream(&IsStandartWithId::False);
+            let impl_sqlx_type_sqlx_postgres_for_ident_read_token_stream = generate_sqlx_types_json_type_declaration_wrapper_token_stream(&ident_read_upper_camel_case);
+            let impl_sqlx_decode_sqlx_postgres_for_ident_read_token_stream = generate_impl_sqlx_decode_sqlx_postgres_for_ident_wrapper_token_stream(
                 &ident_read_upper_camel_case
             );
             let maybe_ident_with_id_read_token_stream = match &postgresql_json_type_pattern {
@@ -2813,8 +2812,8 @@ pub fn generate_postgresql_json_object_type(input_token_stream: proc_macro::Toke
                         );
                         let ident_with_id_read_try_from_error_named_token_stream = generate_ident_read_try_from_error_named_token_stream(&ident_with_id_read_try_from_error_named_standart_not_null_upper_camel_case);
                         let impl_try_new_for_ident_with_id_read_try_from_error_named_token_stream = generate_impl_try_new_for_ident_read_try_from_error_named_token_stream(&IsStandartWithId::True);
-                        let impl_serde_deserialize_for_ident_with_id_read_token_stream = generate_impl_serde_deserialize_for_ident_read_or_ident_with_id_read_token_stream(&IsStandartWithId::True);
-                        let impl_postgresql_crud_default_but_option_is_always_some_and_vec_always_contains_one_element_for_ident_with_id_read_token_stream = generate_impl_postgresql_crud_default_but_option_is_always_some_and_vec_always_contains_one_element_for_ident_read_or_ident_with_id_read_token_stream(&IsStandartWithId::True);
+                        let impl_serde_deserialize_for_ident_with_id_read_token_stream = generate_impl_serde_deserialize_for_ident_read_token_stream(&IsStandartWithId::True);
+                        let impl_postgresql_crud_default_but_option_is_always_some_and_vec_always_contains_one_element_for_ident_with_id_read_token_stream = generate_impl_postgresql_crud_default_but_option_is_always_some_and_vec_always_contains_one_element_for_ident_read_token_stream(&IsStandartWithId::True);
                         let impl_sqlx_type_sqlx_postgres_for_ident_with_id_read_token_stream = generate_sqlx_types_json_type_declaration_wrapper_token_stream(&ident_with_id_read_standart_not_null_upper_camel_case);
                         let impl_sqlx_decode_sqlx_postgres_for_ident_with_id_read_token_stream = generate_impl_sqlx_decode_sqlx_postgres_for_ident_wrapper_token_stream(
                             &ident_with_id_read_standart_not_null_upper_camel_case
@@ -2838,12 +2837,12 @@ pub fn generate_postgresql_json_object_type(input_token_stream: proc_macro::Toke
             };
             quote::quote! {
                 #ident_read_token_stream
-                #ident_read_or_ident_with_id_read_try_from_error_named_token_stream
-                #impl_try_new_for_ident_read_or_ident_with_id_read_try_from_error_named_token_stream
-                #impl_serde_deserialize_for_ident_read_or_ident_with_id_read_token_stream
-                #impl_postgresql_crud_default_but_option_is_always_some_and_vec_always_contains_one_element_for_ident_read_or_ident_with_id_read_token_stream
-                #impl_sqlx_type_sqlx_postgres_for_ident_read_or_ident_with_id_read_token_stream
-                #impl_sqlx_decode_sqlx_postgres_for_ident_read_or_ident_with_id_read_token_stream
+                #ident_read_try_from_error_named_token_stream
+                #impl_try_new_for_ident_read_try_from_error_named_token_stream
+                #impl_serde_deserialize_for_ident_read_token_stream
+                #impl_postgresql_crud_default_but_option_is_always_some_and_vec_always_contains_one_element_for_ident_read_token_stream
+                #impl_sqlx_type_sqlx_postgres_for_ident_read_token_stream
+                #impl_sqlx_decode_sqlx_postgres_for_ident_read_token_stream
                 #maybe_ident_with_id_read_token_stream
             }
         };
