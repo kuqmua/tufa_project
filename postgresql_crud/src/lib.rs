542fffd0062df00e259339667b3a18c712a4cb45
kuqmua <serezhashmakov@outlook.com>
2025-05-17 09:50:17 +0300

    next steps

--- postgresql_crud/src/lib.rs
+++ postgresql_crud/src/lib.rs
@@ -95,6 +95,54 @@ where
     }
 }
 
+#[derive(Debug, Clone, PartialEq, Default, serde::Serialize, serde::Deserialize, utoipa::ToSchema, schemars::JsonSchema)]
+pub struct ObjectWithIdAsNotNullJsonbObjectWithIdUpdateElement<T: PostgresqlJsonType> {
+    pub id: <crate::postgresql_json_type::UuidUuidAsNotNullJsonbString as PostgresqlJsonType>::Update,
+    pub fields: <T as PostgresqlJsonType>::Update,
+}
+impl<T: PostgresqlJsonType> DefaultButOptionIsAlwaysSomeAndVecAlwaysContainsOneElement for ObjectWithIdAsNotNullJsonbObjectWithIdUpdateElement<T> {
+    fn default_but_option_is_always_some_and_vec_always_contains_one_element() -> Self {
+        Self {
+            id: DefaultButOptionIsAlwaysSomeAndVecAlwaysContainsOneElement::default_but_option_is_always_some_and_vec_always_contains_one_element(),
+            fields: DefaultButOptionIsAlwaysSomeAndVecAlwaysContainsOneElement::default_but_option_is_always_some_and_vec_always_contains_one_element(),
+        }
+    }
+}
+impl<T: PostgresqlJsonType> ObjectWithIdAsNotNullJsonbObjectWithIdUpdateElement<T> {
+    pub fn update_query_part(
+        &self,
+        jsonb_set_accumulator: &std::primitive::str,
+        jsonb_set_target: &std::primitive::str,
+        jsonb_set_path: &std::primitive::str,
+        increment: &mut std::primitive::u64
+    ) -> Result<std::string::String, crate::QueryPartErrorNamed> {
+        let id_increment = match increment.checked_add(1) {
+            Some(value) => {
+                *increment = value;
+                increment.to_string()
+            }
+            None => {
+                return Err(crate::QueryPartErrorNamed::CheckedAdd { code_occurence: error_occurence_lib::code_occurence!() });
+            }
+        };
+        match <T as PostgresqlJsonType>::update_query_part(
+            &self.fields,
+            &jsonb_set_accumulator,
+            &jsonb_set_target,
+            &jsonb_set_path,
+            increment
+        ) {
+            Ok(value) => Ok(format!("when {jsonb_set_target}->>'id' = ${id_increment} then {value} ")),
+            Err(error) => Err(error),
+        }
+    }
+    pub fn update_query_bind(self, mut query: sqlx::query::Query<'_, sqlx::Postgres, sqlx::postgres::PgArguments>) -> sqlx::query::Query<'_, sqlx::Postgres, sqlx::postgres::PgArguments> {
+        query = self.id.query_bind_as_postgresql_text(query);
+        query = <T as PostgresqlJsonType>::update_query_bind(self.fields, query);
+        query
+    }
+}
+
 pub trait PostgresqlTypePrimaryKey {
     type PrimaryKey;
 }
