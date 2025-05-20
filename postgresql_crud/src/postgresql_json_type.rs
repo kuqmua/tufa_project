4f307d660a7a4a0f10b5769c9889168a9a00534d
kuqmua <serezhashmakov@outlook.com>
2025-05-20 09:54:30 +0300

    next steps

--- postgresql_crud/src/postgresql_json_type.rs
+++ postgresql_crud/src/postgresql_json_type.rs
@@ -1,12 +1,12 @@
 generate_postgresql_json_types::generate_postgresql_json_types!([
     {
         "postgresql_json_type": "StdPrimitiveI8AsJsonbNumber",
-        "not_null_or_nullable": "NotNull",
+        "not_null_or_nullable": "Nullable",
         "postgresql_json_type_pattern": {
             "ArrayDimension3": {
-                "dimension1_not_null_or_nullable": "Nullable",
-                "dimension2_not_null_or_nullable": "Nullable",
-                "dimension3_not_null_or_nullable": "Nullable"
+                "dimension1_not_null_or_nullable": "NotNull",
+                "dimension2_not_null_or_nullable": "NotNull",
+                "dimension3_not_null_or_nullable": "NotNull"
             }
         }
     },
