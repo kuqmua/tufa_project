9329eed633f57ae8a62688a41a2b64df3a6682fe
kuqmua <serezhashmakov@outlook.com>
2025-05-20 09:43:30 +0300

    next steps

--- postgresql_crud/generate_postgresql_json_types/src/lib.rs
+++ postgresql_crud/generate_postgresql_json_types/src/lib.rs
@@ -599,7 +599,19 @@ pub fn generate_postgresql_json_types(input_token_stream: proc_macro::TokenStrea
                                 acc
                             },
                             (NotNullOrNullable::Nullable, NotNullOrNullable::NotNull, NotNullOrNullable::NotNull) => {
-                                vec![]
+                                let mut acc = vec![];
+                                for element in generate_vec_handle(PostgresqlJsonTypeRecordHandle {
+                                    not_null_or_nullable: NotNullOrNullable::NotNull,
+                                    postgresql_json_type_pattern: PostgresqlJsonTypePattern::ArrayDimension3 {
+                                        dimension1_not_null_or_nullable: NotNullOrNullable::NotNull,
+                                        dimension2_not_null_or_nullable: NotNullOrNullable::Nullable,
+                                        dimension3_not_null_or_nullable: NotNullOrNullable::Nullable,
+                                    },
+                                }) {
+                                    acc.push(element);
+                                }
+                                acc.push(postgresql_json_type_record_handle);
+                                acc
                             },
                             (NotNullOrNullable::Nullable, NotNullOrNullable::NotNull, NotNullOrNullable::Nullable) => {
                                 vec![]
--- postgresql_crud/src/postgresql_json_type.rs
+++ postgresql_crud/src/postgresql_json_type.rs
@@ -4,9 +4,9 @@ generate_postgresql_json_types::generate_postgresql_json_types!([
         "not_null_or_nullable": "NotNull",
         "postgresql_json_type_pattern": {
             "ArrayDimension3": {
-                "dimension1_not_null_or_nullable": "NotNull",
-                "dimension2_not_null_or_nullable": "Nullable",
-                "dimension3_not_null_or_nullable": "Nullable"
+                "dimension1_not_null_or_nullable": "Nullable",
+                "dimension2_not_null_or_nullable": "NotNull",
+                "dimension3_not_null_or_nullable": "NotNull"
             }
         }
     },
