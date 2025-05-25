37e85ddf7279a0df0a2beb1dd786ec90b09584ee
kuqmua <serezhashmakov@outlook.com>
2025-05-25 15:27:01 +0300

    next steps

--- postgresql_crud/generate_postgresql_json_object_type/src/lib.rs
+++ postgresql_crud/generate_postgresql_json_object_type/src/lib.rs
@@ -1805,7 +1805,10 @@ pub fn generate_postgresql_json_object_type(input_token_stream: proc_macro::Toke
                         postgresql_crud_macros_common::NotNullOrNullable::Nullable => generate_ident_read_wrapper_token_stream(
                             &{
                                 let type_token_stream = postgresql_crud_macros_common::generate_std_option_option_tokens_declaration_token_stream(
-                                    &ident_with_id_read_array_not_null_upper_camel_case
+                                    &generate_type_as_postgresql_json_type_subtype_token_stream(
+                                        &ident_with_id_array_not_null_upper_camel_case,
+                                        &PostgresqlJsonTypeSubtype::Read
+                                    )
                                 );
                                 quote::quote!{(#type_token_stream);}
                             },
