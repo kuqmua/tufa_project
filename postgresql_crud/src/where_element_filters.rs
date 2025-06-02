bfc576d4442f2d1c8a28b3ee6c33f11dfc95ccfe
kuqmua <serezhashmakov@outlook.com>
2025-06-02 23:40:39 +0300

    next steps

--- postgresql_crud/src/where_element_filters.rs
+++ postgresql_crud/src/where_element_filters.rs
@@ -45,159 +45,149 @@ impl PostgresqlJsonTypeWhereElementCaseInsensitiveRegularExpression {
         Self { logical_operator, value }
     }
 }
-// const _: () = {
-//     #[allow(unused_extern_crates, clippy::useless_attribute)]
-//     extern crate serde as _serde;
-//     #[automatically_derived]
-//     impl<'de> _serde::Deserialize<'de> for PostgresqlJsonTypeWhereElementCaseInsensitiveRegularExpression {
-//         fn deserialize<__D>(__deserializer: __D) -> _serde::__private::Result<Self, __D::Error>
-//         where
-//             __D: _serde::Deserializer<'de>,
-//         {
-//             #[allow(non_camel_case_types)]
-//             #[doc(hidden)]
-//             enum __Field {
-//                 __field0,
-//                 __field1,
-//                 __ignore,
-//             }
-//             #[doc(hidden)]
-//             struct __FieldVisitor;
-//             impl<'de> _serde::de::Visitor<'de> for __FieldVisitor {
-//                 type Value = __Field;
-//                 fn expecting(&self, __f: &mut _serde::__private::Formatter<'_>) -> _serde::__private::fmt::Result {
-//                     _serde::__private::Formatter::write_str(__f, "field identifier")
-//                 }
-//                 fn visit_u64<__E>(self, __value: u64) -> _serde::__private::Result<Self::Value, __E>
-//                 where
-//                     __E: _serde::de::Error,
-//                 {
-//                     match __value {
-//                         0u64 => _serde::__private::Ok(__Field::__field0),
-//                         1u64 => _serde::__private::Ok(__Field::__field1),
-//                         _ => _serde::__private::Ok(__Field::__ignore),
-//                     }
-//                 }
-//                 fn visit_str<__E>(self, __value: &str) -> _serde::__private::Result<Self::Value, __E>
-//                 where
-//                     __E: _serde::de::Error,
-//                 {
-//                     match __value {
-//                         "logical_operator" => _serde::__private::Ok(__Field::__field0),
-//                         "value" => _serde::__private::Ok(__Field::__field1),
-//                         _ => _serde::__private::Ok(__Field::__ignore),
-//                     }
-//                 }
-//                 fn visit_bytes<__E>(self, __value: &[u8]) -> _serde::__private::Result<Self::Value, __E>
-//                 where
-//                     __E: _serde::de::Error,
-//                 {
-//                     match __value {
-//                         b"logical_operator" => _serde::__private::Ok(__Field::__field0),
-//                         b"value" => _serde::__private::Ok(__Field::__field1),
-//                         _ => _serde::__private::Ok(__Field::__ignore),
-//                     }
-//                 }
-//             }
-//             impl<'de> _serde::Deserialize<'de> for __Field {
-//                 #[inline]
-//                 fn deserialize<__D>(__deserializer: __D) -> _serde::__private::Result<Self, __D::Error>
-//                 where
-//                     __D: _serde::Deserializer<'de>,
-//                 {
-//                     _serde::Deserializer::deserialize_identifier(__deserializer, __FieldVisitor)
-//                 }
-//             }
-//             #[doc(hidden)]
-//             struct __Visitor<'de> {
-//                 marker: _serde::__private::PhantomData<PostgresqlJsonTypeWhereElementCaseInsensitiveRegularExpression>,
-//                 lifetime: _serde::__private::PhantomData<&'de ()>,
-//             }
-//             impl<'de> _serde::de::Visitor<'de> for __Visitor<'de> {
-//                 type Value = PostgresqlJsonTypeWhereElementCaseInsensitiveRegularExpression;
-//                 fn expecting(&self, __f: &mut _serde::__private::Formatter<'_>) -> _serde::__private::fmt::Result {
-//                     _serde::__private::Formatter::write_str(__f, "struct PostgresqlJsonTypeWhereElementCaseInsensitiveRegularExpression")
-//                 }
-//                 #[inline]
-//                 fn visit_seq<__A>(self, mut __seq: __A) -> _serde::__private::Result<Self::Value, __A::Error>
-//                 where
-//                     __A: _serde::de::SeqAccess<'de>,
-//                 {
-//                     let __field0 = match _serde::de::SeqAccess::next_element::<crate::LogicalOperator>(&mut __seq)? {
-//                         _serde::__private::Some(__value) => __value,
-//                         _serde::__private::None => {
-//                             return _serde::__private::Err(_serde::de::Error::invalid_length(0usize, &"struct PostgresqlJsonTypeWhereElementCaseInsensitiveRegularExpression with 2 elements"));
-//                         }
-//                     };
-//                     let __field1 = match _serde::de::SeqAccess::next_element::<regex::Regex>(&mut __seq)? {
-//                         _serde::__private::Some(__value) => __value,
-//                         _serde::__private::None => {
-//                             return _serde::__private::Err(_serde::de::Error::invalid_length(1usize, &"struct PostgresqlJsonTypeWhereElementCaseInsensitiveRegularExpression with 2 elements"));
-//                         }
-//                     };
-//                     match PostgresqlJsonTypeWhereElementCaseInsensitiveRegularExpression::try_new(__field0, __field1) {
-//                         Ok(value) => serde::__private::Ok(value),
-//                         Err(error) => {
-//                             return Err(serde::de::Error::custom(format!("{error:?}")));
-//                         }
-//                     }
-//                 }
-//                 #[inline]
-//                 fn visit_map<__A>(self, mut __map: __A) -> _serde::__private::Result<Self::Value, __A::Error>
-//                 where
-//                     __A: _serde::de::MapAccess<'de>,
-//                 {
-//                     let mut __field0: _serde::__private::Option<crate::LogicalOperator> = _serde::__private::None;
-//                     let mut __field1: _serde::__private::Option<regex::Regex> = _serde::__private::None;
-//                     while let _serde::__private::Some(__key) = _serde::de::MapAccess::next_key::<__Field>(&mut __map)? {
-//                         match __key {
-//                             __Field::__field0 => {
-//                                 if _serde::__private::Option::is_some(&__field0) {
-//                                     return _serde::__private::Err(<__A::Error as _serde::de::Error>::duplicate_field("logical_operator"));
-//                                 }
-//                                 __field0 = _serde::__private::Some(_serde::de::MapAccess::next_value::<crate::LogicalOperator>(&mut __map)?);
-//                             }
-//                             __Field::__field1 => {
-//                                 if _serde::__private::Option::is_some(&__field1) {
-//                                     return _serde::__private::Err(<__A::Error as _serde::de::Error>::duplicate_field("value"));
-//                                 }
-//                                 __field1 = _serde::__private::Some(_serde::de::MapAccess::next_value::<regex::Regex>(&mut __map)?);
-//                             }
-//                             _ => {
-//                                 let _ = _serde::de::MapAccess::next_value::<_serde::de::IgnoredAny>(&mut __map)?;
-//                             }
-//                         }
-//                     }
-//                     let __field0 = match __field0 {
-//                         _serde::__private::Some(__field0) => __field0,
-//                         _serde::__private::None => _serde::__private::de::missing_field("logical_operator")?,
-//                     };
-//                     let __field1 = match __field1 {
-//                         _serde::__private::Some(__field1) => __field1,
-//                         _serde::__private::None => _serde::__private::de::missing_field("value")?,
-//                     };
-//                     match PostgresqlJsonTypeWhereElementCaseInsensitiveRegularExpression::try_new(__field0, __field1) {
-//                         Ok(value) => serde::__private::Ok(value),
-//                         Err(error) => {
-//                             return Err(serde::de::Error::custom(format!("{error:?}")));
-//                         }
-//                     }
-//                 }
-//             }
-//             #[doc(hidden)]
-//             const FIELDS: &'static [&'static str] = &["logical_operator", "value"];
-//             _serde::Deserializer::deserialize_struct(
-//                 __deserializer,
-//                 "PostgresqlJsonTypeWhereElementCaseInsensitiveRegularExpression",
-//                 FIELDS,
-//                 __Visitor {
-//                     marker: _serde::__private::PhantomData::<PostgresqlJsonTypeWhereElementCaseInsensitiveRegularExpression>,
-//                     lifetime: _serde::__private::PhantomData,
-//                 },
-//             )
-//         }
-//     }
-// };
+const _: () = {
+    #[allow(unused_extern_crates, clippy::useless_attribute)]
+    extern crate serde as _serde;
+    #[automatically_derived]
+    impl<'de> _serde::Deserialize<'de> for PostgresqlJsonTypeWhereElementCaseInsensitiveRegularExpression {
+        fn deserialize<__D>(__deserializer: __D) -> _serde::__private::Result<Self, __D::Error>
+        where
+            __D: _serde::Deserializer<'de>,
+        {
+            #[allow(non_camel_case_types)]
+            #[doc(hidden)]
+            enum __Field {
+                __field0,
+                __field1,
+                __ignore,
+            }
+            #[doc(hidden)]
+            struct __FieldVisitor;
+            impl<'de> _serde::de::Visitor<'de> for __FieldVisitor {
+                type Value = __Field;
+                fn expecting(&self, __f: &mut _serde::__private::Formatter<'_>) -> _serde::__private::fmt::Result {
+                    _serde::__private::Formatter::write_str(__f, "field identifier")
+                }
+                fn visit_u64<__E>(self, __value: u64) -> _serde::__private::Result<Self::Value, __E>
+                where
+                    __E: _serde::de::Error,
+                {
+                    match __value {
+                        0u64 => _serde::__private::Ok(__Field::__field0),
+                        1u64 => _serde::__private::Ok(__Field::__field1),
+                        _ => _serde::__private::Ok(__Field::__ignore),
+                    }
+                }
+                fn visit_str<__E>(self, __value: &str) -> _serde::__private::Result<Self::Value, __E>
+                where
+                    __E: _serde::de::Error,
+                {
+                    match __value {
+                        "logical_operator" => _serde::__private::Ok(__Field::__field0),
+                        "value" => _serde::__private::Ok(__Field::__field1),
+                        _ => _serde::__private::Ok(__Field::__ignore),
+                    }
+                }
+                fn visit_bytes<__E>(self, __value: &[u8]) -> _serde::__private::Result<Self::Value, __E>
+                where
+                    __E: _serde::de::Error,
+                {
+                    match __value {
+                        b"logical_operator" => _serde::__private::Ok(__Field::__field0),
+                        b"value" => _serde::__private::Ok(__Field::__field1),
+                        _ => _serde::__private::Ok(__Field::__ignore),
+                    }
+                }
+            }
+            impl<'de> _serde::Deserialize<'de> for __Field {
+                #[inline]
+                fn deserialize<__D>(__deserializer: __D) -> _serde::__private::Result<Self, __D::Error>
+                where
+                    __D: _serde::Deserializer<'de>,
+                {
+                    _serde::Deserializer::deserialize_identifier(__deserializer, __FieldVisitor)
+                }
+            }
+            #[doc(hidden)]
+            struct __Visitor<'de> {
+                marker: _serde::__private::PhantomData<PostgresqlJsonTypeWhereElementCaseInsensitiveRegularExpression>,
+                lifetime: _serde::__private::PhantomData<&'de ()>,
+            }
+            impl<'de> _serde::de::Visitor<'de> for __Visitor<'de> {
+                type Value = PostgresqlJsonTypeWhereElementCaseInsensitiveRegularExpression;
+                fn expecting(&self, __f: &mut _serde::__private::Formatter<'_>) -> _serde::__private::fmt::Result {
+                    _serde::__private::Formatter::write_str(__f, "struct PostgresqlJsonTypeWhereElementCaseInsensitiveRegularExpression")
+                }
+                #[inline]
+                fn visit_seq<__A>(self, mut __seq: __A) -> _serde::__private::Result<Self::Value, __A::Error>
+                where
+                    __A: _serde::de::SeqAccess<'de>,
+                {
+                    let __field0 = match _serde::de::SeqAccess::next_element::<crate::LogicalOperator>(&mut __seq)? {
+                        _serde::__private::Some(__value) => __value,
+                        _serde::__private::None => {
+                            return _serde::__private::Err(_serde::de::Error::invalid_length(0usize, &"struct PostgresqlJsonTypeWhereElementCaseInsensitiveRegularExpression with 2 elements"));
+                        }
+                    };
+                    let __field1 = match _serde::de::SeqAccess::next_element::<crate::RegexRegex>(&mut __seq)? {
+                        _serde::__private::Some(__value) => __value,
+                        _serde::__private::None => {
+                            return _serde::__private::Err(_serde::de::Error::invalid_length(1usize, &"struct PostgresqlJsonTypeWhereElementCaseInsensitiveRegularExpression with 2 elements"));
+                        }
+                    };
+                    Ok(PostgresqlJsonTypeWhereElementCaseInsensitiveRegularExpression::new(__field0, __field1))
+                }
+                #[inline]
+                fn visit_map<__A>(self, mut __map: __A) -> _serde::__private::Result<Self::Value, __A::Error>
+                where
+                    __A: _serde::de::MapAccess<'de>,
+                {
+                    let mut __field0: _serde::__private::Option<crate::LogicalOperator> = _serde::__private::None;
+                    let mut __field1: _serde::__private::Option<crate::RegexRegex> = _serde::__private::None;
+                    while let _serde::__private::Some(__key) = _serde::de::MapAccess::next_key::<__Field>(&mut __map)? {
+                        match __key {
+                            __Field::__field0 => {
+                                if _serde::__private::Option::is_some(&__field0) {
+                                    return _serde::__private::Err(<__A::Error as _serde::de::Error>::duplicate_field("logical_operator"));
+                                }
+                                __field0 = _serde::__private::Some(_serde::de::MapAccess::next_value::<crate::LogicalOperator>(&mut __map)?);
+                            }
+                            __Field::__field1 => {
+                                if _serde::__private::Option::is_some(&__field1) {
+                                    return _serde::__private::Err(<__A::Error as _serde::de::Error>::duplicate_field("value"));
+                                }
+                                __field1 = _serde::__private::Some(_serde::de::MapAccess::next_value::<crate::RegexRegex>(&mut __map)?);
+                            }
+                            _ => {
+                                let _ = _serde::de::MapAccess::next_value::<_serde::de::IgnoredAny>(&mut __map)?;
+                            }
+                        }
+                    }
+                    let __field0 = match __field0 {
+                        _serde::__private::Some(__field0) => __field0,
+                        _serde::__private::None => _serde::__private::de::missing_field("logical_operator")?,
+                    };
+                    let __field1 = match __field1 {
+                        _serde::__private::Some(__field1) => __field1,
+                        _serde::__private::None => _serde::__private::de::missing_field("value")?,
+                    };
+                    Ok(PostgresqlJsonTypeWhereElementCaseInsensitiveRegularExpression::new(__field0, __field1))
+                }
+            }
+            #[doc(hidden)]
+            const FIELDS: &'static [&'static str] = &["logical_operator", "value"];
+            _serde::Deserializer::deserialize_struct(
+                __deserializer,
+                "PostgresqlJsonTypeWhereElementCaseInsensitiveRegularExpression",
+                FIELDS,
+                __Visitor {
+                    marker: _serde::__private::PhantomData::<PostgresqlJsonTypeWhereElementCaseInsensitiveRegularExpression>,
+                    lifetime: _serde::__private::PhantomData,
+                },
+            )
+        }
+    }
+};
 // const _: () = {
 //     #[automatically_derived]
 //     #[allow(unused_braces)]
