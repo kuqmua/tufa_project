d1dc10f29dcc25ac37231d8e7d78f29362d4f452
kuqmua <serezhashmakov@outlook.com>
2025-06-08 11:45:35 +0300

    next steps

--- postgresql_crud/generate_postgresql_json_types/src/lib.rs
+++ postgresql_crud/generate_postgresql_json_types/src/lib.rs
@@ -1555,7 +1555,13 @@ pub fn generate_postgresql_json_types(input_token_stream: proc_macro::TokenStrea
                                     vec
                                 };
                                 match &postgresql_json_type_specific {
-                                    PostgresqlJsonTypeSpecific::Number => common_array_dimension4_postgresql_json_type_filters,
+                                    PostgresqlJsonTypeSpecific::Number => {
+                                        let mut filters = common_array_dimension4_postgresql_json_type_filters.clone();
+                                        filters.push(postgresql_crud_macros_common::PostgresqlJsonTypeFilter::DimensionFourPositionGreaterThan {
+                                            ident: array_dimension4_inner_element_ident_origin_upper_camel_case.clone(),
+                                        });
+                                        filters
+                                    },
                                     PostgresqlJsonTypeSpecific::Bool => common_array_dimension4_postgresql_json_type_filters,
                                     PostgresqlJsonTypeSpecific::String => common_array_dimension4_postgresql_json_type_filters,
                                 }
