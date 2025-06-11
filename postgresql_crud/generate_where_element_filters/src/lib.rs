48a00425770055d41762a24b92ff3ffbb9558f96
kuqmua <serezhashmakov@outlook.com>
2025-06-11 16:34:10 +0300

    next etsps

--- postgresql_crud/generate_where_element_filters/src/lib.rs
+++ postgresql_crud/generate_where_element_filters/src/lib.rs
@@ -1707,6 +1707,71 @@ pub fn generate_where_element_filters(_input_token_stream: proc_macro::TokenStre
                     },
                 )
             };
+            let generate_dimension_all_elements_greater_than_token_stream = |dimension_number: &DimensionNumber| -> (
+                ShouldAddDeclarationOfStructIdentGeneric,
+                proc_macro2::TokenStream,
+                proc_macro2::TokenStream,
+                proc_macro2::TokenStream,
+                proc_macro2::TokenStream,
+            ) {
+                let range_minus_one = 1..=std::convert::Into::<std::primitive::u8>::into(dimension_number.clone()).checked_sub(1).unwrap();
+                (
+                    ShouldAddDeclarationOfStructIdentGeneric::True {
+                        maybe_additional_traits_token_stream: None
+                    },
+                    {
+                        let struct_additional_fields_token_stream = generate_struct_additional_fields_token_stream(range_minus_one.clone());
+                        quote::quote! {
+                            #struct_additional_fields_token_stream
+                            pub value: T
+                        }
+                    },
+                    {
+                        let impl_default_but_option_is_always_some_and_vec_always_contains_one_element_additional_fields_token_stream = generate_impl_default_but_option_is_always_some_and_vec_always_contains_one_element_additional_fields_token_stream(
+                            range_minus_one.clone()
+                        );
+                        quote::quote! {
+                            #impl_default_but_option_is_always_some_and_vec_always_contains_one_element_additional_fields_token_stream
+                            value: #path_default_but_option_is_always_some_and_vec_always_contains_one_element_token_stream
+                        }
+                    },
+                    {
+                        let increments_initialization_token_stream = generate_increments_initialization_token_stream(range_minus_one.clone());
+                        let format_handle_token_stream = generate_quotes::double_quotes_token_stream(&format!(
+                            // "{{}}(exists(select 1 from jsonb_array_elements({{}}{}) as el where (el) > ${{value}}))",
+                            "{}(not exists(select 1 from jsonb_array_elements({}) as el where (el) <= ${}))",
+                            generate_indexes_stringified(range_minus_one.clone())
+                        ));
+                        let format_increments_token_stream = generate_format_increments_token_stream(range_minus_one.clone());
+                        quote::quote! {
+                            #increments_initialization_token_stream
+                            let value = match increment.checked_add(1) {
+                                Some(value) => {
+                                    *increment = value;
+                                    value
+                                },
+                                None => {
+                                    return Err(#crate_query_part_error_named_checked_add_initialization_token_stream);
+                                },
+                            };
+                            Ok(format!(
+                                #format_handle_token_stream,
+                                &self.logical_operator.to_query_part(is_need_to_add_logical_operator),
+                                column,
+                                #format_increments_token_stream
+                            ))
+                        }
+                    },
+                    {
+                        let query_bind_dimension_position_token_stream = generate_query_bind_dimension_position_token_stream(range_minus_one);
+                        quote::quote! {
+                            #query_bind_dimension_position_token_stream
+                            query = query.bind(sqlx::types::Json(self.value));
+                            query
+                        }
+                    },
+                )
+            };
             let (
                 should_add_declaration_of_struct_ident_generic,
                 struct_additional_fields_token_stream,
