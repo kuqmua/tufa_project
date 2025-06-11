b9de4610b970c165afbab40713f36bd3990e45e8
kuqmua <serezhashmakov@outlook.com>
2025-06-11 23:04:08 +0300

    next steps

--- postgresql_crud/generate_where_element_filters/src/lib.rs
+++ postgresql_crud/generate_where_element_filters/src/lib.rs
@@ -1402,6 +1402,7 @@ pub fn generate_where_element_filters(_input_token_stream: proc_macro::TokenStre
             ) {
                 let dimension_number_std_primitive_u8 = std::convert::Into::<std::primitive::u8>::into(dimension_number.clone());
                 let range = 1..=dimension_number_std_primitive_u8;
+                let two_range = 2..=dimension_number_std_primitive_u8;
                 (
                     ShouldAddDeclarationOfStructIdentGeneric::False,
                     {
@@ -1426,7 +1427,7 @@ pub fn generate_where_element_filters(_input_token_stream: proc_macro::TokenStre
                         let increments_initialization_token_stream = generate_increments_initialization_token_stream(range.clone());
                         let format_handle_token_stream = generate_quotes::double_quotes_token_stream(&format!(
                             "{{}}((trim(both '\\\"' from ({{}}{}->>${{}})::text) {{}} ${{}}))",
-                            generate_indexes_stringified(2..=dimension_number_std_primitive_u8)
+                            generate_indexes_stringified(two_range)
                         ));
                         let format_increments_token_stream = generate_format_increments_token_stream(range.clone());
                         quote::quote! {
@@ -1772,6 +1773,74 @@ pub fn generate_where_element_filters(_input_token_stream: proc_macro::TokenStre
                             query
                         }
                     },
+                )
+            };
+            let generate_dimension_contains_element_regular_expression_token_stream = |dimension_number: &DimensionNumber| -> (
+                ShouldAddDeclarationOfStructIdentGeneric,
+                proc_macro2::TokenStream,
+                proc_macro2::TokenStream,
+                proc_macro2::TokenStream,
+                proc_macro2::TokenStream,
+            ) {
+                let dimension_number_std_primitive_u8 = std::convert::Into::<std::primitive::u8>::into(dimension_number.clone());
+                let range = 1..dimension_number_std_primitive_u8;
+                let two_range = 2..=dimension_number_std_primitive_u8;
+                (
+                    ShouldAddDeclarationOfStructIdentGeneric::False,
+                    {
+                        let struct_additional_fields_token_stream = generate_struct_additional_fields_token_stream(range.clone());
+                        quote::quote! {
+                            #struct_additional_fields_token_stream
+                            pub regular_expression_case: crate::RegularExpressionCase,
+                            pub value: crate::RegexRegex
+                        }
+                    },
+                    {
+                        let impl_default_but_option_is_always_some_and_vec_always_contains_one_element_additional_fields_token_stream = generate_impl_default_but_option_is_always_some_and_vec_always_contains_one_element_additional_fields_token_stream(
+                            range.clone()
+                        );
+                        quote::quote! {
+                            #impl_default_but_option_is_always_some_and_vec_always_contains_one_element_additional_fields_token_stream
+                            regular_expression_case: #path_default_but_option_is_always_some_and_vec_always_contains_one_element_token_stream,
+                            value: #path_default_but_option_is_always_some_and_vec_always_contains_one_element_token_stream,
+                        }
+                    },
+                    {
+                        let increments_initialization_token_stream = generate_increments_initialization_token_stream(range.clone());
+                        let format_handle_token_stream = generate_quotes::double_quotes_token_stream(&format!(
+                            "{{}}(exists(select 1 from jsonb_array_elements({{}}{}) as el where substring(el::text from 2 for length(el::text) - 2) {{}} ${{}}))",
+                            generate_indexes_stringified(two_range)
+                        ));
+                        let format_increments_token_stream = generate_format_increments_token_stream(range.clone());
+                        quote::quote! {
+                            #increments_initialization_token_stream
+                            let last_increment = match increment.checked_add(1) {
+                                Some(value) => {
+                                    *increment = value;
+                                    value
+                                },
+                                None => {
+                                    return Err(#crate_query_part_error_named_checked_add_initialization_token_stream);
+                                },
+                            };
+                            Ok(format!(
+                                #format_handle_token_stream,
+                                &self.logical_operator.to_query_part(is_need_to_add_logical_operator),
+                                column,
+                                #format_increments_token_stream,
+                                self.regular_expression_case.postgreql_syntax(),
+                                last_increment
+                            ))
+                        }
+                    },
+                    {
+                        let query_bind_dimension_position_token_stream = generate_query_bind_dimension_position_token_stream(range.clone());
+                        quote::quote! {
+                            #query_bind_dimension_position_token_stream
+                            query = query.bind(self.value.to_string());
+                            query
+                        }
+                    }
                 )
             };
             let (
