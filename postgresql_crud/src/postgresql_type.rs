d07f0a44adb5ffe3461abe734a0f64b5c001a5a9
kuqmua <serezhashmakov@outlook.com>
2025-06-20 14:49:59 +0300

    next steps

--- postgresql_crud/src/postgresql_type.rs
+++ postgresql_crud/src/postgresql_type.rs
@@ -90,6 +90,15 @@ generate_postgresql_types::generate_postgresql_types!([
                 "dimension1_not_null_or_nullable": "NotNull"
             }
         }
+    },
+    {
+        "postgresql_type": "SqlxTypesChronoNaiveTimeAsTime",
+        "not_null_or_nullable": "NotNull",
+        "postgresql_type_pattern": {
+            "ArrayDimension1": {
+                "dimension1_not_null_or_nullable": "NotNull"
+            }
+        }
     }
 ]);
 
