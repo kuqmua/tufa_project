20476ba34e8a0ef67813a8a2dd35c86d329db3c7
kuqmua <serezhashmakov@outlook.com>
2025-06-20 16:57:01 +0300

    next steps

--- postgresql_crud/src/postgresql_type.rs
+++ postgresql_crud/src/postgresql_type.rs
@@ -108,6 +108,15 @@ generate_postgresql_types::generate_postgresql_types!([
                 "dimension1_not_null_or_nullable": "NotNull"
             }
         }
+    },
+    {
+        "postgresql_type": "SqlxPostgresTypesPgRangeStdPrimitiveI32AsInt4Range",
+        "not_null_or_nullable": "NotNull",
+        "postgresql_type_pattern": {
+            "ArrayDimension1": {
+                "dimension1_not_null_or_nullable": "NotNull"
+            }
+        }
     }
 ]);
 
