27ebe2fc2e4ce7b6c9a4b885a9ec0031f190f917
kuqmua <serezhashmakov@outlook.com>
2025-06-26 10:53:54 +0300

    next steps

--- postgresql_crud/generate_where_element_filters/src/lib.rs
+++ postgresql_crud/generate_where_element_filters/src/lib.rs
@@ -488,42 +488,76 @@ pub fn generate_where_element_filters(_input_token_stream: proc_macro::TokenStre
                 fn generate_current_time_format_handle_token_stream(value: &PostgresqlTypeKind) -> proc_macro2::TokenStream {
                     generate_quotes::double_quotes_token_stream(&format!("{{}}({{}}{} = current_time)", value.format_argument()))
                 }
-                // fn generate__format_handle_token_stream(value: &PostgresqlTypeKind) -> proc_macro2::TokenStream {
-                //     generate_quotes::double_quotes_token_stream(&format!(, value.format_argument()))
-                // }
-                // fn generate__format_handle_token_stream(value: &PostgresqlTypeKind) -> proc_macro2::TokenStream {
-                //     generate_quotes::double_quotes_token_stream(&format!(, value.format_argument()))
-                // }
-                // fn generate__format_handle_token_stream(value: &PostgresqlTypeKind) -> proc_macro2::TokenStream {
-                //     generate_quotes::double_quotes_token_stream(&format!(, value.format_argument()))
-                // }
-                // fn generate__format_handle_token_stream(value: &PostgresqlTypeKind) -> proc_macro2::TokenStream {
-                //     generate_quotes::double_quotes_token_stream(&format!(, value.format_argument()))
-                // }
-                // fn generate__format_handle_token_stream(value: &PostgresqlTypeKind) -> proc_macro2::TokenStream {
-                //     generate_quotes::double_quotes_token_stream(&format!(, value.format_argument()))
-                // }
-                // fn generate__format_handle_token_stream(value: &PostgresqlTypeKind) -> proc_macro2::TokenStream {
-                //     generate_quotes::double_quotes_token_stream(&format!(, value.format_argument()))
-                // }
-                // fn generate__format_handle_token_stream(value: &PostgresqlTypeKind) -> proc_macro2::TokenStream {
-                //     generate_quotes::double_quotes_token_stream(&format!(, value.format_argument()))
-                // }
-                // fn generate__format_handle_token_stream(value: &PostgresqlTypeKind) -> proc_macro2::TokenStream {
-                //     generate_quotes::double_quotes_token_stream(&format!(, value.format_argument()))
-                // }
-                match &filter {
-                    postgresql_crud_macros_common::PostgresqlTypeFilter::Equal { ident: _ } => (
+                let generate_equal_token_stream = |postgresql_type_pattern_handle: &PostgresqlTypePatternHandle| -> (
+                    ShouldAddDeclarationOfStructIdentGeneric,
+                    proc_macro2::TokenStream,
+                    proc_macro2::TokenStream,
+                    proc_macro2::TokenStream,
+                    proc_macro2::TokenStream,
+                ) {
+                    let (
+                        maybe_dimensions_declaration_token_stream,
+                        maybe_dimensions_default_initialization_token_stream,
+                        maybe_dimensions_indexes_initialization_token_stream,
+                        postgresql_type_kind,
+                        maybe_additional_parameters_token_stream,
+                        maybe_dimensions_query_bind_content_token_stream
+                    ) = if let Ok(dimension_number) = DimensionNumber::try_from(postgresql_type_pattern_handle) {
+                        (
+                            {
+                                let pub_dimensions_bounded_vec_not_zero_unsigned_part_of_std_primitive_i32_token_stream = generate_pub_dimensions_bounded_vec_not_zero_unsigned_part_of_std_primitive_i32_token_stream(&dimension_number.dimension_token_stream());
+                                quote::quote! {#pub_dimensions_bounded_vec_not_zero_unsigned_part_of_std_primitive_i32_token_stream,}
+                            },
+                            quote::quote!{#dimensions_default_initialization_token_stream,},
+                            dimensions_indexes_postgresql_type_query_part_token_stream.clone(),
+                            PostgresqlTypeKind::ArrayDimension,
+                            quote::quote!{dimensions_indexes,},
+                            query_self_dimensions_query_bind_query_token_stream.clone()
+                        )
+                    }
+                    else {
+                        (
+                            proc_macro2::TokenStream::new(),
+                            proc_macro2::TokenStream::new(),
+                            proc_macro2::TokenStream::new(),
+                            PostgresqlTypeKind::Standart,
+                            proc_macro2::TokenStream::new(),
+                            proc_macro2::TokenStream::new()
+                        )
+                    };
+                    (
                         should_add_declaration_of_struct_ident_generic_true_type_encode.clone(),
-                        pub_value_t_token_stream.clone(),
-                        value_default_but_option_is_always_some_and_vec_always_contains_one_element_token_stream.clone(),
-                        generate_query_part_one_value_token_stream(&generate_format_handle_8bbcc2f2_f3a1_4aed_9c46_2992ea2e9e9b_token_stream(&equal_sign)),
-                        query_bind_one_value_token_stream.clone(),
-                    ),
-                    postgresql_crud_macros_common::PostgresqlTypeFilter::DimensionOneEqual { ident: _ } => generate_dimension_6bad7b4b_e612_42bd_8464_915d8e717255_token_stream(
-                        &dimension_number_one,
-                        &equal_sign
-                    ),
+                        quote::quote! {
+                            #maybe_dimensions_declaration_token_stream
+                            #pub_value_t_token_stream
+                        },
+                        quote::quote!{
+                            #maybe_dimensions_default_initialization_token_stream
+                            #value_default_but_option_is_always_some_and_vec_always_contains_one_element_token_stream
+                        },
+                        {
+                            let format_handle_token_stream = generate_quotes::double_quotes_token_stream(&format!("{{}}({{}}{} = ${{}})", postgresql_type_kind.format_argument()));
+                            quote::quote! {
+                                #maybe_dimensions_indexes_initialization_token_stream
+                                #value_match_increment_checked_add_one_initialization_token_stream
+                                Ok(format!(
+                                    #format_handle_token_stream,
+                                    &self.logical_operator.to_query_part(is_need_to_add_logical_operator),
+                                    column,
+                                    #maybe_additional_parameters_token_stream
+                                    value
+                                ))
+                            }
+                        },
+                        quote::quote!{
+                            #maybe_dimensions_query_bind_content_token_stream
+                            #query_bind_one_value_token_stream
+                        }
+                    )
+                };
+                match &filter {
+                    postgresql_crud_macros_common::PostgresqlTypeFilter::Equal { ident: _ } => generate_equal_token_stream(&PostgresqlTypePatternHandle::Standart),
+                    postgresql_crud_macros_common::PostgresqlTypeFilter::DimensionOneEqual { ident: _ } => generate_equal_token_stream(&PostgresqlTypePatternHandle::ArrayDimension1),
                     postgresql_crud_macros_common::PostgresqlTypeFilter::GreaterThan { ident: _ } => (
                         should_add_declaration_of_struct_ident_generic_true_type_encode.clone(),
                         pub_value_t_token_stream.clone(),
