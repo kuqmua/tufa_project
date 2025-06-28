50545b94bb65ec1cedbf17be1133cbb18f27a69f
kuqmua <serezhashmakov@outlook.com>
2025-06-28 22:21:04 +0300

    next steps

--- postgresql_crud/generate_postgresql_json_types/src/lib.rs
+++ postgresql_crud/generate_postgresql_json_types/src/lib.rs
@@ -1398,7 +1398,7 @@ pub fn generate_postgresql_json_types(input_token_stream: proc_macro::TokenStrea
                                     PostgresqlJsonTypeSpecific::String => {
                                         let mut filters = common_array_dimension1_postgresql_json_type_filters.clone();
                                         filters.push(postgresql_crud_macros_common::PostgresqlJsonTypeFilter::DimensionOneRegularExpression);
-                                        filters.push(postgresql_crud_macros_common::PostgresqlJsonTypeFilter::DimensionOneContainsElementRegularExpression);
+                                        filters.push(postgresql_crud_macros_common::PostgresqlJsonTypeFilter::ContainsElementRegularExpression);
                                         filters.push(postgresql_crud_macros_common::PostgresqlJsonTypeFilter::DimensionOneAllElementsRegularExpression);
                                         filters
                                     }
