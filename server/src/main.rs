d7b9f2df038712fed44f539c3bdf7bd18c4396d4
kuqmua <serezhashmakov@outlook.com>
2025-07-11 23:11:55 +0300

    next steps

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,11 +1,11 @@
 pub mod global_variables;
+pub mod routes;
 // #[cfg(test)]
 // mod tests;
 //query! containing mods
 // pub mod authentication;
 // pub mod idempotency;
 // pub mod issue_delivery_worker;
-pub mod routes;
 
 fn main() {
     std::thread::Builder::new().stack_size(16 * 1024 * 1024) // 16 MB
@@ -42,7 +42,7 @@ fn main() {
                 //         ))
                 //     },
                 // };
-                println!("server running on {}", app_state::GetServiceSocketAddress::get_service_socket_address(&config));
+                println!("trying to up server on {}", app_state::GetServiceSocketAddress::get_service_socket_address(&config));
                 let app_state = std::sync::Arc::new(common::repositories_types::server::routes::app_state::AppState {
                     postgres_pool,
                     config,
@@ -66,8 +66,89 @@ fn main() {
                             .allow_origin(["http://127.0.0.1".parse().unwrap()]),
                         )
                         .merge(utoipa_swagger_ui::SwaggerUi::new(constants::SLASH_SWAGGER_UI).url("/api-docs/openapi.json", {
-                            use utoipa::OpenApi;
-                            ApiDoc::openapi()
+                            #[derive(utoipa::OpenApi)]
+                            #[openapi(
+                                paths(
+                                    common::server::routes::git_info::git_info,
+                                    // common::repositories_types::server::routes::api::cats::create_many,
+                                    // common::repositories_types::server::routes::api::cats::create_one,
+                                    // common::repositories_types::server::routes::api::cats::read_many,
+                                    // common::repositories_types::server::routes::api::cats::read_one,
+                                    // common::repositories_types::server::routes::api::cats::update_many,
+                                    // common::repositories_types::server::routes::api::cats::update_one,
+                                    // common::repositories_types::server::routes::api::cats::delete_many,
+                                    // common::repositories_types::server::routes::api::cats::delete_one
+                                ),
+                                components(
+                                    schemas(
+                                        common::server::routes::git_info::GitInfo,                          
+
+                                        common::common::utoipa::std::time::StdTimeDuration,
+                                        error_occurence_lib::code_occurence::CodeOccurence,
+                                        //
+                                        // postgresql_crud::TimeMonth,
+                                        // postgresql_crud::SqlxTypesTimeUtcOffset,
+                                        // postgresql_crud::NumBigintSign,
+                                        // postgresql_crud::NumBigintBigInt,
+                                        // postgresql_crud::StdPrimitiveBool,
+                                        // postgresql_crud::StdPrimitiveI16,
+                                        // postgresql_crud::StdPrimitiveI32,
+                                        // postgresql_crud::StdPrimitiveI64,
+                                        // postgresql_crud::StdPrimitiveF32,
+                                        // postgresql_crud::StdPrimitiveF64,
+                                        // postgresql_crud::StdStringString,
+                                        // postgresql_crud::StdVecVecStdPrimitiveU8,
+                                        // postgresql_crud::SqlxPostgresTypesPgInterval,
+                                        // postgresql_crud::SqlxPostgresTypesPgRangeStdPrimitiveI64,
+                                        // postgresql_crud::SqlxPostgresTypesPgRangeStdPrimitiveI32,
+                                        // postgresql_crud::SqlxPostgresTypesPgRangeSqlxTypesChronoDateTimeSqlxTypesChronoUtc,
+                                        // postgresql_crud::SqlxPostgresTypesPgRangeSqlxTypesTimePrimitiveDateTime,
+                                        // //todo check all types and type decl order
+                                        // postgresql_crud::SqlxPostgresTypesPgRangeSqlxTypesChronoDateTimeSqlxTypesChronoLocal,
+                                        // postgresql_crud::SqlxPostgresTypesPgRangeSqlxTypesTimeOffsetDateTime,
+                                        // postgresql_crud::SqlxPostgresTypesPgRangeSqlxTypesChronoNaiveDate,
+                                        // postgresql_crud::SqlxPostgresTypesPgRangeSqlxTypesTimeDate,
+                                        // postgresql_crud::SqlxPostgresTypesPgRangeSqlxTypesBigDecimal,
+                                        // postgresql_crud::SqlxPostgresTypesPgRangeSqlxTypesDecimal,
+                                        // postgresql_crud::SqlxPostgresTypesPgMoney,
+                                        // postgresql_crud::SqlxPostgresTypesPgCiText,
+                                        // postgresql_crud::SqlxTypesBigDecimal,
+                                        // postgresql_crud::SqlxTypesDecimal,
+                                        // //todo
+                                        // postgresql_crud::SqlxTypesChronoDateTimeSqlxTypesChronoLocal,
+                                        // postgresql_crud::SqlxTypesChronoDateTimeSqlxTypesChronoUtc,
+                                        // postgresql_crud::SqlxTypesChronoNaiveDateTime,
+                                        // postgresql_crud::SqlxTypesChronoNaiveDate,
+                                        // postgresql_crud::SqlxTypesChronoNaiveTime,
+                                        // postgresql_crud::SqlxPostgresTypesPgTimeTz,
+                                        // postgresql_crud::SqlxTypesTimePrimitiveDateTime,
+                                        // postgresql_crud::SqlxTypesTimeOffsetDateTime,
+                                        // postgresql_crud::SqlxTypesTimeDate,
+                                        // postgresql_crud::SqlxTypesTimeTime,
+                                        // postgresql_crud::SqlxTypesUuidUuid,
+                                        // postgresql_crud::SqlxTypesIpnetworkIpNetwork,
+                                        // postgresql_crud::StdNetIpAddr,
+                                        // postgresql_crud::SqlxTypesMacAddressMacAddress,
+                                        // postgresql_crud::SqlxTypesBitVec,
+                                        // // postgresql_crud::SqlxTypesJson,//todo what to do with generics?
+                                        // postgresql_crud::SerdeJsonValue,
+                                    )
+                                ),
+                                modifiers(&SecurityAddon),
+                                tags(
+                                    (name = "server", description = "server api")
+                                )
+                            )] //todo - this thing actually using builder pattern. maybe generate builder in GeneratePostgresqlCrud then merge it together?
+                            struct ApiDoc;
+                            struct SecurityAddon;
+                            impl utoipa::Modify for SecurityAddon {
+                                fn modify(&self, openapi: &mut utoipa::openapi::OpenApi) {
+                                    if let Some(components) = openapi.components.as_mut() {
+                                        components.add_security_scheme("api_key", utoipa::openapi::security::SecurityScheme::ApiKey(utoipa::openapi::security::ApiKey::Header(utoipa::openapi::security::ApiKeyValue::new("todo_apikey"))));
+                                    }
+                                }
+                            }
+                            <ApiDoc as utoipa::OpenApi>::openapi()
                         }))
                         .into_make_service(),
                 )
@@ -79,90 +160,4 @@ fn main() {
     .unwrap()
     .join()
     .unwrap();
-}
-
-
-
-
-#[derive(utoipa::OpenApi)]
-#[openapi(
-    paths(
-        common::server::routes::git_info::git_info,
-        // common::repositories_types::server::routes::api::cats::create_many,
-        // common::repositories_types::server::routes::api::cats::create_one,
-        // common::repositories_types::server::routes::api::cats::read_many,
-        // common::repositories_types::server::routes::api::cats::read_one,
-        // common::repositories_types::server::routes::api::cats::update_many,
-        // common::repositories_types::server::routes::api::cats::update_one,
-        // common::repositories_types::server::routes::api::cats::delete_many,
-        // common::repositories_types::server::routes::api::cats::delete_one
-    ),
-    components(
-        schemas(
-            common::server::routes::git_info::GitInfo,
-
-            common::common::utoipa::std::time::StdTimeDuration,
-            error_occurence_lib::code_occurence::CodeOccurence,
-            //
-            // postgresql_crud::TimeMonth,
-            // postgresql_crud::SqlxTypesTimeUtcOffset,
-            // postgresql_crud::NumBigintSign,
-            // postgresql_crud::NumBigintBigInt,
-            // postgresql_crud::StdPrimitiveBool,
-            // postgresql_crud::StdPrimitiveI16,
-            // postgresql_crud::StdPrimitiveI32,
-            // postgresql_crud::StdPrimitiveI64,
-            // postgresql_crud::StdPrimitiveF32,
-            // postgresql_crud::StdPrimitiveF64,
-            // postgresql_crud::StdStringString,
-            // postgresql_crud::StdVecVecStdPrimitiveU8,
-            // postgresql_crud::SqlxPostgresTypesPgInterval,
-            // postgresql_crud::SqlxPostgresTypesPgRangeStdPrimitiveI64,
-            // postgresql_crud::SqlxPostgresTypesPgRangeStdPrimitiveI32,
-            // postgresql_crud::SqlxPostgresTypesPgRangeSqlxTypesChronoDateTimeSqlxTypesChronoUtc,
-            // postgresql_crud::SqlxPostgresTypesPgRangeSqlxTypesTimePrimitiveDateTime,
-            // //todo check all types and type decl order
-            // postgresql_crud::SqlxPostgresTypesPgRangeSqlxTypesChronoDateTimeSqlxTypesChronoLocal,
-            // postgresql_crud::SqlxPostgresTypesPgRangeSqlxTypesTimeOffsetDateTime,
-            // postgresql_crud::SqlxPostgresTypesPgRangeSqlxTypesChronoNaiveDate,
-            // postgresql_crud::SqlxPostgresTypesPgRangeSqlxTypesTimeDate,
-            // postgresql_crud::SqlxPostgresTypesPgRangeSqlxTypesBigDecimal,
-            // postgresql_crud::SqlxPostgresTypesPgRangeSqlxTypesDecimal,
-            // postgresql_crud::SqlxPostgresTypesPgMoney,
-            // postgresql_crud::SqlxPostgresTypesPgCiText,
-            // postgresql_crud::SqlxTypesBigDecimal,
-            // postgresql_crud::SqlxTypesDecimal,
-            // //todo
-            // postgresql_crud::SqlxTypesChronoDateTimeSqlxTypesChronoLocal,
-            // postgresql_crud::SqlxTypesChronoDateTimeSqlxTypesChronoUtc,
-            // postgresql_crud::SqlxTypesChronoNaiveDateTime,
-            // postgresql_crud::SqlxTypesChronoNaiveDate,
-            // postgresql_crud::SqlxTypesChronoNaiveTime,
-            // postgresql_crud::SqlxPostgresTypesPgTimeTz,
-            // postgresql_crud::SqlxTypesTimePrimitiveDateTime,
-            // postgresql_crud::SqlxTypesTimeOffsetDateTime,
-            // postgresql_crud::SqlxTypesTimeDate,
-            // postgresql_crud::SqlxTypesTimeTime,
-            // postgresql_crud::SqlxTypesUuidUuid,
-            // postgresql_crud::SqlxTypesIpnetworkIpNetwork,
-            // postgresql_crud::StdNetIpAddr,
-            // postgresql_crud::SqlxTypesMacAddressMacAddress,
-            // postgresql_crud::SqlxTypesBitVec,
-            // // postgresql_crud::SqlxTypesJson,//todo what to do with generics?
-            // postgresql_crud::SerdeJsonValue,
-        )
-    ),
-    modifiers(&SecurityAddon),
-    tags(
-        (name = "server", description = "server api")
-    )
-)] //todo - this thing actually using builder pattern. maybe generate builder in GeneratePostgresqlCrud then merge it together?
-struct ApiDoc;
-struct SecurityAddon;
-impl utoipa::Modify for SecurityAddon {
-    fn modify(&self, openapi: &mut utoipa::openapi::OpenApi) {
-        if let Some(components) = openapi.components.as_mut() {
-            components.add_security_scheme("api_key", utoipa::openapi::security::SecurityScheme::ApiKey(utoipa::openapi::security::ApiKey::Header(utoipa::openapi::security::ApiKeyValue::new("todo_apikey"))));
-        }
-    }
 }
