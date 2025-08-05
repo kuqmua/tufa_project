33b7f8e59c4b90b3194ebdec1cf5a2cbf5398e5f
kuqmua <serezhashmakov@outlook.com>
2025-08-05 14:53:27 +0300

    todo

--- postgresql_crud/src/lib.rs
+++ postgresql_crud/src/lib.rs
@@ -60,6 +60,7 @@ pub trait PostgresqlType {
         // + error_occurence_lib::ToStdStringString
         // + crate::DefaultButOptionIsAlwaysSomeAndVecAlwaysContainsOneElement
         ;
+    //todo add normalize method to Read
     type ReadInner;
     fn into_inner(value: Self::Read) -> Self::ReadInner;
     type Update: std::fmt::Debug + Clone + PartialEq + serde::Serialize + for<'__> serde::Deserialize<'__> + crate::DefaultButOptionIsAlwaysSomeAndVecAlwaysContainsOneElement;
