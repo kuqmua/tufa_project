77ae0ccb53c44af527f82f274751383c23eef91a
kuqmua <serezhashmakov@outlook.com>
2025-08-09 17:25:25 +0300

    next steps

--- common/src/repositories_types/server/routes/api/example/mod.rs
+++ common/src/repositories_types/server/routes/api/example/mod.rs
@@ -1451,28 +1451,32 @@ impl postgresql_crud::tests::PostgresqlTypeTestCases<crate::repositories_types::
         value: crate::repositories_types::server::routes::api::example::AnimalAsNotNullJsonbObjectReadInner
     ) -> <Self::Element as postgresql_crud::PostgresqlType>::Update {
         <Self::Element as postgresql_crud::PostgresqlType>::Update::new(
-            postgresql_crud::NotEmptyUniqueEnumVec::try_new(
-                vec![
-                    AnimalAsNotNullJsonbObjectUpdateElement::Field0(postgresql_crud::Value {
+            postgresql_crud::NotEmptyUniqueEnumVec::try_new({
+                let mut acc = vec![];
+                if let Some(value) = value.field_0 {
+                    acc.push(AnimalAsNotNullJsonbObjectUpdateElement::Field0(postgresql_crud::Value {
                         value: <
                             postgresql_crud::postgresql_json_type::StdPrimitiveI8AsNotNullJsonbNumber
                             as
                             postgresql_crud::tests::PostgresqlJsonTypeTestCases<
                                 <postgresql_crud::postgresql_json_type::StdPrimitiveI8AsNotNullJsonbNumber as postgresql_crud::PostgresqlJsonType>::ReadInner
                             >
-                        >::update_new_or_try_new_unwraped_for_test(value.field_0.unwrap().value),
-                    }),
-                    AnimalAsNotNullJsonbObjectUpdateElement::Field1(postgresql_crud::Value {
+                        >::update_new_or_try_new_unwraped_for_test(value.value),
+                    }));
+                }
+                if let Some(value) = value.field_1 {
+                    acc.push(AnimalAsNotNullJsonbObjectUpdateElement::Field1(postgresql_crud::Value {
                         value: <
                             postgresql_crud::postgresql_json_type::OptionStdPrimitiveI8AsNullableJsonbNumber
                             as
                             postgresql_crud::tests::PostgresqlJsonTypeTestCases<
                                 <postgresql_crud::postgresql_json_type::OptionStdPrimitiveI8AsNullableJsonbNumber as postgresql_crud::PostgresqlJsonType>::ReadInner
                             >
-                        >::update_new_or_try_new_unwraped_for_test(value.field_1.unwrap().value),
-                    })
-                ]
-            ).unwrap()
+                        >::update_new_or_try_new_unwraped_for_test(value.value),
+                    }));
+                }
+                acc
+            }).unwrap()
         )
     }
 }
