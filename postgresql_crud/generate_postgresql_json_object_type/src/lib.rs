412169fc9152bfb68c534a93ee58fa5f2c220003
kuqmua <serezhashmakov@outlook.com>
2025-08-28 11:36:58 +0300

    next steps

--- postgresql_crud/generate_postgresql_json_object_type/src/lib.rs
+++ postgresql_crud/generate_postgresql_json_object_type/src/lib.rs
@@ -3180,7 +3180,7 @@ pub fn generate_postgresql_json_object_type(input_token_stream: proc_macro::Toke
                                         let field_type = &element.ty;
                                         let field_ident_upper_camel_case_token_stream = naming::ToTokensToUpperCamelCaseTokenStream::case_or_panic(&field_ident);
                                         let field_ident_double_quotes_token_stream = generate_quotes::double_quotes_token_stream(&field_ident);
-                                        let format_handle_token_stream = generate_quotes::double_quotes_token_stream(&format!("{{{column_name_and_maybe_field_getter_snake_case}}}->'{field_ident}'"));
+                                        let format_handle_token_stream = generate_quotes::double_quotes_token_stream(&format!("{{{column_name_and_maybe_field_getter_snake_case}}}->'{{field_ident}}'"));
                                         quote::quote! {
                                             #ident_update_element_standart_not_null_upper_camel_case::#field_ident_upper_camel_case_token_stream(value) => match <
                                                 #field_type
