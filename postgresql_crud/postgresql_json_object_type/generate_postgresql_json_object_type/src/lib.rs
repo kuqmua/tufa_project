20db8abf0b491d3523d8a1cc79a7a138071d750d
kuqmua <serezhashmakov@outlook.com>
2025-09-02 17:10:07 +0300

    next steps

--- postgresql_crud/postgresql_json_object_type/generate_postgresql_json_object_type/src/lib.rs
+++ postgresql_crud/postgresql_json_object_type/generate_postgresql_json_object_type/src/lib.rs
@@ -3120,8 +3120,9 @@ pub fn generate_postgresql_json_object_type(input_token_stream: proc_macro::Toke
                                                     &format!(#format_handle_token_stream),
                                                     #increment_snake_case
                                                 ) {
-                                                    Ok(#value_snake_case) => {
-                                                        #acc_snake_case.push_str(&#value_snake_case);
+                                                    Ok(mut #value_snake_case) => {
+                                                        let _ = #value_snake_case.pop();
+                                                        #acc_snake_case.push_str(&format!("jsonb_build_object({})||", #value_snake_case));
                                                     },
                                                     Err(#error_snake_case) => {
                                                         return Err(#error_snake_case);
@@ -3138,7 +3139,8 @@ pub fn generate_postgresql_json_object_type(input_token_stream: proc_macro::Toke
                                             }
                                         }
                                         let _ = #acc_snake_case.pop();
-                                        Ok(format!("'{field_ident}',jsonb_build_object('value',jsonb_build_object({acc})),"))
+                                        let _ = #acc_snake_case.pop();
+                                        Ok(format!("'{field_ident}',jsonb_build_object('value',{}),", #acc_snake_case))
                                     }
                                 }
                                 postgresql_crud_macros_common::NotNullOrNullable::Nullable => {
