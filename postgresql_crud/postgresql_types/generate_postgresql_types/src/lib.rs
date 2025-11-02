28387e510d1bd075de7b84115ec1efe02a7f0026
kuqmua <serezhashmakov@outlook.com>
2025-11-02 19:14:11 +0300

    next steps

--- postgresql_crud/postgresql_types/generate_postgresql_types/src/lib.rs
+++ postgresql_crud/postgresql_types/generate_postgresql_types/src/lib.rs
@@ -5192,80 +5192,23 @@ pub fn generate_postgresql_types(input_token_stream: proc_macro::TokenStream) ->
                     }
                 };
                 //todo maybe it into function (not in proc macro)
-                let read_only_ids_merged_with_create_into_where_element_equal_common_token_stream = match &postgresql_type_pattern {
-                    PostgresqlTypePattern::Standart => match &not_null_or_nullable {
-                        postgresql_crud_macros_common::NotNullOrNullable::NotNull => {
-                            let content_token_stream = if let IsNotNullStandartCanBePrimaryKey::True = &is_not_null_standart_can_be_primary_key {
-                                quote::quote! {#read_only_ids_snake_case.0.0}
-                            }
-                            else {
-                                quote::quote! {#create_snake_case.0}
-                            };
-                            quote::quote! {
-                                vec![
-                                    #ident_where_element_upper_camel_case::#equal_upper_camel_case(where_element_filters::PostgresqlTypeWhereElementEqual {
-                                        logical_operator: postgresql_crud_common::LogicalOperator::Or,
-                                        #value_snake_case: #ident_table_type_declaration_upper_camel_case(#content_token_stream),
-                                    })
-                                ]
-                            }
-                        }
-                        postgresql_crud_macros_common::NotNullOrNullable::Nullable => quote::quote! {
-                            vec![
-                                #ident_where_element_upper_camel_case::#equal_upper_camel_case(
-                                    where_element_filters::PostgresqlTypeWhereElementEqual {
-                                        logical_operator: postgresql_crud_common::LogicalOperator::Or,
-                                        #value_snake_case: #ident_table_type_declaration_upper_camel_case(#create_snake_case.0),
-                                    }
-                                )
-                            ]
-                        }
-                    },
-                    PostgresqlTypePattern::ArrayDimension1 { dimension1_not_null_or_nullable } => match &not_null_or_nullable {
-                        postgresql_crud_macros_common::NotNullOrNullable::NotNull => match &dimension1_not_null_or_nullable {
-                            postgresql_crud_macros_common::NotNullOrNullable::NotNull => quote::quote! {
-                                vec![
-                                    #ident_where_element_upper_camel_case::Equal(
-                                        where_element_filters::PostgresqlTypeWhereElementEqual {
-                                            logical_operator: postgresql_crud_common::LogicalOperator::Or,
-                                            #value_snake_case: #ident_table_type_declaration_upper_camel_case(#create_snake_case.0),
-                                        }
-                                    )
-                                ]
-                            },
-                            postgresql_crud_macros_common::NotNullOrNullable::Nullable => quote::quote! {
-                                vec![
-                                    #ident_where_element_upper_camel_case::Equal(
-                                        where_element_filters::PostgresqlTypeWhereElementEqual {
-                                            logical_operator: postgresql_crud_common::LogicalOperator::Or,
-                                            #value_snake_case: #ident_table_type_declaration_upper_camel_case(#create_snake_case.0),
-                                        }
-                                    )
-                                ]
-                            },
-                        },
-                        postgresql_crud_macros_common::NotNullOrNullable::Nullable => match &dimension1_not_null_or_nullable {
-                            postgresql_crud_macros_common::NotNullOrNullable::NotNull => quote::quote! {
-                                vec![
-                                    #ident_where_element_upper_camel_case::Equal(
-                                        where_element_filters::PostgresqlTypeWhereElementEqual {
-                                            logical_operator: postgresql_crud_common::LogicalOperator::Or,
-                                            #value_snake_case: #ident_table_type_declaration_upper_camel_case(#create_snake_case.0),
-                                        }
-                                    )
-                                ]
-                            },
-                            postgresql_crud_macros_common::NotNullOrNullable::Nullable => quote::quote! {
-                                vec![
-                                    #ident_where_element_upper_camel_case::Equal(
-                                        where_element_filters::PostgresqlTypeWhereElementEqual {
-                                            logical_operator: postgresql_crud_common::LogicalOperator::Or,
-                                            #value_snake_case: #ident_table_type_declaration_upper_camel_case(#create_snake_case.0),
-                                        }
-                                    )
-                                ]
-                            }
-                        }
+                let read_only_ids_merged_with_create_into_where_element_equal_common_token_stream = {
+                    let content_token_stream = if let PostgresqlTypePattern::Standart = &postgresql_type_pattern &&
+                        let postgresql_crud_macros_common::NotNullOrNullable::NotNull = &not_null_or_nullable &&
+                        let IsNotNullStandartCanBePrimaryKey::True = &is_not_null_standart_can_be_primary_key
+                    {
+                        quote::quote! {#read_only_ids_snake_case.0.0}
+                    }
+                    else {
+                        quote::quote! {#create_snake_case.0}
+                    };
+                    quote::quote! {
+                        vec![
+                            #ident_where_element_upper_camel_case::#equal_upper_camel_case(where_element_filters::PostgresqlTypeWhereElementEqual {
+                                logical_operator: postgresql_crud_common::LogicalOperator::Or,
+                                #value_snake_case: #ident_table_type_declaration_upper_camel_case(#content_token_stream),
+                            })
+                        ]
                     }
                 };
                 postgresql_crud_macros_common::generate_impl_postgresql_type_test_cases_for_ident_token_stream(
