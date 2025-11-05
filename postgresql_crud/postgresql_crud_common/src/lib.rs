cc599395a6a35e3cfc420cc6ca0dd46f8969f3b0
kuqmua <serezhashmakov@outlook.com>
2025-11-05 20:27:05 +0300

    next steps

--- postgresql_crud/postgresql_crud_common/src/lib.rs
+++ postgresql_crud/postgresql_crud_common/src/lib.rs
@@ -185,7 +185,6 @@ pub trait PostgresqlTypeTestCases {
         read_only_ids: <Self::PostgresqlType as crate::PostgresqlType>::ReadOnlyIds,
         create: <Self::PostgresqlType as crate::PostgresqlType>::Create
     ) -> std::vec::Vec<<Self::PostgresqlType as crate::PostgresqlType>::WhereElement>;
-    //todo must different name for PostgresqlTypeTestCases and PostgresqlJsonTypeTestCases
     fn read_only_ids_merged_with_create_into_option_vec_where_element_equal_to_json_field(
         read_only_ids: <Self::PostgresqlType as crate::PostgresqlType>::ReadOnlyIds,
         create: <Self::PostgresqlType as crate::PostgresqlType>::Create
