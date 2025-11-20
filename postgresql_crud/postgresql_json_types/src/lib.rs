9c77c72f6c6be953058bac985fad2bcb248760dd
kuqmua <serezhashmakov@outlook.com>
2025-11-20 15:23:26 +0300

    next steps

--- postgresql_crud/postgresql_json_types/src/lib.rs
+++ postgresql_crud/postgresql_json_types/src/lib.rs
@@ -8433,40 +8433,43 @@ generate_postgresql_json_types::generate_postgresql_json_types!({
                 }
             }
         }
+        ,
+        {
+            "postgresql_json_type": "UuidUuidAsJsonbString",
+            "not_null_or_nullable": "NotNull",
+            "postgresql_json_type_pattern": {
+                "ArrayDimension3": {
+                    "dimension1_not_null_or_nullable": "Nullable",
+                    "dimension2_not_null_or_nullable": "NotNull",
+                    "dimension3_not_null_or_nullable": "Nullable"
+                }
+            }
+        }
+        ,
+        {
+            "postgresql_json_type": "UuidUuidAsJsonbString",
+            "not_null_or_nullable": "NotNull",
+            "postgresql_json_type_pattern": {
+                "ArrayDimension3": {
+                    "dimension1_not_null_or_nullable": "Nullable",
+                    "dimension2_not_null_or_nullable": "Nullable",
+                    "dimension3_not_null_or_nullable": "NotNull"
+                }
+            }
+        }
+        ,
+        {
+            "postgresql_json_type": "UuidUuidAsJsonbString",
+            "not_null_or_nullable": "NotNull",
+            "postgresql_json_type_pattern": {
+                "ArrayDimension3": {
+                    "dimension1_not_null_or_nullable": "Nullable",
+                    "dimension2_not_null_or_nullable": "Nullable",
+                    "dimension3_not_null_or_nullable": "Nullable"
+                }
+            }
+        }
         // ,
-        // {
-        //     "postgresql_json_type": "UuidUuidAsJsonbString",
-        //     "not_null_or_nullable": "NotNull",
-        //     "postgresql_json_type_pattern": {
-        //         "ArrayDimension3": {
-        //             "dimension1_not_null_or_nullable": "Nullable",
-        //             "dimension2_not_null_or_nullable": "NotNull",
-        //             "dimension3_not_null_or_nullable": "Nullable"
-        //         }
-        //     }
-        // },
-        // {
-        //     "postgresql_json_type": "UuidUuidAsJsonbString",
-        //     "not_null_or_nullable": "NotNull",
-        //     "postgresql_json_type_pattern": {
-        //         "ArrayDimension3": {
-        //             "dimension1_not_null_or_nullable": "Nullable",
-        //             "dimension2_not_null_or_nullable": "Nullable",
-        //             "dimension3_not_null_or_nullable": "NotNull"
-        //         }
-        //     }
-        // },
-        // {
-        //     "postgresql_json_type": "UuidUuidAsJsonbString",
-        //     "not_null_or_nullable": "NotNull",
-        //     "postgresql_json_type_pattern": {
-        //         "ArrayDimension3": {
-        //             "dimension1_not_null_or_nullable": "Nullable",
-        //             "dimension2_not_null_or_nullable": "Nullable",
-        //             "dimension3_not_null_or_nullable": "Nullable"
-        //         }
-        //     }
-        // },
         // {
         //     "postgresql_json_type": "UuidUuidAsJsonbString",
         //     "not_null_or_nullable": "Nullable",
