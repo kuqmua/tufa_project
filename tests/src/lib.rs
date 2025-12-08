b3bb3d8968dff8666a2b3478f6326d3766fe6635
kuqmua <serezhashmakov@outlook.com>
2025-12-08 13:16:51 +0300

    reuse test code

--- tests/src/lib.rs
+++ tests/src/lib.rs
@@ -26,9 +26,37 @@ mod tests {
         };
         toml_value_table.keys().cloned().collect::<std::vec::Vec<std::string::String>>()
     }
+    fn compare(
+        rust_or_clippy: RustOrClippy,
+        lints_vec_from_cargo_toml: std::vec::Vec<std::string::String>,
+        lints_to_check: std::vec::Vec<std::string::String>,
+        lints_not_in_cargo_toml_vec_exceptions: std::vec::Vec<std::string::String>
+    ) {
+        let rust_or_clippy_name = rust_or_clippy.name();
+        let mut lints_not_in_cargo_toml = vec![];
+        for element in &lints_to_check {
+            if !lints_vec_from_cargo_toml.contains(element) {
+                if lints_not_in_cargo_toml_vec_exceptions.contains(element) {
+                    println!("todo!() {rust_or_clippy_name} {element} 158b5c43-05fa-4b8f-b6fe-9cda49d26997");
+                }
+                else {
+                    lints_not_in_cargo_toml.push(element);
+                }
+            }
+        }
+        assert!(lints_not_in_cargo_toml.is_empty(), "this {rust_or_clippy_name} lints are not in the [workspace.lints.{rust_or_clippy_name}]: {lints_not_in_cargo_toml:#?}");
+        let mut outdated_lints_in_file = vec![];
+        for element in &lints_vec_from_cargo_toml {
+            if !lints_to_check.contains(element) {
+                outdated_lints_in_file.push(element);
+            }
+        }
+        assert!(outdated_lints_in_file.is_empty(), "this {rust_or_clippy_name} lints are outdated but still in [workspace.lints.{rust_or_clippy_name}]: {outdated_lints_in_file:#?}");
+    }
     #[test]
     fn check_if_workspace_cargo_toml_workspace_lints_rust_contains_all_rust_lints() {
-        let lints_vec_from_cargo_toml = lints_vec_from_cargo_toml(RustOrClippy::Rust);
+        let rust_or_clippy = RustOrClippy::Rust;
+        let lints_vec_from_cargo_toml = lints_vec_from_cargo_toml(rust_or_clippy);
         let lints_from_command = {
             let output = std::process::Command::new("rustc")
                 .args(["-W", "help"])
@@ -61,104 +89,84 @@ mod tests {
             })
             .collect::<std::vec::Vec<std::string::String>>()
         };
-        let mut lints_not_in_file = vec![];
-        for element in &lints_from_command {
-            if !lints_vec_from_cargo_toml.contains(element) {
-                //todo seems like this lints still not added to rustc, but in the list of rustc -W help
-                if element == "fuzzy_provenance_casts" ||
-                    element == "lossy_provenance_casts" ||
-                    element == "multiple_supertrait_upcastable" ||
-                    element == "must_not_suspend" ||
-                    element == "non_exhaustive_omitted_patterns" ||
-                    element == "supertrait_item_shadowing_definition" ||
-                    element == "supertrait_item_shadowing_usage" ||
-                    element == "aarch_64_softfloat_neon" ||
-                    element == "default_overrides_default_fields" ||
-                    element == "test_unstable_lint" ||
-                    element == "resolving_to_items_shadowing_supertrait_items" ||
-                    element == "shadowing_supertrait_items"
-                {
-                    println!("todo!() 158b5c43-05fa-4b8f-b6fe-9cda49d26997");
-                }
-                else {
-                    lints_not_in_file.push(element);
-                }
-            }
-        }
-        assert!(lints_not_in_file.is_empty(), "this rust lints are not in the [workspace.lints.rust]: {lints_not_in_file:#?}");
-        let mut outdated_lints_in_file = vec![];
-        for element in &lints_vec_from_cargo_toml {
-            if !lints_from_command.contains(element) {
-                outdated_lints_in_file.push(element);
-            }
-        }
-        assert!(outdated_lints_in_file.is_empty(), "this rust lints are outdated but still in [workspace.lints.rust]: {outdated_lints_in_file:#?}");
+        compare(
+            rust_or_clippy,
+            lints_vec_from_cargo_toml,
+            lints_from_command,
+            //todo on commit momment seems like this lints still not added to rustc, but in the list of rustc -W help
+            vec![
+                std::string::String::from("fuzzy_provenance_casts"),
+                std::string::String::from("lossy_provenance_casts"),
+                std::string::String::from("multiple_supertrait_upcastable"),
+                std::string::String::from("must_not_suspend"),
+                std::string::String::from("non_exhaustive_omitted_patterns"),
+                std::string::String::from("supertrait_item_shadowing_definition"),
+                std::string::String::from("supertrait_item_shadowing_usage"),
+                std::string::String::from("aarch_64_softfloat_neon"),
+                std::string::String::from("default_overrides_default_fields"),
+                std::string::String::from("test_unstable_lint"),
+                std::string::String::from("resolving_to_items_shadowing_supertrait_items"),
+                std::string::String::from("shadowing_supertrait_items")
+            ]
+        )
     }
     #[test]
     fn check_if_workspace_cargo_toml_workspace_lints_clippy_contains_all_clippy_lints() {
-        let lints_vec_from_cargo_toml = lints_vec_from_cargo_toml(RustOrClippy::Clippy);
-        let body = reqwest::blocking::get("https://rust-lang.github.io/rust-clippy/master/index.html").expect("error d1a0544a-566e-4bf4-a37e-7dac73be02fd").text().expect("error 012e3328-53a4-4266-b403-24ac3b8dcbf3");
-        fn parse_article_ids_from_file(html: &std::primitive::str) -> std::vec::Vec<std::string::String> {
-            let document = scraper::Html::parse_document(html);
-            let html_selector = scraper::Selector::parse("html").expect("error 80427609-cfed-4b38-bdea-0794535ef84a");
-            let body_selector = scraper::Selector::parse("body").expect("error 620c597c-0faa-408f-b9bc-29059d179951");
-            let div_container_selector = scraper::Selector::parse(r#"div[class="container"]"#).expect("error eb483b13-e70e-40f4-b83a-3eeb00413d57");
-            let article_selector = scraper::Selector::parse("article").expect("error d21dbe55-6f9f-4695-bf08-78da4f2424ea");
-            let label_selector = scraper::Selector::parse("label").expect("error fe3d9f11-f3b0-4e54-a54a-842fabe3d8a7");
-            let h2_lint_title_selector = scraper::Selector::parse(r#"h2[class="lint-title"]"#).expect("error f1473d4e-e26a-491d-9980-e1874301a6b2");
-            let span_label_label_default_lint_group_group_deprecated_selector = scraper::Selector::parse(r#"span[class="label label-default lint-group group-deprecated"]"#).expect("error e86d5496-f62b-428c-ac6c-d533e0f6f775");
-            let mut ids = std::vec::Vec::new();
-            for html_element in document.select(&html_selector) {
-                for body_element in html_element.select(&body_selector) {
-                    for div_container_element in body_element.select(&div_container_selector) {
-                        for article_element in div_container_element.select(&article_selector) {
-                            let mut is_deprecated = false;
-                            for label_selector_element in article_element.select(&label_selector) {
-                                if is_deprecated {
-                                    break;
-                                }
-                                for h2_lint_title_selector_element in label_selector_element.select(&h2_lint_title_selector) {
+        let rust_or_clippy = RustOrClippy::Clippy;
+        let lints_vec_from_cargo_toml = lints_vec_from_cargo_toml(rust_or_clippy);
+        let clippy_lints_from_docs = {
+            let body = reqwest::blocking::get("https://rust-lang.github.io/rust-clippy/master/index.html").expect("error d1a0544a-566e-4bf4-a37e-7dac73be02fd").text().expect("error 012e3328-53a4-4266-b403-24ac3b8dcbf3");
+            fn parse_article_ids_from_file(html: &std::primitive::str) -> std::vec::Vec<std::string::String> {
+                let document = scraper::Html::parse_document(html);
+                let html_selector = scraper::Selector::parse("html").expect("error 80427609-cfed-4b38-bdea-0794535ef84a");
+                let body_selector = scraper::Selector::parse("body").expect("error 620c597c-0faa-408f-b9bc-29059d179951");
+                let div_container_selector = scraper::Selector::parse(r#"div[class="container"]"#).expect("error eb483b13-e70e-40f4-b83a-3eeb00413d57");
+                let article_selector = scraper::Selector::parse("article").expect("error d21dbe55-6f9f-4695-bf08-78da4f2424ea");
+                let label_selector = scraper::Selector::parse("label").expect("error fe3d9f11-f3b0-4e54-a54a-842fabe3d8a7");
+                let h2_lint_title_selector = scraper::Selector::parse(r#"h2[class="lint-title"]"#).expect("error f1473d4e-e26a-491d-9980-e1874301a6b2");
+                let span_label_label_default_lint_group_group_deprecated_selector = scraper::Selector::parse(r#"span[class="label label-default lint-group group-deprecated"]"#).expect("error e86d5496-f62b-428c-ac6c-d533e0f6f775");
+                let mut ids = std::vec::Vec::new();
+                for html_element in document.select(&html_selector) {
+                    for body_element in html_element.select(&body_selector) {
+                        for div_container_element in body_element.select(&div_container_selector) {
+                            for article_element in div_container_element.select(&article_selector) {
+                                let mut is_deprecated = false;
+                                for label_selector_element in article_element.select(&label_selector) {
                                     if is_deprecated {
                                         break;
                                     }
-                                    if h2_lint_title_selector_element.select(&span_label_label_default_lint_group_group_deprecated_selector).next().is_some() {
-                                        is_deprecated = true;
-                                        break;
+                                    for h2_lint_title_selector_element in label_selector_element.select(&h2_lint_title_selector) {
+                                        if is_deprecated {
+                                            break;
+                                        }
+                                        if h2_lint_title_selector_element.select(&span_label_label_default_lint_group_group_deprecated_selector).next().is_some() {
+                                            is_deprecated = true;
+                                            break;
+                                        }
                                     }
                                 }
-                            }
-                            if let Some(id) = article_element.value().attr("id") && !is_deprecated {
-                                ids.push(id.to_string());
+                                if let Some(id) = article_element.value().attr("id") && !is_deprecated {
+                                    ids.push(id.to_string());
+                                }
                             }
                         }
                     }
                 }
+                ids
             }
-            ids
-        }
-        let clippy_lints_from_docs = parse_article_ids_from_file(&body);
-        let mut lints_not_in_file = vec![];
-        for element in &clippy_lints_from_docs {
-            if !lints_vec_from_cargo_toml.contains(element) {
-                //todo seems like this lints still not added to clippy, but in the list in clippy site
-                if element == "decimal_bitwise_operands" ||
-                    element == "manual_ilog2" ||
-                    element == "ptr_offset_by_literal"
-                {
-                    println!("todo!() f5525a7e-a54f-44b2-8fb3-a28ec8ec8e77");
-                }
-                else {
-                    lints_not_in_file.push(element);
-                }
-            }
-        }
-        assert!(lints_not_in_file.is_empty(), "this clippy lints are not in the [workspace.lints.clippy]: {lints_not_in_file:#?}");
-        let mut outdated_lints_in_file = vec![];
-        for element in &lints_vec_from_cargo_toml {
-            if !clippy_lints_from_docs.contains(element) {
-                outdated_lints_in_file.push(element);
-            }
-        }
-        assert!(outdated_lints_in_file.is_empty(), "this clippy lints are outdated but still in [workspace.lints.clippy]: {outdated_lints_in_file:#?}");
+            parse_article_ids_from_file(&body)
+        };
+        compare(
+            rust_or_clippy,
+            lints_vec_from_cargo_toml,
+            clippy_lints_from_docs,
+            //todo on commit momment seems like this lints still not added to clippy, but in the list in clippy site
+            vec![
+                std::string::String::from(""),
+                std::string::String::from("decimal_bitwise_operands"),
+                std::string::String::from("manual_ilog2"),
+                std::string::String::from("ptr_offset_by_literal")
+            ]
+        )
     }
 }
