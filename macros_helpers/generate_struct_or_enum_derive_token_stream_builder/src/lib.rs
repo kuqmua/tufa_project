ba4c0a1fd745e7ef94bd6bce4da94c2660d170e7
kuqmua <serezhashmakov@outlook.com>
2025-12-26 21:05:04 +0300

    reuse

--- macros_helpers/generate_struct_or_enum_derive_token_stream_builder/src/lib.rs
+++ macros_helpers/generate_struct_or_enum_derive_token_stream_builder/src/lib.rs
@@ -1,12 +1,7 @@
 #[proc_macro]
 pub fn generate_struct_or_enum_derive_token_stream_builder(input_token_stream: proc_macro::TokenStream) -> proc_macro::TokenStream {
-    #[derive(Debug, Clone, serde::Deserialize)]
-    struct Element {
-        trait_name: String,
-        trait_type: String
-    }
     #[derive(Clone)]
-    struct ElementToUse {
+    struct Element {
         derive_trait_name_upper_camel_case: proc_macro2::TokenStream,
         derive_trait_name_snake_case: proc_macro2::TokenStream,
         derive_trait_name_if_snake_case: proc_macro2::TokenStream,
@@ -15,30 +10,55 @@ pub fn generate_struct_or_enum_derive_token_stream_builder(input_token_stream: p
     let make_pub_snake_case_token_stream = quote::quote!{make_pub};
     let make_pub_if_snake_case_token_stream = quote::quote!{make_pub_if};
     let make_pub_upper_camel_case_token_stream = quote::quote!{MakePub};
-    let element_to_use_vec = serde_json::from_str::<Vec<Element>>(&input_token_stream.to_string())
+    let element_vec = serde_json::from_str::<Vec<String>>(&input_token_stream.to_string())
         .expect("error c5d09740-51b2-4c11-9b66-931622d1a053")
         .into_iter()
         .map(|element| {
-            let derive_trait_name_upper_camel_case = {
-                let value = naming::parameter::DeriveSelfUpperCamelCase::from_display(&element.trait_name);
-                quote::quote!{#value}
+            let snake_case = {
+                let mut result = String::with_capacity(element.len());
+                let mut prev_is_underscore = false;
+                let mut prev_is_lowercase = false;
+                for element in element.chars() {
+                    if element.is_alphabetic() {
+                        if element.is_uppercase() {
+                            if prev_is_lowercase && !prev_is_underscore {
+                                result.push('_');
+                            }
+                            for lower in element.to_lowercase() {
+                                result.push(lower);
+                            }
+                            prev_is_lowercase = false;
+                        } else {
+                            result.push(element);
+                            prev_is_lowercase = true;
+                        }
+                        prev_is_underscore = false;
+                    } else {
+                        if !prev_is_underscore && !result.is_empty() {
+                            result.push('_');
+                            prev_is_underscore = true;
+                        }
+                        prev_is_lowercase = false;
+                    }
+                }
+                result.trim_matches('_').to_string()
             };
-            let derive_trait_name_snake_case = {
-                let value = naming::parameter::DeriveSelfSnakeCase::from_display(&element.trait_name);
-                quote::quote!{#value}
-            };
-            let derive_trait_name_if_snake_case = {
-                let value = naming::parameter::DeriveSelfIfSnakeCase::from_display(&element.trait_name);
-                quote::quote!{#value}
-            };
-            let trait_type = element.trait_type.parse::<proc_macro2::TokenStream>().expect("error 8672240f-97b3-40f5-bf14-dc4b13af528f");
-            ElementToUse {
-                derive_trait_name_upper_camel_case,
-                derive_trait_name_snake_case,
-                derive_trait_name_if_snake_case,
-                trait_type,
+            Element {
+                derive_trait_name_upper_camel_case: {
+                    let value = naming::parameter::DeriveSelfUpperCamelCase::from_display(&snake_case);
+                    quote::quote!{#value}
+                },
+                derive_trait_name_snake_case: {
+                    let value = naming::parameter::DeriveSelfSnakeCase::from_display(&snake_case);
+                    quote::quote!{#value}
+                },
+                derive_trait_name_if_snake_case: {
+                    let value = naming::parameter::DeriveSelfIfSnakeCase::from_display(&snake_case);
+                    quote::quote!{#value}
+                },
+                trait_type: element.parse::<proc_macro2::TokenStream>().expect("error 8672240f-97b3-40f5-bf14-dc4b13af528f"),
             }
-        }).collect::<Vec<ElementToUse>>();
+        }).collect::<Vec<Element>>();
     let (
         make_pub_pub_enum_token_stream,
         pub_enum_derive_vec_token_stream
@@ -54,7 +74,7 @@ pub fn generate_struct_or_enum_derive_token_stream_builder(input_token_stream: p
         }
         (
             generate_pun_enum_token_stream(&make_pub_upper_camel_case_token_stream),
-            element_to_use_vec.iter().map(|element|generate_pun_enum_token_stream(
+            element_vec.iter().map(|element|generate_pun_enum_token_stream(
                 &element.derive_trait_name_upper_camel_case
             ))
         )
@@ -68,7 +88,7 @@ pub fn generate_struct_or_enum_derive_token_stream_builder(input_token_stream: p
         }
         (
             generate_derive_trait_name_bool_token_stream(&make_pub_snake_case_token_stream),
-            element_to_use_vec.iter().map(|element|generate_derive_trait_name_bool_token_stream(
+            element_vec.iter().map(|element|generate_derive_trait_name_bool_token_stream(
                 &element.derive_trait_name_snake_case
             ))
         )
@@ -101,7 +121,7 @@ pub fn generate_struct_or_enum_derive_token_stream_builder(input_token_stream: p
                 &make_pub_if_snake_case_token_stream,
                 &make_pub_upper_camel_case_token_stream
             ),
-            element_to_use_vec.iter().map(|element|{
+            element_vec.iter().map(|element|{
                 let derive_trait_name_upper_camel_case = &element.derive_trait_name_upper_camel_case;
                 let derive_trait_name_snake_case = &element.derive_trait_name_snake_case;
                 let derive_trait_name_if_snake_case = &element.derive_trait_name_if_snake_case;
@@ -120,7 +140,7 @@ pub fn generate_struct_or_enum_derive_token_stream_builder(input_token_stream: p
             })
         )
     };
-    let if_self_derive_acc_push_vec_token_stream = element_to_use_vec.iter().map(|element|{
+    let if_self_derive_acc_push_vec_token_stream = element_vec.iter().map(|element|{
         let derive_trait_name_snake_case = &element.derive_trait_name_snake_case;
         let trait_type = &element.trait_type;
         quote::quote!{
--- macros_helpers/src/struct_or_enum_derive_token_stream_builder.rs
+++ macros_helpers/src/struct_or_enum_derive_token_stream_builder.rs
@@ -1,58 +1,16 @@
 generate_struct_or_enum_derive_token_stream_builder::generate_struct_or_enum_derive_token_stream_builder!([
-    {
-        "trait_name": "debug",
-        "trait_type": "Debug"
-    },
-    {
-        "trait_name": "default",
-        "trait_type": "Default"
-    },
-    {
-        "trait_name": "clone",
-        "trait_type": "Clone"
-    },
-    {
-        "trait_name": "copy",
-        "trait_type": "Copy"
-    },
-    {
-        "trait_name": "partial_eq",
-        "trait_type": "PartialEq"
-    },
-    {
-        "trait_name": "eq",
-        "trait_type": "Eq"
-    },
-    {
-        "trait_name": "partial_ord",
-        "trait_type": "PartialOrd"
-    },
-    {
-        "trait_name": "ord",
-        "trait_type": "Ord"
-    },
-    {
-        "trait_name": "serde_serialize",
-        "trait_type": "serde::Serialize"
-    },
-    {
-        "trait_name": "serde_deserialize",
-        "trait_type": "serde::Deserialize"
-    },
-    {
-        "trait_name": "utoipa_to_schema",
-        "trait_type": "utoipa::ToSchema"
-    },
-    {
-        "trait_name": "schemars_json_schema",
-        "trait_type": "schemars::JsonSchema"
-    },
-    {
-        "trait_name": "thiserror_error",
-        "trait_type": "thiserror::Error"
-    },
-    {
-        "trait_name": "error_occurence_lib_error_occurence",
-        "trait_type": "error_occurence_lib::ErrorOccurence"
-    }
+    "Debug",
+    "Default",
+    "Clone",
+    "Copy",
+    "PartialEq",
+    "Eq",
+    "PartialOrd",
+    "Ord",
+    "serde::Serialize",
+    "serde::Deserialize",
+    "utoipa::ToSchema",
+    "schemars::JsonSchema",
+    "thiserror::Error",
+    "error_occurence_lib::ErrorOccurence"
 ]);
