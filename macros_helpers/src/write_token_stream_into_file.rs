ddb605cb5b7387359871a504234b31de8f2f64be
kuqmua <serezhashmakov@outlook.com>
2026-01-12 10:41:52 +0300

    cargo fmt + clippy

--- macros_helpers/src/write_token_stream_into_file.rs
+++ macros_helpers/src/write_token_stream_into_file.rs
@@ -8,13 +8,13 @@ pub fn write_token_stream_into_file(
     token_stream: &proc_macro2::TokenStream,
     format_with_rustfmt: &FormatWithRustfmt,
 ) {
-    let path = format!("{file_name}.rs");
-    let path = std::path::Path::new(&path);
+    let path_string = format!("{file_name}.rs");
+    let path = std::path::Path::new(&path_string);
     {
         let mut file = std::fs::File::create(path).expect("create output file failed");
         std::io::Write::write_all(&mut file, token_stream.to_string().as_bytes())
             .expect("write token stream failed");
-    }
+    };
     //no other way to format only one file. it formats all files in project
     if let FormatWithRustfmt::True = format_with_rustfmt {
         let status = std::process::Command::new("cargo")
@@ -23,8 +23,6 @@ pub fn write_token_stream_into_file(
             .arg(path)
             .status()
             .expect("failed to run cargo fmt");
-        if !status.success() {
-            panic!("cargo fmt failed for {}", path.display());
-        }
+        assert!(status.success(), "cargo fmt failed for {}", path.display());
     }
 }
