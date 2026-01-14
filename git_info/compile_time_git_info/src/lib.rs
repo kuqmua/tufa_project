1c4ed634dccdb9a684b3763952547d52ae47334d
kuqmua <serezhashmakov@outlook.com>
2026-01-14 12:54:12 +0300

    cargo fmt

--- git_info/compile_time_git_info/src/lib.rs
+++ git_info/compile_time_git_info/src/lib.rs
@@ -7,13 +7,19 @@ pub fn compile_time_project_git_info(
         .args(["rev-parse", "HEAD"])
         .output()
         .expect("d6b0f81a-1fc1-42fe-a20c-5e3cfd8a6403");
-    assert!(output.status.success(), "f7185b72-c92e-4558-8615-1ae11bcaa131");
+    assert!(
+        output.status.success(),
+        "f7185b72-c92e-4558-8615-1ae11bcaa131"
+    );
     let hash = String::from_utf8(output.stdout)
         .expect("9a3f659d-aca1-47d7-be32-cc3ba06e1b01")
         .trim()
         .to_owned();
     // Validate SHA-1 (40 hex chars)
-    assert!(hash.len() == 40 && hash.chars().all(|element| element.is_ascii_hexdigit()), "093516ae-a89f-42df-8b01-9b2897111705");
+    assert!(
+        hash.len() == 40 && hash.chars().all(|element| element.is_ascii_hexdigit()),
+        "093516ae-a89f-42df-8b01-9b2897111705"
+    );
     let commit_id_token_stream = format!("\"{hash}\"")
         .parse::<proc_macro2::TokenStream>()
         .expect("842e75e8-1c25-44af-bb71-f15ee1c0c67d");
