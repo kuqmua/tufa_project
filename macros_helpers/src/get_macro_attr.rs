21e9623cfcc56b1aa19dbc22288b555cc74f1476
kuqmua <serezhashmakov@outlook.com>
2026-02-16 00:37:16 +0300

    ref

--- macros_helpers/src/get_macro_attr.rs
+++ macros_helpers/src/get_macro_attr.rs
@@ -6,14 +6,16 @@ pub fn get_macro_attr<'attrs_litime>(
     attrs: &'attrs_litime [Attribute],
     attr_path: &String,
 ) -> &'attrs_litime Attribute {
-    let option_attr = attrs.iter().find(|attr| {
-        *attr_path == {
-            let mut str_path = ToTokens::to_token_stream(&attr.path()).to_string();
-            str_path.retain(|value_3b43b8ea| !value_3b43b8ea.is_whitespace());
-            str_path
-        }
-    });
-    option_attr.expect("68acaa15")
+    attrs
+        .iter()
+        .find(|el0| {
+            *attr_path == {
+                let mut value = ToTokens::to_token_stream(&el0.path()).to_string();
+                value.retain(|el1| !el1.is_whitespace());
+                value
+            }
+        })
+        .expect("68acaa15")
 }
 
 #[must_use]
@@ -21,17 +23,16 @@ pub fn get_macro_attr_meta_list_ts<'attrs_lifetime>(
     attrs: &'attrs_lifetime [Attribute],
     attr_path: &String,
 ) -> &'attrs_lifetime Ts2 {
-    let option_attr = attrs
-        .iter()
-        .find(|attr| {
-            *attr_path == {
-                let mut str_path = ToTokens::to_token_stream(&attr.path()).to_string();
-                str_path.retain(|value_e5eda357| !value_e5eda357.is_whitespace());
-                str_path
-            }
-        })
-        .clone();
-    let attr = option_attr.expect("9d057161");
+    let attr = attrs
+    .iter()
+    .find(|el0| {
+        *attr_path == {
+            let mut value = ToTokens::to_token_stream(&el0.path()).to_string();
+            value.retain(|el1| !el1.is_whitespace());
+            value
+        }
+    })
+    .clone().expect("9d057161");
     if let Meta::List(value) = &attr.meta {
         &value.tokens
     } else {
