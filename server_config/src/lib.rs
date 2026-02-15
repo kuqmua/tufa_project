be9441039cad92aaea97c692f29e07bc0c7c50fc
kuqmua <serezhashmakov@outlook.com>
2026-02-15 12:32:48 +0300

    use secrecy::SecretBox;

--- server_config/src/lib.rs
+++ server_config/src/lib.rs
@@ -1,12 +1,12 @@
 use chrono::FixedOffset;
 use config_lib::types::{SourcePlaceType, TracingLevel};
+use secrecy::SecretBox;
 use std::net::SocketAddr;
 use thiserror::Error;
-
 #[derive(Debug, config_lib::TryFromEnv, config_lib::GenGetterTraitsForStructFields)]
 pub struct Config {
     //todo maybe auto gen .env and docker-compose environment variables. and maybe write in directly into files
-    pub database_url: secrecy::SecretBox<String>,
+    pub database_url: SecretBox<String>,
     pub enable_api_git_commit_check: bool,
     pub maximum_size_of_http_body_in_bytes: usize,
     pub service_socket_address: SocketAddr,
