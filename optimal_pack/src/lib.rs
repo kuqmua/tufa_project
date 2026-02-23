91c7d9cf0c2d87dde2368338815a4e98bfcd2850
kuqmua <serezhashmakov@outlook.com>
2026-02-23 18:03:57 +0300

    OptimalPack for enum

--- optimal_pack/src/lib.rs
+++ optimal_pack/src/lib.rs
@@ -2,43 +2,35 @@ use gen_quotes::dq_ts;
 use naming::ToTokensToScStr;
 use proc_macro::TokenStream as Ts;
 use proc_macro2::TokenStream as Ts2;
-use quote::quote;
+use quote::{quote, ToTokens};
 use syn::{Data, DeriveInput, Fields, Ident, parse};
 #[proc_macro_derive(OptimalPack)]
 pub fn optimal_pack(input_ts: Ts) -> Ts {
     let di: DeriveInput = parse(input_ts).expect("a1d306de");
     let ident = &di.ident;
-    let fields = match &di.data {
-        Data::Struct(data) => match &data.fields {
-            Fields::Named(fields) => &fields.named,
-            Fields::Unnamed(fields) => &fields.unnamed,
-            Fields::Unit => {
-                return Ts::new();
-            }
-        },
-        Data::Enum(_) | Data::Union(_) => {
-            return Ts::new();
-        }
-    };
+    let ident_dq_ts = dq_ts(&ident);
     let ident_test_optimal_pack = format!("{}_test_optimal_pack", ToTokensToScStr::case(ident))
         .parse::<Ts2>()
         .expect("63213aaa");
-    let ts = fields.iter().enumerate().map(|(i, field)| {
-        let fi_dq_ts = dq_ts(&field.ident.as_ref().map_or_else(
-            || Ident::new(&format!("field_{i}"), ident.span()),
-            Clone::clone,
-        ));
-        let ft = &field.ty;
-        quote! {
-            (#fi_dq_ts, std::mem::align_of::<#ft>())
-        }
-    });
-    let generated = quote! {
-        #[cfg(test)]
-        mod #ident_test_optimal_pack {
-            use super::*;
-            #[test]
-            fn #ident_test_optimal_pack() {
+    let gen_unnamed_ts = |first_ts: &dyn ToTokens, second_ts: &dyn ToTokens|quote!{(#first_ts, std::mem::align_of::<#second_ts>())};
+    let gen_fi = |i: usize|Ident::new(&format!("field_{i}"), ident.span());
+    let ts = match &di.data {
+        Data::Struct(data) => {
+            let fields = match &data.fields {
+                Fields::Named(fields) => &fields.named,
+                Fields::Unnamed(fields) => &fields.unnamed,
+                Fields::Unit => {
+                    return Ts::new();
+                }
+            };
+            let ts = fields.iter().enumerate().map(|(i, field)|gen_unnamed_ts(
+                &dq_ts(&field.ident.as_ref().map_or_else(
+                    ||gen_fi(i),
+                    Clone::clone,
+                )),
+                &field.ty
+            ));
+            quote! {
                 let fields = [#(#ts),*];
                 for i in 1..fields.len() {
                     let (prev_name, prev_align) = fields.get(i - 1).expect("ba86f1dd");
@@ -47,14 +39,70 @@ pub fn optimal_pack(input_ts: Ts) -> Ts {
                         curr_align <= prev_align,
                         "Fields in `{}` are not optimally packed by alignment:\n\
                          field `{}` (alignment={}) > previous field `{}` (alignment={})",
-                        stringify!(ServerPortEr),
+                        #ident_dq_ts,
                         curr_name, curr_align,
                         prev_name, prev_align
                     );
                 }
             }
         }
+        Data::Enum(data_enum) => {
+            let mut vars_ts = Vec::new();
+            for var in &data_enum.variants {
+                let var_ident = &var.ident;
+                let var_ident_dq_ts = dq_ts(&var_ident);
+                let fields = match &var.fields {
+                    Fields::Named(fields) => &fields.named,
+                    Fields::Unnamed(fields) => &fields.unnamed,
+                    Fields::Unit => continue,
+                };
+                if fields.is_empty() {
+                    continue;
+                }
+                let fields_ts = fields.iter().enumerate().map(|(i, field)|gen_unnamed_ts(
+                    &dq_ts(&field.ident.as_ref().map_or_else(
+                        ||gen_fi(i),
+                        Clone::clone,
+                    )),
+                    &field.ty
+                ));
+                vars_ts.push(quote! {{
+                    let fields = [#(#fields_ts),*];
+                    for i in 1..fields.len() {
+                        let (prev_name, prev_align) = fields.get(i-1).expect("2011de63");
+                        let (curr_name, curr_align) = fields.get(i).expect("1272b618");
+                        assert!(
+                            curr_align <= prev_align,
+                            "In enum `{}`, variant `{}`: field `{}` (alignment={}) > previous field `{}` (alignment={})",
+                            #ident_dq_ts,
+                            #var_ident_dq_ts,
+                            curr_name, curr_align,
+                            prev_name, prev_align
+                        );
+                    }
+                }});
+            }
+            if vars_ts.is_empty() {
+                return Ts::new();
+            }
+            quote! {#(#vars_ts)*}
+        }
+        Data::Union(_) => {
+            return Ts::new();
+        }
     };
-    // println!("{generated}");
+    let generated = quote! {
+        #[cfg(test)]
+        mod #ident_test_optimal_pack {
+            use super::*;
+            #[test]
+            fn #ident_test_optimal_pack() {
+                #ts
+            }
+        }
+    };
+    // if ident == "" {
+    //     println!("{generated}");
+    // }
     generated.into()
 }
